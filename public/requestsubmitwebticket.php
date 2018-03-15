71e4241c58baafeaaefec831d9b6a88bad199ed7
luchaos <lcs@luchaos.at>
2018-03-15 18:48:09 +0100

    minor code style fixes empty() instead of string comparison (covers array post data potentially not being set) html with php echos instead of echoing out html no php closing tag

--- public/requestsubmitwebticket.php
+++ public/requestsubmitwebticket.php
@@ -1,46 +1,43 @@
-<?php require_once __DIR__ . '/../lib/bootstrap.php';
+<?php
+require_once __DIR__ . '/../lib/bootstrap.php';
 
-	if( !ValidatePOSTChars( "ucip" ) )
-	{
-		echo "FAILED";
-		return;
-	}
+if (!ValidatePOSTChars("ucip")) {
+    echo "FAILED";
+    return;
+}
 
-	$user          = seekPOST( 'u' );
-	$cookie        = seekPOST( 'c' );
-	$achievementID = seekPOST( 'i' );
-	$problemType   = seekPOST( 'p' );
+$user = seekPOST('u');
+$cookie = seekPOST('c');
+$achievementID = seekPOST('i');
+$problemType = seekPOST('p');
 
-	if( isset( $_POST['note'] ) )
-	{
-		$appendNote = $_POST['note']['description'];
+if (isset($_POST['note'])) {
+    $appendNote = $_POST['note']['description'];
 
-		if( "" != trim( $_POST['note']['checksum'] ))
-			$appendNote .= "<br/>MD5: " . $_POST['note']['checksum'];
+    if (!empty(trim($_POST['note']['checksum']))) {
+        $appendNote .= "<br/>MD5: " . $_POST['note']['checksum'];
+    }
 
-		if( "" != trim( $_POST['note']['emulator'] ))
-		{
-			$appendNote .= "<br/>Emulator: " . $_POST['note']['emulator'];
+    if (!empty(trim($_POST['note']['emulator']))) {
+        $appendNote .= "<br/>Emulator: " . $_POST['note']['emulator'];
 
-			if( $_POST['note']['emulator'] == "RetroArch" )
-				$appendNote .= " (" . $_POST['note']['core'] . ")";
-		}
+        if ($_POST['note']['emulator'] == "RetroArch") {
+            $appendNote .= " (" . $_POST['note']['core'] . ")";
+        }
+    }
 
-		$note = $appendNote;
-	}
+    $note = $appendNote;
+}
 
-	if( validateUser_cookie( $user, $cookie, 0 ) == TRUE )
-	{
-		$success = submitNewTickets( $user, $achievementID, $problemType, $note, $msgOut );
-    if ( $msgOut == "FAILED!")
-      header( "Location: http://" . AT_HOST . "/Achievement/$achievementID?e=issue_failed" );
-    else
-      header( "Location: http://" . AT_HOST . "/Achievement/$achievementID?e=issue_submitted" );
+if (validateUser_cookie($user, $cookie, 0) == true) {
+    $success = submitNewTickets($user, $achievementID, $problemType, $note, $msgOut);
+    if ($msgOut == "FAILED!") {
+        header("Location: http://" . AT_HOST . "/Achievement/$achievementID?e=issue_failed");
+    } else {
+        header("Location: http://" . AT_HOST . "/Achievement/$achievementID?e=issue_submitted");
+    }
 
-		echo $msgOut;
-	}
-	else
-	{
-		echo "FAILED: Cannot validate user! Try logging out and back in, or confirming your email.";
-	}
-?>
+    echo $msgOut;
+} else {
+    echo "FAILED: Cannot validate user! Try logging out and back in, or confirming your email.";
+}
