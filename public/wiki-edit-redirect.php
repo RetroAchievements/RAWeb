e63351a8318468803238269ef8b9f898149aeb65
luchaos <lcs@luchaos.at>
2018-04-17 21:04:49 +0200

    added wiki edit redirect for docs

---
+++ public/wiki-edit-redirect.php
@@ -0,0 +1,15 @@
+<?php
+if(empty($_GET['page'])) {
+    return;
+}
+$page = pathinfo($_GET['page'])['filename'];
+if(empty($page)) {
+    return;
+}
+// path rewrites
+switch ($page) {
+    case 'index':
+        $page = 'Home';
+        break;
+}
+header("location: https://github.com/RetroAchievements/docs/wiki/$page/_edit");
