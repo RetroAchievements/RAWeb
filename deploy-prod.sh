14df066d80f33d9da4ddf9afbe14b7a79a34bd30
luchaos <lcs@luchaos.at>
2018-05-14 02:06:58 +0200

    added deployment scripts

---
+++ deploy-prod.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ssh -T deploy-web@retroachievements.org << EOF
+    cd /var/www/html
+    #php artisan down
+    git pull -v
+    php composer.phar install --no-dev
+    #php artisan config:cache
+    #php artisan route:cache
+    #php artisan up
+EOF
