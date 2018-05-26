47fd5a856fb982be0f6ca5d62d0de5863c7bd362
meleu <meleuzord@yahoo.com>
2018-05-26 15:31:23 -0300

    link to Most Reported Games on Developers menu

--- lib/dynrender.php
+++ lib/dynrender.php
@@ -772,6 +772,7 @@ function RenderToolbar( $user, $permissions = 0 )
         echo "<li><a href='/developerstats.php'>Developer Stats</a></li>";	    
         echo "<li><a href='/achievementinspector.php'>Ach. Inspector</a></li>";
         echo "<li><a href='/ticketmanager.php'>Ticket Manager</a></li>";
+        echo "<li><a href='/ticketmanager.php?f=1'>Most Reported Games</a></li>";
         echo "<li><a href='/viewforum.php?f=0'>Invalid Forum Posts</a></li>";
         echo "<li><a href='/viewtopic.php?t=394'>Official To-Do List</a></li>";
         echo "</ul>";
