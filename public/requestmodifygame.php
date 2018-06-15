508dd0c7c9a98ab08c5f7c3b74d55cc60ed4bc37
meleu <meleu@users.noreply.github.com>
2018-06-15 09:21:49 -0300

    requestmodifygame.php not deprecated yet

--- public/requestmodifygame.php
+++ public/requestmodifygame.php
@@ -1,32 +1,32 @@
-<?php 
-	require_once __DIR__ . '/../lib/bootstrap.php';
-	
-	//var_dump( $_POST );
-	//exit;
-	
-	//	Sanitise!
-	if( !ValidatePOSTChars( "ugfv" ) )
-	{
-		echo "FAILED";
-		return;
-	}
-	
-	$author = $_POST["u"];
-	$gameID = $_POST["g"];
-	$field = $_POST["f"];
-	$value = $_POST["v"];
-	
-	if( RA_ReadCookieCredentials( $user, $points, $truePoints, $unreadMessageCount, $permissions, \RA\Permissions::Developer ) )
-	{
-		if( requestModifyGame( $author, $gameID, $field, $value ) )
-		{
-			header( "location: http://" . AT_HOST . "/Game/$gameID?e=modify_game_ok" );
-			exit;
-		}
-		else
-		{
-			header( "location: http://" . AT_HOST . "/Game/$gameID?e=errors_in_modify_game" );
-			exit;
-		}
-	}
+<?php
+    //require_once __DIR__ . '/../lib/bootstrap.php';
+
+    //var_dump( $_POST );
+    //exit;
+
+    // Sanitise!
+    if( !ValidatePOSTChars( "ugfv" ) )
+    {
+        echo "FAILED";
+        return;
+    }
+
+    $author = $_POST["u"];
+    $gameID = $_POST["g"];
+    $field = $_POST["f"];
+    $value = $_POST["v"];
+
+    if( RA_ReadCookieCredentials( $user, $points, $truePoints, $unreadMessageCount, $permissions, \RA\Permissions::Developer ) )
+    {
+        if( requestModifyGame( $author, $gameID, $field, $value ) )
+        {
+            header( "location: http://" . AT_HOST . "/Game/$gameID?e=modify_game_ok" );
+            exit;
+        }
+        else
+        {
+            header( "location: http://" . AT_HOST . "/Game/$gameID?e=errors_in_modify_game" );
+            exit;
+        }
+    }
 ?>
