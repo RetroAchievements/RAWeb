a37bd3471dc28e376a63b0b383a7e5673b5f6299
meleu <meleu@users.noreply.github.com>
2018-10-05 00:32:33 -0300

    indentation with spaces

--- src/Emulators.php
+++ src/Emulators.php
@@ -4,16 +4,16 @@ namespace RA;
 
 abstract class Emulators
 {
-  // reference for the numbers used here:
-  // https://github.com/RetroAchievements/RAIntegration/blob/master/src/RA_Interface.h
-  // enum EmulatorID
-	const RAGens = 0;
-	const RAP64 = 1;
-	const RASnes9x = 2;
-	const RAVBA = 3;
-  // const RA_Nester = 4; // unused
-	const RANes = 5;
-  const RAPCE = 6;
-	const RALibretro = 7;
-	const RAMeka = 8;
+    // reference for the numbers used here:
+    // https://github.com/RetroAchievements/RAIntegration/blob/master/src/RA_Interface.h
+    // enum EmulatorID
+    const RAGens = 0;
+    const RAP64 = 1;
+    const RASnes9x = 2;
+    const RAVBA = 3;
+    // const RA_Nester = 4; // unused
+    const RANes = 5;
+    const RAPCE = 6;
+    const RALibretro = 7;
+    const RAMeka = 8;
 }
