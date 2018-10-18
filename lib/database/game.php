9cf0f292aaecfac1419405aff329b52ee8c21a51
meleu <meleu@users.noreply.github.com>
2018-10-18 19:42:37 -0300

    fixed valid ConsoleIDs (Events=32; Atari7800=51)

--- lib/database/game.php
+++ lib/database/game.php
@@ -1430,7 +1430,8 @@ function isValidConsoleID( $consoleID ) {
         case 25: // Atari 2600
         case 27: // Arcade
         case 28: // Virtual Boy
-        case 32: // Atari 7800
+        case 32: // Events (not an actual console)
+        case 51: // Atari7800
             return TRUE;
         default:
             return FALSE;
