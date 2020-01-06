1e0770d97f95b5b760b1519750661b79fe4f4fc4
luchaos <luchaos@gmail.com>
2020-01-06 10:24:17 +0100

    fix submit news layout

--- public/submitnews.php
+++ public/submitnews.php
@@ -79,145 +79,141 @@ RenderHtmlHead("Manage News");
       alert(data)
     }
   }
-
 </script>
 <div id="mainpage">
-    <?php
-    $yOffs = 0;
-    //RenderTwitchTVStream( $yOffs );
-    //RenderChat( $user, $yOffs );
+    <div id="fullcontainer">
+        <div class="navpath">
+            <b>Manage News</b>
+        </div>
 
-    // echo "<div class='right'>";
-    // echo "<h2 class='longheader'>Upload news image</h2>";
-    // echo "470px max width! Image size will be scaled to fit.<br>";
+        <div class="largelist">
+            <h2 class="longheader">How to use</h2>
+            <p>
+                Here you can submit new articles or modify old articles that can be viewed on the frontpage of the site.<br>
+                Please note: news images will be scaled to 470px width, and (currently) drawn at 220px height.
+            </p>
+            <br>
+        </div>
 
-    // //echo "<form style='padding: 2px;' method='post' enctype='multipart/form-data' >";
-    // echo "<input type='submit' name='submit' style='float: right;' value='Select News Image' />";
-    // //echo "</form>";
+        <h2 class="longheader">Submit News</h2>
+        <?php
+        $yOffs = 0;
+        // RenderTwitchTVStream( $yOffs );
+        // RenderChat( $user, $yOffs );
 
-    // echo "<br>";
-    // echo "</div>";
+        // echo "<div class='right'>";
+        // echo "<h2 class='longheader'>Upload news image</h2>";
+        // echo "470px max width! Image size will be scaled to fit.<br>";
+        //
+        // //echo "<form style='padding: 2px;' method='post' enctype='multipart/form-data' >";
+        // echo "<input type='submit' name='submit' style='float: right;' value='Select News Image' />";
+        // //echo "</form>";
+        //
+        // echo "<br>";
+        // echo "</div>";
 
-    RenderErrorCodeWarning('left', $errorCode);
+        RenderErrorCodeWarning('left', $errorCode);
 
-    echo "<div id='fullcontainer'>";
+        echo "Select Existing or Create New:&nbsp;";
+        echo "<select name='ab' onchange=\"if (this.selectedIndex >= 0) window.location = '/submitnews.php?n=' + this.value; return false;\" >";
 
-    echo "<div class='navpath'>";
-    echo "<b>Manage News</b>";
-    echo "</div>";
+        echo "<option value=0>--New Post--</option>";
+        for ($i = 0; $i < $newsCount; $i++) {
+            $nextNews = $newsData[$i];
+            $nextID = $nextNews['ID'];
+            $nextTitle = $nextNews['Title'];
 
-    echo "<div class='largelist'>"; //?
-    echo "<h2 class='longheader'>How to use</h2>";
-    echo "<p>Here you can submit new articles or modify old articles that can be viewed on the frontpage of the site.<br>";
-    echo "Please note: news images will be scaled to 470px width, and (currently) drawn at 220px height.</p>";
-    echo "<br>";
+            $selected = ($nextID == $newsArticleID) ? "selected" : "";
 
-    echo "</div>";
+            echo "<option value='$nextID' $selected><a href='/submitnews.php?n=$nextID'>$nextID - $nextTitle</a></option>";
 
-    echo "<h2 class='longheader'>Submit News</h2>";
-
-    echo "Select Existing or Create New:&nbsp;";
-    echo "<select name='ab' onchange=\"if (this.selectedIndex >= 0) window.location = '/submitnews.php?n=' + this.value; return false;\" >";
-
-    echo "<option value=0>--New Post--</option>";
-    for ($i = 0; $i < $newsCount; $i++) {
-        $nextNews = $newsData[$i];
-        $nextID = $nextNews['ID'];
-        $nextTitle = $nextNews['Title'];
-
-        $selected = ($nextID == $newsArticleID) ? "selected" : "";
-
-        echo "<option value='$nextID' $selected><a href='/submitnews.php?n=$nextID'>$nextID - $nextTitle</a></option>";
-
-        if ($nextNews['ID'] == $newsArticleID) {
-            $activeNewsArticle = $nextNews;
+            if ($nextNews['ID'] == $newsArticleID) {
+                $activeNewsArticle = $nextNews;
+            }
         }
-    }
-    echo "</select><br>";
+        echo "</select><br>";
 
-    ?>
-    <br>
-    <?php
-    if (isset($newsArticleID) && $newsArticleID != 0) {
-        echo "ID: <input type='text' name='i' size='2' value='$newsArticleID' readonly><br><br> ";
-    }
+        if (isset($newsArticleID) && $newsArticleID != 0) {
+            echo "ID: <input type='text' name='i' size='2' value='$newsArticleID' readonly><br><br> ";
+        }
 
-    $newsTitle = "";
-    if (isset($activeNewsArticle)) {
-        $newsTitle = $activeNewsArticle['Title'];
-    }
+        $newsTitle = "";
+        if (isset($activeNewsArticle)) {
+            $newsTitle = $activeNewsArticle['Title'];
+        }
 
-    $newsContent = "";
-    if (isset($activeNewsArticle)) {
-        $newsContent = $activeNewsArticle['Payload'];
-    }
+        $newsContent = "";
+        if (isset($activeNewsArticle)) {
+            $newsContent = $activeNewsArticle['Payload'];
+        }
 
-    $newsAuthor = $user;
-    if (isset($activeNewsArticle)) {
-        $newsAuthor = $activeNewsArticle['Author'];
-    }
+        $newsAuthor = $user;
+        if (isset($activeNewsArticle)) {
+            $newsAuthor = $activeNewsArticle['Author'];
+        }
 
-    $newsLink = "";
-    if (isset($activeNewsArticle)) {
-        $newsLink = $activeNewsArticle['Link'];
-    }
+        $newsLink = "";
+        if (isset($activeNewsArticle)) {
+            $newsLink = $activeNewsArticle['Link'];
+        }
 
-    $newsImage = $newsImageInput;
-    if (isset($activeNewsArticle)) {
-        $newsImage = $activeNewsArticle['Image'];
-    }
+        $newsImage = $newsImageInput;
+        if (isset($activeNewsArticle)) {
+            $newsImage = $activeNewsArticle['Image'];
+        }
 
-    echo "<table><tbody>";
+        echo "<table><tbody>";
 
-    echo "<tr>";
-    echo "<td colspan='2'>";
-    echo "Title: <input id='NewsTitle' type='text' name='t' style='width: 80%; float: right;' value='$newsTitle' ><br>";
-    echo "</td>";
-    echo "</tr>";
+        echo "<tr>";
+        echo "<td colspan='2'>";
+        echo "Title: <input id='NewsTitle' type='text' name='t' style='width: 80%; float: right;' value='$newsTitle' ><br>";
+        echo "</td>";
+        echo "</tr>";
 
-    echo "<tr>";
-    echo "<td colspan='2'>";
-    echo "Link (optional): <input id='NewsLink' type='text' name='l' style='width: 80%; float: right;' value='$newsLink'><br>";
-    echo "</td>";
-    echo "</tr>";
+        echo "<tr>";
+        echo "<td colspan='2'>";
+        echo "Link (optional): <input id='NewsLink' type='text' name='l' style='width: 80%; float: right;' value='$newsLink'><br>";
+        echo "</td>";
+        echo "</tr>";
 
-    echo "<tr>";
-    echo "<td>";
-    echo "Image: <input id='NewsImage' size='44' type='text' name='g' value='$newsImage' onchange=\"$('#NewsImagePreview').attr( 'src', $('#NewsImage').val() ); return false;\">";
-    echo "</td>";
-    echo "<td>";
-    echo "&nbsp;<img id='loadingicon' style='opacity: 0;' src='" . getenv('APP_STATIC_URL') . "/Images/loading.gif' alt='loading icon' />";
-    echo "&nbsp;New image:";
-    echo "<input type='file' style='float: right;' name='file' id='uploadimagefile' onchange=\"return UploadImage();\" /> <br>";
-    echo "</td>";
-    echo "</tr>";
+        echo "<tr>";
+        echo "<td>";
+        echo "Image: <input id='NewsImage' size='44' type='text' name='g' value='$newsImage' onchange=\"$('#NewsImagePreview').attr( 'src', $('#NewsImage').val() ); return false;\">";
+        echo "</td>";
+        echo "<td>";
+        echo "&nbsp;<img id='loadingicon' style='opacity: 0;' src='" . getenv('APP_STATIC_URL') . "/Images/loading.gif' alt='loading icon' />";
+        echo "&nbsp;New image:";
+        echo "<input type='file' style='float: right;' name='file' id='uploadimagefile' onchange=\"return UploadImage();\" /> <br>";
+        echo "</td>";
+        echo "</tr>";
 
-    echo "<tr>";
-    echo "<td colspan='2'>";
-    echo "Preview: <br>";
-    echo "<div class='submitnewstestimageholder'>";
-    echo "<img id='NewsImagePreview' src='$newsImage' width='470' style='margin:0;' /><br>";
-    echo "</div>";
-    echo "</td>";
-    echo "</tr>";
+        echo "<tr>";
+        echo "<td colspan='2'>";
+        echo "Preview: <br>";
+        echo "<div class='submitnewstestimageholder'>";
+        echo "<img id='NewsImagePreview' src='$newsImage' width='470' style='margin:0;' /><br>";
+        echo "</div>";
+        echo "</td>";
+        echo "</tr>";
 
-    echo "<tr>";
-    echo "<td colspan='2'>";
-    echo "Article Content (most HTML supported): <br>";
-    echo "<textarea id='NewsPayload' rows='10' cols='80' name='p' style='width: 100%;'>$newsContent</textarea><br>";
-    echo "</td>";
-    echo "</tr>";
+        echo "<tr>";
+        echo "<td colspan='2'>";
+        echo "Article Content (most HTML supported): <br>";
+        echo "<textarea id='NewsPayload' rows='10' cols='80' name='p' style='width: 100%;'>$newsContent</textarea><br>";
+        echo "</td>";
+        echo "</tr>";
 
-    echo "<tr>";
-    echo "<td colspan='2'>";
-    echo "<input onclick=\"onSubmitNews()\" type='submit' name='submit' size='37' style='float: right;' value='Submit News Article!' />";
-    echo "Author: <input id='NewsAuthor' type='text' name='a' value='$newsAuthor' readonly><br>";
-    echo "</td>";
-    echo "</tr>";
+        echo "<tr>";
+        echo "<td colspan='2'>";
+        echo "<input onclick=\"onSubmitNews()\" type='submit' name='submit' size='37' style='float: right;' value='Submit News Article!' />";
+        echo "Author: <input id='NewsAuthor' type='text' name='a' value='$newsAuthor' readonly><br>";
+        echo "</td>";
+        echo "</tr>";
 
-    echo "</tbody></table>";
-    ?>
-    <br>
+        echo "</tbody></table>";
+        ?>
+        <br>
+    </div>
 </div>
 <?php RenderFooter(); ?>
 </body>
