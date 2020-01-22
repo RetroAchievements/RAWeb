e310664dba94e0b0b81355678c2c2d9d24b8d656
luchaos <luchaos@gmail.com>
2020-01-22 01:28:50 +0100

    fix create topic page layout

--- public/createtopic.php
+++ public/createtopic.php
@@ -43,41 +43,41 @@ RenderHtmlHead("Create topic: $thisForumTitle");
 <?php RenderTitleBar($user, $points, $truePoints, $unreadMessageCount, $errorCode, $permissions); ?>
 <?php RenderToolbar($user, $permissions); ?>
 <div id="mainpage">
-    <?php RenderRecentForumPostsComponent(4); ?>
-    <div id="forums" class="left">
+    <div id="leftcontainer">
+        <div id="forums">
+            <?php
+            echo "<div class='navpath'>";
+            echo "<a href='forum.php'>Forum Index</a>";
+            echo " &raquo; <a href='/forum.php?c=$thisCategoryID'>$thisCategoryName</a>";
+            echo " &raquo; <a href='/viewforum.php?f=$thisForumID'>$thisForumTitle</a>";
+            echo " &raquo; <b>Create Topic</b></a>";
+            echo "</div>";
 
-        <?php
-        echo "<div class='navpath'>";
-        echo "<a href='forum.php'>Forum Index</a>";
-        echo " &raquo; <a href='/forum.php?c=$thisCategoryID'>$thisCategoryName</a>";
-        echo " &raquo; <a href='/viewforum.php?f=$thisForumID'>$thisForumTitle</a>";
-        echo " &raquo; <b>Create Topic</b></a>";
-        echo "</div>";
+            echo "<h2 class='longheader'>Create Topic: $thisForumTitle</h2>";
 
-        echo "<h2 class='longheader'>Create Topic: $thisForumTitle</h2>";
+            echo "<table>";
+            echo "<tbody>";
 
-        echo "<table>";
-        echo "<tbody>";
+            echo "<form action='/request/forum-topic/create.php' method='post'>";
+            echo "<input type='hidden' value='$cookieRaw' name='c'>";
+            echo "<input type='hidden' value='$requestedForumID' name='f'>";
+            echo "<tr>" . "<td>Forum:</td><td><input type='text' readonly value='$thisForumTitle'></td></tr>";
+            echo "<tr>" . "<td>Author:</td><td><input type='text' readonly value='$user' name='u'></td></tr>";
+            echo "<tr>" . "<td>Title:</td><td><input class='fullwidth' type='text' value='' name='t'></td></tr>";
+            echo "<tr>" . "<td>Message:</td><td>";
 
-        echo "<form action='/request/forum-topic/create.php' method='post'>";
-        echo "<input type='hidden' value='$cookieRaw' name='c'>";
-        echo "<input type='hidden' value='$requestedForumID' name='f'>";
-        echo "<tr>" . "<td>Forum:</td><td><input type='text' readonly value='$thisForumTitle'></td></tr>";
-        echo "<tr>" . "<td>Author:</td><td><input type='text' readonly value='$user' name='u'></td></tr>";
-        echo "<tr>" . "<td>Title:</td><td><input class='fullwidth' type='text' value='' name='t'></td></tr>";
-        echo "<tr>" . "<td>Message:</td><td>";
+            RenderPHPBBIcons();
 
-        RenderPHPBBIcons();
-
-        echo "<textarea id='commentTextarea' class='fullwidth forum' style='height:160px' rows=5 cols=63 name='p'></textarea></td></tr>";
-        echo "<tr>" . "<td></td><td class='fullwidth'><input type='submit' value='Submit new topic' SIZE='37'/></td></tr>";
-        echo "</form>";
-        echo "</tbody>";
-        echo "</table>";
-
-        ?>
-
-        <br>
+            echo "<textarea id='commentTextarea' class='fullwidth forum' style='height:160px' rows=5 name='p'></textarea></td></tr>";
+            echo "<tr>" . "<td></td><td class='fullwidth'><input type='submit' value='Submit new topic' SIZE='37'/></td></tr>";
+            echo "</form>";
+            echo "</tbody>";
+            echo "</table>";
+            ?>
+        </div>
+    </div>
+    <div id="rightcontainer">
+        <?php RenderRecentForumPostsComponent(4); ?>
     </div>
 </div>
 <?php RenderFooter(); ?>
