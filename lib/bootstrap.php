e23aeef3b740769df7b7d75aa8b4291f6b4ac0b3
luchaos <luchaos@gmail.com>
2020-01-31 15:44:39 +0100

    bump version

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -71,7 +71,7 @@ require_once(__DIR__ . '/util/utf8.php');
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
-define("VERSION", "1.39.0");
+define("VERSION", "1.40.0");
 
 try {
     $db = mysqli_connect(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_DATABASE'), getenv('DB_PORT'));
