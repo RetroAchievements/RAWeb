3bcbfe873778ece29c2b8ad5f595d44ea32cf81d
luchaos <luchaos@gmail.com>
2020-03-13 00:45:08 +0100

    migration: set missing default collations to latin1_general & convert latin1_swedish -> latin1_general

---
+++ database/202003013_230000_Fix_Collations.sql
@@ -0,0 +1,12 @@
+ALTER TABLE GameAlternatives DEFAULT COLLATE latin1_general_ci;
+ALTER TABLE PlaylistVideo DEFAULT COLLATE latin1_general_ci;
+ALTER TABLE PlaylistVideo CONVERT TO CHARACTER SET latin1 COLLATE latin1_general_ci;
+ALTER TABLE Rating DEFAULT COLLATE latin1_general_ci;
+ALTER TABLE Rating CONVERT TO CHARACTER SET latin1 COLLATE latin1_general_ci;
+ALTER TABLE ScoreHistory DEFAULT COLLATE latin1_general_ci;
+ALTER TABLE ScoreHistory CONVERT TO CHARACTER SET latin1 COLLATE latin1_general_ci;
+ALTER TABLE SetRequest DEFAULT COLLATE latin1_general_ci;
+ALTER TABLE SiteAwards DEFAULT COLLATE latin1_general_ci;
+ALTER TABLE SiteAwards CONVERT TO CHARACTER SET latin1 COLLATE latin1_general_ci;
+ALTER TABLE Ticket DEFAULT COLLATE latin1_general_ci;
+ALTER TABLE Ticket CONVERT TO CHARACTER SET latin1 COLLATE latin1_general_ci;
