9bfdf232f329d4c19d2a359e0d6708df9c4676c5
luchaos <luchaos@gmail.com>
2020-04-03 01:39:03 +0200

    query rank on user list

--- public/userInfo.php
+++ public/userInfo.php
@@ -266,8 +266,8 @@ RenderHtmlStart(true);
         } else {
             $countRankedUsers = countRankedUsers();
             $rankPct = sprintf("%1.0f", (($userRank / $countRankedUsers) * 100.0) + 1.0);
-            $rankOffset = (int)($userRank / 25) * 25;
-            echo "<a href='/userList.php?s=2&o=$rankOffset&u=1'>$userRank</a> / $countRankedUsers ranked users (Top $rankPct%)";
+            $rankOffset = (int)(($userRank - 1) / 25) * 25;
+            echo "<a href='/userList.php?s=2&o=$rankOffset'>$userRank</a> / $countRankedUsers ranked users (Top $rankPct%)";
         }
         echo "<br><br>";
 
