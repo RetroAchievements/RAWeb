1a62b7443c89deb72f0c5e8a28c1ff33380c4bcd
luchaos <luchaos@gmail.com>
2020-05-11 22:07:07 +0200

    Adjust `Comment` foreign key migration to not include `ActivityID`

--- database/20200502_000000_Add_Comment_Foreign_Keys.sql
+++ database/20200502_000000_Add_Comment_Foreign_Keys.sql
@@ -1,5 +1,4 @@
 ALTER TABLE `Comment`
-  ADD FOREIGN KEY (`ArticleType`) REFERENCES `Activity` (`ID`),
-  ADD FOREIGN KEY (`ArticleID`) REFERENCES `ArticleTypeDimension` (`ArticleTypeID`),
+  ADD FOREIGN KEY (`ArticleType`) REFERENCES `ArticleTypeDimension` (`ArticleTypeID`),
   ADD FOREIGN KEY (`UserID`) REFERENCES `UserAccounts` (`ID`)
 ;
