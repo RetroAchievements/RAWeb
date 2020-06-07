01bc8a647ccaa9ad7fdac154ebd2ad79dd5871a6
Searo <searo1205@gmail.com>
2020-06-07 08:13:02 -0400

    Update to Set Request calculation and adds a link on profile (#485)
    closes #401 
    * Adds request sets link to users profile
    
    * Removes set cap limit (was capped at 20) and changes the set request calculation (add a request at 2500, after 180k earn a request every 20k and earn a request for every year of account age)
    
    * get score based total requests using a loop
    
    Implementation of the logic described by @Tsearo here:
    https://github.com/RetroAchievements/RAWeb/pull/485#issue-422514585
    
    * Update setrequest.php
    
    * Update user.php
    
    Co-authored-by: meleu <meleu@users.noreply.github.com>

--- lib/database/setrequest.php
+++ lib/database/setrequest.php
@@ -56,25 +56,25 @@ function getUserRequestsInformation($user, $list, $gameID = -1)
     $requests['requestedThisGame'] = 0;
     $points = GetScore($user);
 
-    //Determine how many requests the user can make
-    if ($points >= 5000) {
-        $requests['total']++;
-    }
-    if ($points >= 10000) {
-        $requests['total']++;
-    }
-    if ($points >= 15000) {
-        $requests['total']++;
-    }
-    if ($points >= 20000) {
-        $requests['total']++;
-        $requests['total'] += floor(($points - 20000) / 10000);
+    // logic behind the amount of requests based on player's score:
+    $boundariesAndChunks = array(
+        180000 => 20000, // from 180k to infinite, +1 for each 20k chunk of points
+        20000 => 10000,  // from 20k to 180k, +1 for each 10k chunk
+        5000 => 5000,    // from 5k to 20k, +1 for each 5k chunk
+        0 => 2500,       // from 0 to 5k, +1 for each 2.5k chunk
+    );
+
+    $pointsLeft = $points;
+    foreach ($boundariesAndChunks as $boundary => $chunk) {
+        if ($pointsLeft >= $boundary) {
+            $aboveBoundary = $pointsLeft - $boundary;
+            $requests['total'] += floor($aboveBoundary / $chunk);
+            $pointsLeft = $boundary;
+        }
     }
 
-    //Cap the allowed set requests at 20
-    if ($requests['total'] > 20) {
-        $requests['total'] = 20;
-    }
+    // adding the number of years the user is here
+    $requests['total'] += getAge($user);
 
     //Determine how many of the users current requests are still valid.
     //Requests made for games that now have achievements do no count towards a used request
