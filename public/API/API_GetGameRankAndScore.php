0736797e85dfcf4b7a6027f2bca535d67236847e
meleu <meleuzord@yahoo.com>
2020-06-08 10:15:44 -0300

    use correct $requestedBy username

--- public/API/API_GetGameRankAndScore.php
+++ public/API/API_GetGameRankAndScore.php
@@ -4,8 +4,14 @@ require_once __DIR__ . '/../../lib/bootstrap.php';
 
 runPublicApiMiddleware();
 
+settype($gameId, 'integer');
 $gameId = seekGET('g');
-$username = seekGET('u');
+if ($gameId <= 0) {
+    echo json_encode(['success' => false]);
+    return;
+}
+
+$username = seekGET('z');
 
 $gameTopAchievers = getGameTopAchievers($gameId, 0, 10, $username);
 
