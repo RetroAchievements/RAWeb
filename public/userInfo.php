f80828d570d7fc735e0d17b00ea7a3490ac9854b
luchaos <luchaos@gmail.com>
2020-06-10 15:46:21 +0200

    CS

--- public/userInfo.php
+++ public/userInfo.php
@@ -273,22 +273,16 @@ RenderHtmlStart(true);
         }
         echo "<br>";
 
-        echo "<a href='/setRequestList.php?u=$userPage'> Requested Sets</a>" 
-                . " - " . $userSetRequestInformation['used'] . " of " . $userSetRequestInformation['total'] . " Requests Made";
+        echo "<a href='/setRequestList.php?u=$userPage'> Requested Sets</a>"
+            . " - " . $userSetRequestInformation['used']
+            . " of " . $userSetRequestInformation['total'] . " Requests Made";
         echo "<br><br>";
 
         if (!empty($userMassData['RichPresenceMsg']) && $userMassData['RichPresenceMsg'] !== 'Unknown') {
             echo "<div class='mottocontainer'>Last seen ";
             if (!empty($userMassData['LastGameID'])) {
                 $game = getGameData($userMassData['LastGameID']);
-                echo ' in ' . GetGameAndTooltipDiv(
-                    $game['ID'],
-                    $game['Title'],
-                    $game['ImageIcon'],
-                    null,
-                    false,
-                    22
-                ) . '<br>';
+                echo ' in ' . GetGameAndTooltipDiv($game['ID'], $game['Title'], $game['ImageIcon'], null, false, 22) . '<br>';
             }
             echo "<code>" . $userMassData['RichPresenceMsg'] . "</code></div>";
         }
