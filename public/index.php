7a88e7c7bafd386d1abd8848c7e429207815def3
meleu <meleuzord@yahoo.com>
2020-08-22 20:55:39 -0300

    add links to RAPodcast and RANews (and emojis)

--- public/index.php
+++ public/index.php
@@ -216,8 +216,10 @@ RenderToolbar($user, $permissions);
     </div>
     <div id="rightcontainer" style="padding-top: 20px">
         <?php
-        echo '<div class=\'btn-patron text-center\' style="margin-bottom: 10px"><a href=\'https://www.patreon.com/bePatron?u=5403777\' target="_blank" rel="noopener">Become a Patron!</a><!--script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script--></div>';
-        echo '<div class=\'btn-discord text-center\' style="margin-bottom: 10px"><a href=\'https://discord.gg/' . getenv('DISCORD_INVITE_ID') . '\' target="_blank" rel="noopener">Join us on Discord!</a></div>';
+        echo '<div class=\'btn-patron text-center\' style="margin-bottom: 10px"><a href=\'https://www.patreon.com/bePatron?u=5403777\' target="_blank" rel="noopener">️💙 Become a Patron!</a><!--script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script--></div>';
+        echo '<div class=\'btn-discord text-center\' style="margin-bottom: 10px"><a href=\'https://discord.gg/' . getenv('DISCORD_INVITE_ID') . '\' target="_blank" rel="noopener">💬 Join us on Discord!</a></div>';
+        echo '<div class=\'text-center\' style="margin-bottom: 10px"><a href=\'https://www.youtube.com/channel/UCIGdJGxrzmNYMaAGPsk2sIA\' target="_blank" rel="noopener">🎙️ RAPodcast</a></div>';
+        echo '<div class=\'text-center\' style="margin-bottom: 10px"><a href=\'https://news.retroachievements.org/\' target="_blank" rel="noopener">📰 RANews</a></div>';
         RenderDocsComponent();
         RenderAOTWComponent($staticData['Event_AOTW_AchievementID'], $staticData['Event_AOTW_ForumID']);
         //RenderTwitchTVStream();
