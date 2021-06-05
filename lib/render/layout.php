1ab2ed2990a352b7122c5244de4772e22d3d2d9c
MagmarFire <magmarfire7@yahoo.com>
2021-06-05 14:05:08 -0400

    Added 'My Sets' and 'My Tickets' links to the 'My Pages' dropdown

--- lib/render/layout.php
+++ lib/render/layout.php
@@ -289,14 +289,13 @@ function RenderToolbar($user, $permissions = 0)
         echo "<div>";
         echo "<ul>";
         echo "<li><a href='/user/$user'>Profile</a></li>";
-        //echo "<li><a href='/feed.php?i=1'>Feed</a></li>";
-        //echo "<li><a href='/feed.php'>Friends Feed</a></li>";
+        echo "<li><a href='/gameList.php?d=$user'>My Sets</a></li>";
+        echo "<li><a href='/ticketmanager.php?u=admin'>My Tickets</a></li>";
         echo "<li><a href='/achievementList.php?s=14&p=1'>Achievements</a></li>";
         echo "<li><a href='/friends.php'>Friends</a></li>";
         echo "<li><a href='/history.php'>History</a></li>";
         echo "<li><a href='/inbox.php'>Messages</a></li>";
         echo "<li><a href='/setRequestList.php?u=$user'>Requested Sets</a></li>";
-        // echo "<li><a href='/createmessage.php'>New Message</a></li>";
         echo "<li class='divider'></li>";
         echo "<li><a href='/controlpanel.php'>Settings</a></li>";
         echo "<li class='divider'></li>";
