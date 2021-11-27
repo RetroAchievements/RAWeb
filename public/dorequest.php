3d0e8c0ee86fbd32ddc451c07de0e38857bde42b
Jamiras <jamirasmt@gmail.com>
2021-11-27 15:02:10 -0700

    add uploadleaderboard dorequest api

--- public/dorequest.php
+++ public/dorequest.php
@@ -66,6 +66,7 @@ switch ($requestType) {
     case "submitlbentry":
     case "unlocks":
     case "uploadachievement":
+    case "uploadleaderboard":
         $credentialsOK = $validLogin && ($permissions >= \RA\Permissions::Registered);
         break;
 
@@ -425,9 +426,28 @@ switch ($requestType) {
         $newMemString = requestInput('m');
         $newFlags = requestInput('f', 0, 'integer');
         $newBadge = requestInput('b');
+
         $errorOut = "";
         $response['Success'] = UploadNewAchievement($user, $gameID, $newTitle, $newDesc, ' ', ' ', ' ', $newPoints, $newMemString, $newFlags, $achievementID, $newBadge, $errorOut);
         $response['AchievementID'] = $achievementID;
+        $response['Error'] = $errorOut;
+        break;
+
+    case "uploadleaderboard":
+        $leaderboardID = requestInput('i', 0, 'integer');
+        $newTitle = requestInput('n');
+        $newDesc = requestInput('d');
+        $newStartMemString = requestInput('s');
+        $newSubmitMemString = requestInput('b');
+        $newCancelMemString = requestInput('c');
+        $newValueMemString = requestInput('l');
+        $newLowerIsBetter = requestInput('w', 0, 'integer');
+        $newFormat = requestInput('f');
+        $newMemString = "STA:$newStartMemString::SUB:$newSubmitMemString::CAN:$newCancelMemString::VAL:$newValueMemString";
+
+        $errorOut = "";
+        $response['Success'] = UploadNewLeaderboard($user, $gameID, $newTitle, $newDesc, $newFormat, $newLowerIsBetter, $newMemString, $leaderboardID, $errorOut);
+        $response['LeaderboardID'] = $leaderboardID;
         $response['Error'] = $errorOut;
         break;
 
