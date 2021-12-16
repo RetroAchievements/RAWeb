ec0013e889dc68e875a0d740904800bc08f97356
Wes Copeland <wlcopeland1@gmail.com>
2021-12-15 19:56:58 -0500

    feat(api): add API_GetAchievementDistribution.php

---
+++ public/API/API_GetAchievementDistribution.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../lib/bootstrap.php';
+
+runPublicApiMiddleware();
+
+$gameID = requestInputQuery('i');
+$hardcore = requestInputQuery('h', 0, 'integer');
+$requestedBy = requestInputQuery('z');
+$flags = requestInputQuery('f', 3, 'integer');
+
+echo json_encode(getAchievementDistribution($gameId, $hardcore, $requestedBy, $flags));
