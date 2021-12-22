d889211adab2a8e09f4ea7197fdfe5ca053d1463
luchaos <luchaos@gmail.com>
2021-12-22 14:19:02 +0100

    Remove orphaned multiexplode function

--- lib/util/string.php
+++ lib/util/string.php
@@ -44,10 +44,3 @@ function rand_string($length)
 
     return $str;
 }
-
-function multiexplode($delimiters, $string)
-{
-    $ready = str_replace($delimiters, $delimiters[0], $string);
-    $launch = explode($delimiters[0], $ready);
-    return $launch;
-}
