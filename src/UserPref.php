319a38c50359cfaf7cb0daab4ce8140984c5989c
Jamiras <jamirasmt@gmail.com>
2022-02-02 19:35:24 -0700

    add mature content flag to websitePrefs

--- src/UserPref.php
+++ src/UserPref.php
@@ -18,7 +18,7 @@ abstract class UserPref
 
     public const EmailOn_Newsletter = 6;
 
-    public const EmailOn_unused2 = 7;
+    public const SiteMsgOff_MatureContent = 7;
 
     public const SiteMsgOn_ActivityComment = 8;
 
@@ -29,4 +29,8 @@ abstract class UserPref
     public const SiteMsgOn_ForumReply = 11;
 
     public const SiteMsgOn_AddFriend = 12;
+
+    public const SiteMsgOn_PrivateMessage = 13;
+
+    public const SiteMsgOn_Newsletter = 14;
 }
