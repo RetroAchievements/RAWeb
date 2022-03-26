f7fb6a293f88f76d6a63fca52ccaef4069bb0f92
luchaos <luchaos@gmail.com>
2022-03-26 20:26:59 +0100

    Use git native core.hooksPath config
    Chores:
    - Add phpstan dev dependecy & fix new phpstan errors
    - Update npm dependencies & run `npm run fix`
    - Update composer dependencies

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,38 +1,24 @@
 <?php
 
-// $header = <<<'EOF'
-// @link https://github.com/organisation
-// @copyright Organisation
-// @license MIT License
-// EOF;
-
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $rules = [
     /*
-     * A variant of Symfony RuleSet that isn't too far from Laravel's StyleCI config
-     * Use PHPStorm's Default and adapt instead of using "Laravel" / "PSR12" / "Symfony 2", those are far too greedy.
-     *
      * RuleSet cascades/overrides:
      * @PhpCsFixer (risky)  https://raw.githubusercontent.com/FriendsOfPHP/PHP-CS-Fixer/2.18/src/RuleSet/Sets/PhpCsFixerSet.php
      *      @Symfony        https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/src/RuleSet/Sets/SymfonySet.php
      *          @PSR12      https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/src/RuleSet/Sets/PSR12Set.php
      *              @PSR2   https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/src/RuleSet/Sets/PSR2Set.php
-     *
-     * Laravel              https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200
      */
     '@Symfony' => true,
 
     // @Symfony overrides
+    'blank_line_before_statement' => false,
     'concat_space' => ['spacing' => 'one'],
-    'phpdoc_summary' => false,
-    'phpdoc_align' => false,
-    'single_line_throw' => false,
-    'yoda_style' => false,
-
-    // @Symfony overrides: Laravel
+    'echo_tag_syntax' => false,
     'increment_style' => ['style' => 'post'],
+    'no_alternative_syntax' => false,
     'no_extra_blank_lines' => [
         'tokens' => [
             'extra',
@@ -40,22 +26,19 @@ $rules = [
             'use',
         ],
     ],
+    'not_operator_with_space' => false,
+    'phpdoc_align' => false,
+    'phpdoc_summary' => false,
+    'single_line_throw' => false,
+    'single_line_comment_spacing' => false,
     'single_line_comment_style' => [
         'comment_types' => ['hash'],
     ],
-
-    // @Symfony overrides: V1
-    'blank_line_before_statement' => false,
-    'echo_tag_syntax' => false,
-    'no_alternative_syntax' => false,
     'phpdoc_separation' => false,
     'phpdoc_to_comment' => false,
     'semicolon_after_instruction' => false,
     'single_quote' => false,
-
-    // additional rules
-    'not_operator_with_space' => false,
-    // 'not_operator_with_successor_space' => true,
+    'yoda_style' => false,
 ];
 
 $project_path = getcwd();
