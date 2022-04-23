5f1ed67771cdde6f6f960237143790d8509af6ee
Jamiras <jamirasmt@gmail.com>
2022-04-23 10:16:35 -0600

    add NumAchievements,NumLeaderboards,Points, and Hashes to API_GetGameList

--- public/API/API_GetGameList.php
+++ public/API/API_GetGameList.php
@@ -11,8 +11,43 @@ if ($consoleID < 0) {
     exit;
 }
 
-$officialFlag = requestInputQuery('f', false, 'boolean');
+$withAchievements = requestInputQuery('f', false, 'boolean');
+$withHashes = requestInputQuery('h', false, 'boolean');
 
-getGamesList($consoleID, $dataOut, $officialFlag);
+getGamesListByDev(null, $consoleID, $dataOut, 1, false, $withAchievements ? 0 : 2);
 
-echo utf8_encode(json_encode($dataOut, JSON_THROW_ON_ERROR));
+$response = [];
+foreach ($dataOut as &$entry) {
+    $responseEntry = [
+        'Title' => $entry['Title'],
+        'ID' => $entry['ID'],
+        'ConsoleID' => $entry['ConsoleID'],
+        'ConsoleName' => $entry['ConsoleName'],
+        'ImageIcon' => $entry['GameIcon'],
+        'NumAchievements' => $entry['NumAchievements'] ?? 0,
+        'NumLeaderboards' => $entry['NumLBs'] ?? 0,
+        'Points' => $entry['MaxPointsAvailable'] ?? 0,
+    ];
+    settype($responseEntry['NumAchievements'], 'integer');
+    settype($responseEntry['NumLeaderboards'], 'integer');
+    settype($responseEntry['Points'], 'integer');
+
+    if ($withHashes) {
+        $responseEntry['Hashes'] = [];
+    }
+
+    $response[] = $responseEntry;
+}
+
+if ($withHashes) {
+    foreach (getMD5List($consoleID) as $hash => $gameID) {
+        foreach ($response as &$entry) {
+            if ($entry['ID'] == $gameID) {
+                $entry['Hashes'][] = $hash;
+                break;
+            }
+        }
+    }
+}
+
+echo json_encode($response, JSON_THROW_ON_ERROR);
