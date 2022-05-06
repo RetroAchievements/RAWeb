b6598ef8eb44ba29b0e857c9a4750255cd6d0cec
Jamiras <jamirasmt@gmail.com>
2022-05-06 11:55:47 -0600

    fix pagination calculations for achievement unlocks

--- public/API/API_GetAchievementUnlocks.php
+++ public/API/API_GetAchievementUnlocks.php
@@ -1,5 +1,35 @@
 <?php
 
+/*
+ *  API_GetAchievementUnlocks
+ *    a : achievement ID
+ *    o : offset - number of entries to skip (default: 0)
+ *    c : count - number of entries to return (default: 50, max: 500)
+ *
+ *  object     Achievement     information about the achievement
+ *   string     ID             unique identifier of the achievement
+ *   string     Title          title of the achievement
+ *   string     Description    description of the achievement
+ *   string     Points         number of points the achievement is worth
+ *   string     TrueRatio      number of "white" points the achievement is worth
+ *   string     Author         user who first created the achievement
+ *   datetime   DateCreated    when the achievement was created
+ *   datetime   DateModified   when the achievement was last modified
+ *  object     Console         information about the console associated to the game associated to the achievemnt
+ *   string     ID             unique identifier of the console
+ *   string     Title          name of the console
+ *  object     Game            information about the game associated to the achievement
+ *   string     ID             unique identifier of the game
+ *   string     Title          name of the game
+ *  int        UnlocksCount    number of times the achievement has been unlocked
+ *  int        TotalPlayers    number of players who have played the game associated to the achievement
+ *  array      Unlocks         requested unlock information
+ *   string     User           user who unlocked the achievement
+ *   string     RAPoints       number of points the user has
+ *   datetime   DateAwarded    when the achievement was unlocked
+ *   string     HardcoreMode   "1" if unlocked in hardcore, otherwise "0"
+ */
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../lib/bootstrap.php';
 
@@ -7,6 +37,8 @@ runPublicApiMiddleware();
 
 $user = null;
 $achievementID = (int) (requestInputQuery('a') ?? null);
+$count = min(requestInputQuery('c', 50), 500);
+$offset = requestInputQuery('o', 0);
 
 if (empty($achievementID)) {
     echo json_encode([
@@ -38,9 +70,7 @@ $console = [
     'Title' => $achievementData['ConsoleName'] ?? null,
 ];
 
-getAchievementWonData($achievementID, $numWinners, $numPossibleWinners, $numRecentWinners, $winnerInfo, $user);
-
-usort($winnerInfo, fn ($a, $b) => strtotime($a['DateAwarded']) - strtotime($b['DateAwarded']));
+getAchievementWonData($achievementID, $numWinners, $numPossibleWinners, $numRecentWinners, $winnerInfo, $user, $offset, $count);
 
 echo json_encode([
     'Achievement' => $achievement,
