f33d652e626fde43ed4ec7701d296c68dc027bd9
Jamiras <32680403+Jamiras@users.noreply.github.com>
2022-05-22 12:31:19 -0600

    document remaining public APIs (#991)

--- public/API/API_GetUserProgress.php
+++ public/API/API_GetUserProgress.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ *  API_GetUserProgress - gets user's High Scores entry for a game
+ *    i : CSV of game ids
+ *    u : username
+ *
+ *  map
+ *   string     [key]                       unique identifier of the game
+ *    string     NumPossibleAchievements    number of core achievements for the game
+ *    string     PossibleScore              maximum number of points that can be earned from the game
+ *    string     NumAchieved                number of achievements earned by the user
+ *    string     ScoreAchieved              number of points earned by the user
+ *    string     NumAchievedHardcore        number of achievements earned by the user in hardcore
+ *    string     ScoreAchievedHardcore      number of points earned by the user in hardcore
+ */
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../lib/bootstrap.php';
 
