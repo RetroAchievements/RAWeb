c771b7e5fd4e3cfc6c0223484b980af30535f4c9
luchaos <luchaos@gmail.com>
2022-05-26 16:12:54 +0200

    Address feedback

--- tests/src/UtilTest.php
+++ tests/src/UtilTest.php
@@ -12,9 +12,9 @@ final class UtilTest extends TestCase
     {
         $expected = ['x', 10, 20, 30, 40, 50];
 
-        $this->assertEquals($expected, separateList("	x,10;20 30|40	50,	"));
+        $this->assertEquals($expected, separateList("\tx,10;20 30|40\t50,\t"));
         $this->assertEquals($expected, separateList('x, 10, 20, 30, 40, 50'));
-        $this->assertEquals($expected, separateList("x	10	20	30	40	50"));
+        $this->assertEquals($expected, separateList("x\t10\t20\t30\t40\t50"));
         $this->assertEquals($expected, separateList('x|10|20|30|40|50'));
         $this->assertEquals($expected, separateList('x;10;20;30;40;50'));
     }
