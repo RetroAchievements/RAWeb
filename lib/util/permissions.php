22eb122801970e417b56bc99622010f9f3e80b08
Jamiras <jamirasmt@gmail.com>
2022-06-02 21:46:21 -0600

    move PermissionsToString into Permissions enum

--- lib/util/permissions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use RA\Permissions;
-
-function PermissionsToString($permissions): string
-{
-    $permissionsStr = ["Spam", "Banned", "Unregistered", "Registered", "Junior Developer", "Developer", "Admin", "Root"];
-    return $permissionsStr[$permissions - (Permissions::Spam)]; // Offset of 0
-}
