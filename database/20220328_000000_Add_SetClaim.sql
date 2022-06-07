181c0b284a9d1832aaec39fd749e52b6f088a14e
MrOwnership <andrewjcomer@yahoo.com>
2022-06-07 02:58:05 -0400

    SQL script cleanup

--- database/20220328_000000_Add_SetClaim.sql
+++ database/20220328_000000_Add_SetClaim.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS `SetClaim` (
   `SetType` int(10) unsigned NOT NULL COMMENT '0 - New set, 1 - Revision',
   `Status` int(10) unsigned NOT NULL COMMENT '0 - Active, 1 - Complete, 2 - Dropped',
   `Extension` int(10) unsigned NOT NULL COMMENT 'Number of times the claim has been extended',
-  `Special` int(10) unsigned NOT NULL COMMENT '0 - Standard claim, 1 - Own Revision, 2 - Free Rollout claim, 3 - Furutre release approved. >=1 does not count against claim count',
+  `Special` int(10) unsigned NOT NULL COMMENT '0 - Standard claim, 1 - Own Revision, 2 - Free Rollout claim, 3 - Future release approved. >=1 does not count against claim count',
   `Created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Timestamp for when the claim was made',
-  `Finished` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Timestamp for when the claim is completed, dropped or will expire',
+  `Finished` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Timestamp for when the claim is completed, dropped or will expire',
   `Updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Timestamp for when the claim was last modified',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
