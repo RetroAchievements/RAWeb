e72557b15e2441ab15f38362c266a8d881117ddb
luchaos <luchaos@gmail.com>
2022-08-22 01:08:40 +0200

    Do not require Vite manifest during testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,7 +24,7 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
 
-        $this->withoutMix();
+        $this->withoutVite();
     }
 
     protected function seedUsers(): void
