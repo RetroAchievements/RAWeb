5f05c2cc4ec64757067b0af1cbc85c69d32eb0bc
luchaos <luchaos@gmail.com>
2022-09-02 00:29:33 +0200

    Change remove avatar request field naming

--- public/request/user/remove-avatar.php
+++ public/request/user/remove-avatar.php
@@ -1,12 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Validator;
 use RA\Permissions;
 
 if (!authenticateFromCookie($actingUser, $permissions, $actingUserDetails, Permissions::Registered)) {
     return back()->withErrors(__('legacy.error.permissions'));
 }
 
-$targetUser = requestInputPost('u');
+$input = Validator::validate(request()->post(), [
+    'user' => 'sometimes|string|exists:mysql_legacy.UserAccounts,User',
+]);
+
+$targetUser = $input['user'];
 
 if ($targetUser && $targetUser !== $actingUser && $permissions < Permissions::Admin) {
     return back()->withErrors(__('legacy.error.permissions'));
