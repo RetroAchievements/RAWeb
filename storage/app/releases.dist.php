33e983ff5c0809800ebdc532410c380fff8df1ae
televandalist <45054151+televandalist@users.noreply.github.com>
2022-09-28 17:20:00 -0500

    Update releases.dist.php

--- storage/app/releases.dist.php
+++ storage/app/releases.dist.php
@@ -115,7 +115,7 @@ Download from <a href="https://retroarch.com">retroarch.com</a>. See <a href="ht
         ],
         101 => [
             'name' => 'PCSX2',
-            'handle' => 'PCSX2
+            'handle' => 'PCSX2',
             'active' => true,
             'link' => 'https://pcsx2.net/guides/basic-setup/',
             'description' => '<strong>NOTE:</strong> The PCSX2 standalone is the only emulator officially supporting achievements for PlayStation 2. Download from <a href="https://pcsx2.net/downloads">pcsx2.net</a>',
