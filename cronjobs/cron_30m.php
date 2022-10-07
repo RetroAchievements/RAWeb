9bfbe245f9a88eeb1b1d0a4222251698bc432f52
luchaos <luchaos@gmail.com>
2022-10-07 22:36:30 +0200

    Remove hourly cron (#1187)

--- cronjobs/cron_30m.php
+++ cronjobs/cron_30m.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Log;
 
 $playersOnline = count(getCurrentlyOnlinePlayers());
 
-Log::info('cron_hourly', ['playersOnline' => $playersOnline]);
+Log::info('cron_30m', ['playersOnline' => $playersOnline]);
 
 file_put_contents(storage_path('logs/playersonline.log'), $playersOnline . PHP_EOL, FILE_APPEND);
--- cronjobs/cron_hourly.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Log;
-
-$playersOnline = count(getCurrentlyOnlinePlayers());
-
-Log::info('cron_hourly', ['playersOnline' => $playersOnline]);
