c2d6468f0543f7786b82d7b032f8fcf2a8522875
Jamiras <jamirasmt@gmail.com>
2022-10-13 06:38:14 -0600

    only show recent players from the last six months

--- lib/database/player-game.php
+++ lib/database/player-game.php
@@ -324,6 +324,7 @@ function getGameRecentPlayers($gameID, $maximum_results = 0): array
     $query = "SELECT ua.ID as UserID, ua.User, ua.RichPresenceMsgDate AS Date, ua.RichPresenceMsg AS Activity
               FROM UserAccounts AS ua
               WHERE ua.LastGameID = $gameID AND ua.Permissions >= " . Permissions::Unregistered . "
+              AND ua.RichPresenceMsgDate > TIMESTAMPADD(MONTH, -6, NOW())
               ORDER BY ua.RichPresenceMsgDate DESC";
 
     if ($maximum_results > 0) {
