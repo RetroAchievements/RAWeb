33ca11ee4f9ac1c52115e9e0dfe2625b67aa9bf5
Jamiras <jamirasmt@gmail.com>
2022-11-02 09:47:11 -0600

    don't try to send email to deleted user when closing ticket

--- lib/database/ticket.php
+++ lib/database/ticket.php
@@ -391,7 +391,9 @@ function updateTicket($user, $ticketID, $ticketVal, $reason = null): bool
 
     addArticleComment("Server", ArticleType::AchievementTicket, $ticketID, $comment, $user);
 
-    getAccountDetails($userReporter, $reporterData);
+    if (!getAccountDetails($userReporter, $reporterData))
+        return true;
+
     $email = $reporterData['EmailAddress'];
 
     $emailTitle = "Ticket status changed";
