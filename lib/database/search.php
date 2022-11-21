2dea07900656577ced9140e0313168af9b8f5b0e
Jamiras <jamirasmt@gmail.com>
2022-11-21 09:55:08 -0700

    allow escaped characters in search query

--- lib/database/search.php
+++ lib/database/search.php
@@ -80,7 +80,7 @@ function performSearch(int $searchType, string $searchQuery, int $offset, int $c
 
     $query = implode(' UNION ALL ', $parts) . " LIMIT $offset, $count";
 
-    $dbResult = s_mysql_query($query);
+    $dbResult = s_mysql_sanitized_query($query);
     if (!$dbResult) {
         log_sql_fail();
 
