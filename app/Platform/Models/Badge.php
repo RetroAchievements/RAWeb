449456861ce9d1c1c96efb0467afd8bec268e5e6
Jamiras <32680403+Jamiras@users.noreply.github.com>
2022-12-09 14:27:16 -0700

    adjust contribution tier thresholds (#1257)
    * adjust contribution tier thresholds
    
    * fix eyes on contribPoints-10 badge
    
    * convert migration to command

--- app/Platform/Models/Badge.php
+++ app/Platform/Models/Badge.php
@@ -8,6 +8,7 @@ use App\Support\Database\Eloquent\BaseModel;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
+use RA\AwardType;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -17,43 +18,40 @@ class Badge extends BaseModel implements HasMedia
     use Searchable;
     use SoftDeletes;
 
-    public static array $developerCountBoundaries = [
-        5,
-        10,
-        50,
+    private static array $developerCountBoundaries = [
         100,
-        200,
-        400,
-        600,
-        800,
-        1000,
-        2000,
-        3000,
-        4000,
-        5000,
-        6000,
-    ];
-
-    public static array $developerPointBoundaries = [
-        100,
-        200,
-        300,
+        250,
         500,
-        800,
         1000,
-        1500,
-        2000,
-        3000,
-        4000,
+        2500,
         5000,
         10000,
-        15000,
-        20000,
-        30000,
-        40000,
+        25000,
         50000,
-        60000,
-        70000,
+        100000,
+        250000,
+        500000,
+        1000000,
+        2500000,
+        5000000,
+    ];
+
+    private static array $developerPointBoundaries = [
+        1000,
+        2500,
+        5000,
+        10000,
+        25000,
+        50000,
+        100000,
+        250000,
+        500000,
+        1000000,
+        2500000,
+        5000000,
+        10000000,
+        25000000,
+        50000000,
     ];
 
     // == search
@@ -131,4 +129,45 @@ class Badge extends BaseModel implements HasMedia
     }
 
     // == scopes
+
+    // == helpers
+
+    private static function getThresholds(int $awardType): ?array
+    {
+        switch ($awardType) {
+            case AwardType::AchievementUnlocksYield:
+                return self::$developerCountBoundaries;
+
+            case AwardType::AchievementPointsYield:
+                return self::$developerPointBoundaries;
+
+            default:
+                return null;
+        }
+    }
+
+    public static function getBadgeThreshold(int $awardType, int $tier): int
+    {
+        $thresholds = self::getThresholds($awardType);
+        if ($thresholds === null)
+            return 0;
+
+        if ($tier < 0 || $tier >= count($thresholds))
+            return 0;
+
+        return $thresholds[$tier];
+    }
+
+    public static function getNewBadgeTier(int $awardType, int $oldValue, int $newValue): ?int
+    {
+        $thresholds = self::getThresholds($awardType);
+        if ($thresholds !== null) {
+            for ($i = count($thresholds) - 1; $i >= 0; $i--) {
+                if ($newValue >= $thresholds[$i] && $oldValue < $thresholds[$i])
+                    return $i;
+            }
+        }
+
+        return null;
+    }
 }
