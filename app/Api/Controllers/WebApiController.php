ae83631fc03bf30fbf5e1d3e7317376cf7dfab1f
luchaos <luchaos@gmail.com>
2022-12-12 02:11:33 +0100

    Clean up API controllers

--- app/Api/Controllers/WebApiController.php
+++ app/Api/Controllers/WebApiController.php
@@ -15,24 +15,10 @@ class WebApiController extends Controller
         abort(405, 'Method not allowed');
     }
 
-    /**
-     * RPC-style endpoint(s) that users' client implementations rely on
-     * An official single-file client is available as download.
-     */
-    public function request(Request $request, string $method): JsonResponse
-    {
-        return response()->json([
-            'success' => false,
-            'method' => $method,
-            'request' => $request->input(),
-        ], 501);
-    }
-
     public function connectServers(Request $request): JsonResponse
     {
-        /*
-         * TODO
-         */
+        // TODO JSON:API response
+
         return response()->json([
             // 'method' => $method,
             'data' => $request->input(),
@@ -41,9 +27,8 @@ class WebApiController extends Controller
 
     public function users(Request $request): JsonResponse
     {
-        /*
-         * TODO
-         */
+        // TODO JSON:API response
+
         return response()->json([
             // 'method' => $method,
             'data' => $request->input(),
--- app/Api/Controllers/WebApiV1Controller.php
+++ app/Api/Controllers/WebApiV1Controller.php
@@ -9,47 +9,17 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 /**
- * TODO: Replaced by [retroachievements/retroachievements-api-php](https://github.com/retroachievements/retroachievements-api-php)
+ * TODO: Implement V1 in [retroachievements/retroachievements-api-php](https://github.com/retroachievements/retroachievements-api-php)
  */
 class WebApiV1Controller extends Controller
 {
-    public function noop(Request $request, ?string $method = null): void
-    {
-        abort(405, 'Method not allowed');
-    }
+    // TODO refactor public/API/API_*
 
-    /**
-     * RPC-style endpoint(s) that users' client implementations rely on
-     * An official single-file client is available as download.
-     */
     public function request(Request $request, string $method): JsonResponse
     {
         return response()->json([
-            'success' => false,
             'method' => $method,
             'request' => $request->input(),
-        ], 501);
-    }
-
-    public function connectServers(Request $request): JsonResponse
-    {
-        /*
-         * TODO
-         */
-        return response()->json([
-            // 'method' => $method,
-            'data' => $request->input(),
-        ], 501);
-    }
-
-    public function users(Request $request): JsonResponse
-    {
-        /*
-         * TODO
-         */
-        return response()->json([
-            // 'method' => $method,
-            'data' => $request->input(),
         ], 501);
     }
 }
