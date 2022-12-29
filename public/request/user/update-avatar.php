464ddf6ce19e38bded93dfb78498087abe390951
Jamiras <jamirasmt@gmail.com>
2022-12-29 13:17:35 -0700

    only send some exception messages to client

--- public/request/user/update-avatar.php
+++ public/request/user/update-avatar.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use RA\Permissions;
 
 if (!authenticateFromCookie($user, $permissions, $userDetails, Permissions::Registered)) {
@@ -9,7 +10,17 @@ if (!authenticateFromCookie($user, $permissions, $userDetails, Permissions::Regi
 try {
     UploadAvatar($user, request()->post('imageData'));
 } catch (Exception $ex) {
-    return response()->json(['message' => $ex->getMessage()], 400);
+    $error = $ex->getMessage();
+    if ($error == 'Invalid file type' || $error == 'File too large') {
+        return response()->json(['message' => $error], 400);
+    }
+
+    if (preg_match('/(not a .* file)/i', $ex->getMessage(), $match)) {
+        return response()->json(['message' => ucfirst($match[0])], 400);
+    }
+
+    Log::error($ex);
+    abort(500);
 }
 
 return response()->json(['message' => __('legacy.success.ok')]);
