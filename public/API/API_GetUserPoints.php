a94ac54637e5e1e3e605e49286557e965af83a0f
Jamiras <jamirasmt@gmail.com>
2023-01-19 12:17:28 -0700

    change response fields to TitleCase

--- public/API/API_GetUserPoints.php
+++ public/API/API_GetUserPoints.php
@@ -4,9 +4,8 @@
  *  API_GetUserPoints
  *    u : username
  *
- *  int        id                      unique identifier of the user
- *  int        points                  number of hardcore points the user has
- *  int        softcorePoints          number of softcore points the user has
+ *  int        Points                  number of hardcore points the user has
+ *  int        SoftcorePoints          number of softcore points the user has
  */
 
 $user = request()->query('u');
@@ -21,7 +20,6 @@ if (!$userDetails) {
 }
 
 return response()->json(array_map('intval', [
-    'id' => $userDetails['ID'],
-    'points' => $userDetails['RAPoints'],
-    'softcorePoints' => $userDetails['RASoftcorePoints'],
+    'Points' => $userDetails['RAPoints'],
+    'SoftcorePoints' => $userDetails['RASoftcorePoints'],
 ]));
