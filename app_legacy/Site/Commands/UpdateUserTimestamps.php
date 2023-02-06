de3536a44241efcad438c501442c541016b08f9d
luchaos <luchaos@gmail.com>
2023-02-06 21:15:17 +0100

    Cross-reference creation dates with forum topic comments and comments

--- app_legacy/Site/Commands/UpdateUserTimestamps.php
+++ app_legacy/Site/Commands/UpdateUserTimestamps.php
@@ -6,6 +6,7 @@ namespace LegacyApp\Site\Commands;
 
 use Eloquent;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use LegacyApp\Site\Models\User;
 
@@ -16,6 +17,13 @@ class UpdateUserTimestamps extends Command
     protected $description = 'Calculates missing and/or incorrect timestamps for users';
 
     public function handle(): void
+    {
+        $this->updateInitialAccountCreationDates();
+
+        $this->interpolateAccountCreationDates();
+    }
+
+    private function updateInitialAccountCreationDates(): void
     {
         // Scott's account was created on the day of the domain registration
         User::where('User', 'Scott')
@@ -59,21 +67,70 @@ class UpdateUserTimestamps extends Command
                 'Updated' => DB::raw('Updated'),
             ]);
 
-        // Iterate through user accounts by ID, descending, fill gaps and correct order of timestamps.
+        $this->line('Updated initial account creation timestamps');
+    }
+
+    /**
+     * Iterate through user accounts by ID, descending, fill gaps and correct order of timestamps.
+     */
+    private function interpolateAccountCreationDates(): void
+    {
         Eloquent::unguard();
-        $maxId = User::whereNull('Created')->max('ID') + 1; // 106605
+        // 106605, created on 2019-09-22 is the first user to have a real creation timestamp
+        $maxId = 106605;
+
+        $this->line('Fetching forum topic comment timestamps...');
+        $forumTopicTimestamps = User::withTrashed()
+            ->selectRaw('UserAccounts.ID, UserAccounts.User, UserAccounts.Created, MIN(ForumTopicComment.DateCreated) ForumTopicTimestamp')
+            ->leftJoin('ForumTopicComment', 'Author', '=', 'UserAccounts.User')
+            ->where('UserAccounts.ID', '<=', $maxId)
+            ->whereRaw('ForumTopicComment.DateCreated < UserAccounts.Created')
+            ->groupByRaw('UserAccounts.ID, UserAccounts.User, UserAccounts.Created')
+            ->orderByDesc('UserAccounts.ID')
+            ->get()
+            ->keyBy('ID');
+        $this->info('Found ' . $forumTopicTimestamps->count() . ' earlier forum topic comment timestamps');
+
+        $this->line('Fetching comment timestamps...');
+        $commentTimestamps = User::withTrashed()
+            ->selectRaw('UserAccounts.ID, UserAccounts.User, UserAccounts.Created, MIN(Comment.Submitted) CommentTimestamp')
+            ->leftJoin('Comment', 'UserID', '=', 'UserAccounts.ID')
+            ->where('UserAccounts.ID', '<=', $maxId)
+            ->whereRaw('Comment.Submitted < UserAccounts.Created')
+            ->groupByRaw('UserAccounts.ID, UserAccounts.User, UserAccounts.Created')
+            ->orderByDesc('UserAccounts.ID')
+            ->get()
+            ->keyBy('ID');
+        $this->info('Found ' . $commentTimestamps->count() . ' earlier comment timestamps');
+
+        $this->line('Interpolate account creation timestamps...');
         $users = User::withTrashed()
             ->where('ID', '<=', $maxId)
             ->orderByDesc('ID')
             ->get(['ID', 'User', 'Created']);
+
         $progressBar = $this->output->createProgressBar($users->count());
         $progressBar->start();
         $timestampMemento = null;
         $updated = 0;
         /** @var User $user */
         foreach ($users as $user) {
+            $timestampMemento ??= $user->Created;
+
+            // Take earlier forum topic comment timestamp if exists
+            if ($forumTopicTimestamps->has($user->ID)) {
+                $forumTopicTimestamp = $forumTopicTimestamps->get($user->ID)['ForumTopicTimestamp'];
+                $timestampMemento = $timestampMemento->isAfter($forumTopicTimestamp) ? Carbon::parse($forumTopicTimestamp) : $timestampMemento;
+            }
+
+            // Take earlier comment timestamp if exists
+            if ($commentTimestamps->has($user->ID)) {
+                $commentTimestamp = $commentTimestamps->get($user->ID)['CommentTimestamp'];
+                $timestampMemento = $timestampMemento->isAfter($commentTimestamp) ? Carbon::parse($commentTimestamp) : $timestampMemento;
+            }
+
             // Apply previous timestamp if it's earlier than the current or no Created timestamp exists.
-            if ($timestampMemento && (!$user->Created || $user->Created > $timestampMemento)) {
+            if (!$user->Created || $user->Created > $timestampMemento) {
                 // $this->line('Update ' . $user->User . ' from "' . $user->Created . '" to "' . $timestampMemento . '"');
                 $user->update([
                     'Created' => $timestampMemento,
@@ -86,6 +143,6 @@ class UpdateUserTimestamps extends Command
         }
         $progressBar->finish();
 
-        $this->line('Updated ' . $updated . ' timestamps');
+        $this->info(PHP_EOL . 'Updated ' . $updated . ' account creation timestamps');
     }
 }
