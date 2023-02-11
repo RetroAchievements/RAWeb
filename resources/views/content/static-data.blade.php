5997dbc4c99b1922085132c0c23a3c00d444ea3c
Wes Copeland <wlcopeland1@gmail.com>
2023-02-10 19:49:47 -0500

    feat(home): add number separators to static data

--- resources/views/content/static-data.blade.php
+++ resources/views/content/static-data.blade.php
@@ -34,16 +34,16 @@ if ($lastRegisteredUser == null) {
     <h3>Statistics</h3>
     <div class="infobox">
         There are
-        <a title="Achievement List" href="/gameList.php?s=2">{{ $numAchievements }}</a>
+        <a title="Achievement List" href="/gameList.php?s=2">{{ number_format($numAchievements) }}</a>
         achievements registered for
-        <a title="Game List" href="/gameList.php?s=1">{{ $numGames }}</a> games.
-        <a title="Achievement List" href="/achievementList.php">{{ $numAwarded }}</a>
+        <a title="Game List" href="/gameList.php?s=1">{{ number_format($numGames) }}</a> games.
+        <a title="Achievement List" href="/achievementList.php">{{ number_format($numAwarded) }}</a>
         achievements have been awarded to the
-        <a title="User List" href="/userList.php">{{ $numRegisteredPlayers }}</a>
+        <a title="User List" href="/userList.php">{{ number_format($numRegisteredPlayers) }}</a>
         registered players (average: {{ $avAwardedPerPlayer }} per player)<br>
         <br>
         Since 2nd March 2013, a total of
-        <span title="Awesome!"><strong>{{ $totalPointsEarned }}</strong></span>
+        <span title="Awesome!"><strong>{{ number_format($totalPointsEarned) }}</strong></span>
         points have been earned by users on RetroAchievements.org.<br>
         <br>
         @if($staticData->lastRegisteredUser)
