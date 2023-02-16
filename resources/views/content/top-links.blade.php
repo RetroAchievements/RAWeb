fb2ea8c886448d3a7dee62a99099cb04946bbc76
EmoonX <gustavo.chicato@usp.br>
2023-02-15 23:21:47 -0300

    Smaller text for topmost buttons

--- resources/views/content/top-links.blade.php
+++ resources/views/content/top-links.blade.php
@@ -1,20 +1,20 @@
 <x-section class="mb-3 flex flex-col">
-    <a class="btn flex justify-center gap-1.5 p-3" href="/globalRanking.php?s=5&t=2">
+    <a class="btn flex justify-center gap-1.5 p-3 text-xs" href="/globalRanking.php?s=5&t=2">
         <span class="text-yellow-500"><x-fas-medal/></span>
         Global Ranking
     </a>
-    <a class="btn flex justify-center gap-1.5 p-3" href="{{ route('download.index') }}">
-        <span class="text-blue-400"><x-fas-gamepad/></span>
+    <a class="btn flex justify-center gap-1.5 p-3 text-xs" href="{{ route('download.index') }}">
+        <span class="text-purple-400"><x-fas-gamepad/></span>
         Download Emulator
     </a>
     @if(config('services.discord.invite_id'))
-        <a class="btn flex justify-center gap-1.5 p-3" href="https://discord.gg/{{ config('services.discord.invite_id') }}">
+        <a class="btn flex justify-center gap-1.5 p-3 text-xs" href="https://discord.gg/{{ config('services.discord.invite_id') }}">
             <span class="text-discord"><x-fab-discord/></span>
             Join us on Discord
         </a>
     @endif
     @if(config('services.patreon.user_id'))
-        <a class="btn flex justify-center gap-1.5 p-3" href="https://www.patreon.com/bePatron?u={{ config('services.patreon.user_id') }}">
+        <a class="btn flex justify-center gap-1.5 p-3 text-xs" href="https://www.patreon.com/bePatron?u={{ config('services.patreon.user_id') }}">
             <span class="text-patreon"><x-fab-patreon/></span>
             Become a Patron
         </a>
