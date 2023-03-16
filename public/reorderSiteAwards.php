20e0be56adec32851915d2a6288f95b16530d3d4
wescopeland <wlcopeland1@gmail.com>
2023-03-15 20:21:50 -0400

    fix(reorderSiteAwards): require save button press to update

--- public/reorderSiteAwards.php
+++ public/reorderSiteAwards.php
@@ -10,30 +10,47 @@ if (!authenticateFromCookie($user, $permissions, $userDetails, Permissions::Regi
 RenderContentStart("Reorder Site Awards");
 ?>
 <script>
-function updateAwardDisplayOrder(awardType, awardData, awardDataExtra, objID) {
-    var inputText = $('#' + objID).val();
-    var inputNum = Math.max(-1, Math.min(Number(inputText), 10000));
+function updateAllAwardsDisplayOrder() {
     showStatusMessage('Updating...');
-    $.post('/request/user/update-site-award.php', {
-        type: awardType,
-        data: awardData,
-        extra: awardDataExtra,
-        number: inputNum,
+
+    const awards = [];
+
+    $('.displayorderedit').each(function (index, element) {
+        const row = $(element).closest('tr');
+        const awardType = row.find("input[type='hidden'][name='type']").val();
+        const awardData = row.find("input[type='hidden'][name='data']").val();
+        const awardDataExtra = row.find("input[type='hidden'][name='extra']").val();
+        const displayOrder = parseInt($(element).val(), 10);
+
+        awards.push({
+            type: awardType,
+            data: awardData,
+            extra: awardDataExtra,
+            number: displayOrder
+        });
     });
+
+    $.post('/request/user/update-site-awards.php', { awards })
+        .done(function (response) {
+            showStatusMessage('Awards updated successfully');
+            $('#rightcontainer').html(response.updatedAwardsHTML);
+        })
+        .fail(function () {
+            showStatusMessage('Error updating awards');
+        });
 }
 </script>
 <div id="mainpage">
     <div id="leftcontainer">
         <?php
         echo "<h2>Reorder Site Awards</h2>";
-        echo "<a class='btn btn-link' href='/reorderSiteAwards.php'>Refresh Page</a><br>";
 
-        echo "<p class='embedded'><b>Instructions:</b> These are your site awards as displayed on your user page. " .
-            "The awards will be ordered by 'Display Order', the column found on the right, in order from smallest to greatest. " .
-            "Adjust the numbers on the right to set an order for them to appear in. Setting a 'Display Order' value to -1 " .
-            "will hide the site award. Any changes you make on this page will instantly " .
-            "take effect on the website, but you will need to press 'Refresh Page' to see the new order on this page. " .
-            "The right panel represents how the site awards will look on your user page.</p>";
+        echo "<p class='embedded'>";
+        echo "To rearrange your site awards, adjust the 'Display Order' value in the rightmost column of " .
+            "each award row. The awards will appear on your user page in ascending order according to their " .
+            "'Display Order' values. To hide an award, set its 'Display Order' value to -1. Don't forget to save " .
+            "your changes by clicking the 'Save' button. Your updates will be immediately reflected on your user page.";
+        echo "</p>";
 
         $userAwards = getUsersSiteAwards($user, true);
 
@@ -42,7 +59,7 @@ function updateAwardDisplayOrder(awardType, awardData, awardDataExtra, objID) {
         function RenderAwardOrderTable(string $title, array $awards, int &$counter): void
         {
             echo "<br><h4>$title</h4>";
-            echo "<table class='table-highlight'><tbody>";
+            echo "<table class='table-highlight mb-2'><tbody>";
 
             echo "<tr class='do-not-highlight'>";
             echo "<th>Badge</th>";
@@ -50,6 +67,9 @@ function updateAwardDisplayOrder(awardType, awardData, awardDataExtra, objID) {
             echo "<th width=\"25%\">Award Date</th>";
             echo "<th>Display Order</th>";
             echo "</tr>\n";
+
+            // "Game Awards" -> "game"
+            $humanReadableAwardType = strtolower(strtok($title, " "));
 
             foreach ($awards as $award) {
                 $awardType = $award['AwardType'];
@@ -61,30 +81,39 @@ function updateAwardDisplayOrder(awardType, awardData, awardDataExtra, objID) {
 
                 sanitize_outputs(
                     $awardTitle,
-                    $awardGameConsole
+                    $awardGameConsole,
+                    $awardType,
+                    $awardData,
+                    $awardDataExtra,
                 );
 
                 if ($awardType == AwardType::AchievementUnlocksYield) {
                     $awardTitle = "Achievements Earned by Others";
                 } elseif ($awardType == AwardType::AchievementPointsYield) {
                     $awardTitle = "Achievement Points Earned by Others";
-                // } elseif ($awardType == AwardType::Referrals) {
-                //     $awardTitle = "Referral Award";
                 } elseif ($awardType == AwardType::PatreonSupporter) {
                     $awardTitle = "Patreon Supporter";
                 }
 
+                echo "<tr>";
                 echo "<td>";
                 RenderAward($award, 48, false);
                 echo "</td>";
                 echo "<td>$awardTitle</td>";
                 echo "<td style=\"white-space: nowrap\"><span class='smalldate'>$awardDate</span><br></td>";
-                echo "<td><input class='displayorderedit' id='$counter' type='text' value='$awardDisplayOrder' onchange=\"updateAwardDisplayOrder('$awardType', '$awardData', '$awardDataExtra', '$counter')\" size='3' /></td>";
+                echo "<td><input class='displayorderedit' data-award-type='$humanReadableAwardType' id='$counter' type='text' value='$awardDisplayOrder' size='3' /></td>";
+                echo "<input type='hidden' name='type' value='$awardType'>";
+                echo "<input type='hidden' name='data' value='$awardData'>";
+                echo "<input type='hidden' name='extra' value='$awardDataExtra'>";                
 
                 echo "</tr>\n";
                 $counter++;
             }
             echo "</tbody></table>\n";
+
+            echo "<div class='flex w-full justify-end'>";
+            echo "<button onclick='updateAllAwardsDisplayOrder()'>Save</button>";
+            echo "</div>";
         }
 
         $counter = 0;
---
+++ public/request/user/update-site-awards.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+use LegacyApp\Community\Enums\AwardType;
+
+if (!authenticateFromCookie($user, $permissions, $userDetails)) {
+    abort(401);
+}
+
+$input = Validator::validate(Arr::wrap(request()->post()), [
+    'awards.*.type' => 'required|integer',
+    'awards.*.data' => 'required|integer',
+    'awards.*.extra' => 'required|integer',
+    'awards.*.number' => 'required|integer',
+]);
+
+$awards = $input['awards'];
+
+foreach ($awards as $award) {
+    $awardType = $award['type'];
+    $awardData = $award['data'];
+    $awardDataExtra = $award['extra'];
+    $value = $award['number'];
+
+    // Change display order for all entries if it's a "stacking" award type.
+    if (in_array($awardType, [AwardType::AchievementUnlocksYield, AwardType::AchievementPointsYield])) {
+        $query = "UPDATE SiteAwards SET DisplayOrder = $value WHERE User = '$user' " .
+            "AND AwardType = $awardType " .
+            "AND AwardDataExtra = $awardDataExtra";
+    } else {
+        $query = "UPDATE SiteAwards SET DisplayOrder = $value WHERE User = '$user' " .
+            "AND AwardType = $awardType " .
+            "AND AwardData = $awardData";
+    }
+
+    if (!s_mysql_query($query)) {
+        abort(500);
+    }
+}
+
+$userAwards = getUsersSiteAwards($user);
+$updatedAwardsHTML = '';
+ob_start();
+RenderSiteAwards($userAwards);
+$updatedAwardsHTML = ob_get_clean();
+
+return response()->json([
+    'message' => __('legacy.success.ok'),
+    'updatedAwardsHTML' => $updatedAwardsHTML
+]);
