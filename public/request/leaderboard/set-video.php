15be77b522c33b1cec14da3a6a4a544b89be490c
cIymax <94894395+cIymax@users.noreply.github.com>
2023-03-25 00:30:48 -0400

    Create set-video.php

---
+++ public/request/leaderboard/set-video.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+use LegacyApp\Community\Enums\ArticleType;
+use LegacyApp\Site\Enums\Permissions;
+
+if (!authenticateFromCookie($user, $permissions, $userDetails, Permissions::Registered)) {
+    return back()->withErrors(__('legacy.error.permissions'));
+}
+
+$input = Validator::validate(Arr::wrap(request()->post()), [
+    'user' => 'required|string|exists:mysql_legacy.UserAccounts,User',
+    'leaderboard' => 'required|integer|exists:mysql_legacy.LeaderboardDef,ID',
+    'video' => 'nullable|string|max:50',
+]);
+
+$leaderboardId = (int) $input['leaderboard'];
+$targetUser = $input['user'];
+$video = $input['video'];
+
+// You can only set video links for your own leaderboard entries.
+if ($user !== $targetUser) {
+    return back()->withErrors(__('legacy.error.permissions'));
+}
+
+if (setLeaderboardEntryVideo($targetUser, $leaderboardId, $video)) {
+    return back()->with('success', __('legacy.success.ok'));
+}
+
+return back()->withErrors(__('legacy.error.error'));
