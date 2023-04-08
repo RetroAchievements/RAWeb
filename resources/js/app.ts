da3e9c56d775919b97103f78e657ef81adc88d86
Wes Copeland <wlcopeland1@gmail.com>
2023-04-08 11:45:48 -0500

    refactor(all.js): begin migration to TypeScript (#1420)
    * refactor: begin converting all.js to typescript
    
    * Revert "refactor: begin converting all.js to typescript"
    
    This reverts commit ae8acc6b6a9a7698c54bbbdf8c95ed636483e3f3.
    
    * refactor: begin migrating all.js to typescript
    
    * refactor: migrate more of all.js

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,8 +2,14 @@ import Alpine from 'alpinejs';
 // eslint-disable-next-line camelcase,import/no-unresolved
 // import { livewire_hot_reload } from 'virtual:livewire-hot-reload';
 
+import {
+  copyToClipboard,
+  handleLeaderboardTabClick,
+  injectShortcode,
+  mobileSafeTipEvents,
+  themeChange
+} from './utils';
 import { lazyLoadModuleOnIdFound } from './lazyLoadModuleOnIdFound';
-import { clipboard, mobileSafeTipEvents, themeChange } from './utils';
 
 // livewire_hot_reload();
 
@@ -13,11 +19,14 @@ lazyLoadModuleOnIdFound({
   moduleName: 'reorderSiteAwards',
 });
 
+window.copyToClipboard = copyToClipboard;
+window.handleLeaderboardTabClick = handleLeaderboardTabClick;
+window.injectShortcode = injectShortcode;
 window.mobileSafeTipEvents = mobileSafeTipEvents;
 
+// Alpine needs to be placed after all `window` injection
+// or race conditions could occur.
 window.Alpine = Alpine;
 Alpine.start();
-
-window.clipboard = clipboard;
 
 themeChange();
--- resources/js/utils/helpers.ts
+++ resources/js/utils/helpers.ts
@@ -1,11 +1,3 @@
 export const config = (key: string) => window?.cfg?.[key];
 
 export const asset = (uri: string) => `${config('assetsUrl')}${uri}`;
-
-export const clipboard = (text: string) => {
-  navigator.clipboard.writeText(text).then(() => {
-    if (window.showStatusSuccess) {
-      window.showStatusSuccess('Copied!');
-    }
-  });
-};
