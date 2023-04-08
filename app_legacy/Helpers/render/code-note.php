c26ea21eda20c06dbbcfb0a01c039b45fb28b49f
Wes Copeland <wlcopeland1@gmail.com>
2023-04-08 09:21:59 -0500

    feat: allow devs to edit code notes on the website (#1440)

--- app_legacy/Helpers/render/code-note.php
+++ app_legacy/Helpers/render/code-note.php
@@ -1,41 +1,75 @@
 <?php
 
-function RenderCodeNotes(array $codeNotes): void
+use LegacyApp\Site\Enums\Permissions;
+
+function RenderCodeNotes(array $codeNotes, ?string $editingUser = null, ?int $editingPermissions = null): void
 {
-    echo "<table class='table-highlight'><tbody>";
+    $isEditable = $editingUser && $editingPermissions >= Permissions::JuniorDeveloper;
 
-    echo "<tr class='do-not-highlight'><th style='font-size:100%;'>Mem</th><th style='font-size:100%;'>Note</th><th style='font-size:100%;'>Author</th></tr>";
+    echo "<table class='table-highlight'>";
 
+    echo "<thead>";
+    echo "<tr class='do-not-highlight'>";
+    echo "<th style='font-size:100%;'>Mem</th>";
+    echo "<th style='font-size:100%;'>Note</th>";
+    echo "<th style='font-size:100%;'>Author</th>";
+    if ($isEditable) {
+        echo "<th>Dev</th>";
+    }
+    echo "</tr>";
+    echo "</thead>";
+
+    echo "<tbody>";
+
+    $rowIndex = 0;
     foreach ($codeNotes as $nextCodeNote) {
         if (empty(trim($nextCodeNote['Note'])) || $nextCodeNote['Note'] == "''") {
             continue;
         }
 
-        echo "<tr>";
+        $canEditNote = (
+            $editingPermissions >= Permissions::Developer
+            || ($editingPermissions === Permissions::JuniorDeveloper && $nextCodeNote['User'] === $editingUser)
+        );
+
+        echo "<tr id='row-$rowIndex'>";
 
         $addr = $nextCodeNote['Address'];
         $addrInt = hexdec($addr);
 
         $addrFormatted = sprintf("%04x", $addrInt);
-        $memNote = $nextCodeNote['Note'];
+        $originalMemNote = $nextCodeNote['Note'];
 
-        sanitize_outputs($memNote);
+        sanitize_outputs($originalMemNote);
 
-        $memNote = nl2br($memNote);
+        $memNote = nl2br($originalMemNote);
 
-        echo "<td style='width: 25%;'>";
+        echo "<td data-address='$addr' style='width: 25%;'>";
         echo "<span class='font-mono'>0x$addrFormatted</span>";
         echo "</td>";
 
         echo "<td>";
-        echo "<div class='font-mono' style='word-break:break-word'>$memNote</div>";
+        echo "<div class='font-mono note-display block' style='word-break:break-word'>$memNote</div>";
+        echo "<textarea class='w-full font-mono note-edit hidden'>$originalMemNote</textarea>";
+        echo "<button class='mt-[6px] save-btn hidden' onclick='saveCodeNote($rowIndex)'>Save</button>";
         echo "</td>";
 
-        echo "<td>";
+        echo "<td class='note-author-avatar' data-current-author='" . $nextCodeNote['User'] . "'>";
         echo userAvatar($nextCodeNote['User'], label: false, iconSize: 24);
         echo "</td>";
 
+        if ($canEditNote) {
+            echo "<td>";
+            echo "<button class='edit-btn inline' onclick='beginEditMode($rowIndex)'>Edit</button>";
+            echo "<button class='cancel-btn hidden' onclick='cancelEditMode($rowIndex)'>Cancel</button>";
+            echo "</td>";
+        } elseif ($isEditable) {
+            echo "<td></td>";
+        }
+
         echo "</tr>";
+
+        $rowIndex++;
     }
 
     echo "</tbody></table>";
