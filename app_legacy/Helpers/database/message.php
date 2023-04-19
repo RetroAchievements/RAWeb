c13b15085a6d187fb5e77f92503e7ce80fb23f9d
wescopeland <wlcopeland1@gmail.com>
2023-04-19 09:02:55 -0400

    fix(messages): remediate deleted unread messages not affecting message count

--- app_legacy/Helpers/database/message.php
+++ app_legacy/Helpers/database/message.php
@@ -227,6 +227,10 @@ function DeleteMessage(string $user, int $messageID): bool
         return false;
     }
 
+    // If an unread message is deleted by the user, the total unread
+    // messages count displayed on the site will not be updated correctly.
+    markMessageAsRead($user, $messageID);
+
     $query = "DELETE FROM Messages WHERE Messages.ID = $messageID";
     $dbResult = s_mysql_query($query);
     if ($dbResult !== false) {
