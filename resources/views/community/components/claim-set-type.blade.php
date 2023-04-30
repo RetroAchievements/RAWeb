2d7bd2b583a9d51cdd80ffc619f6f84195100291
EmoonX <gustavo.chicato@usp.br>
2023-04-29 21:29:31 -0300

    Checkmarks for icons?

--- resources/views/community/components/claim-set-type.blade.php
+++ resources/views/community/components/claim-set-type.blade.php
@@ -5,9 +5,9 @@ use LegacyApp\Community\Enums\ClaimSetType;
 
 <div class="flex items-center gap-1 text-xs">
 @if ($claim['SetType'] == ClaimSetType::NewSet)    
-    <x-pixelarticons-lock-open class="w-5 h-5" />
+    <x-pixelarticons-check class="w-5 h-5 text-lime-500" />
 @else
-    <x-pixelarticons-lock-open class="w-5 h-5" />
+    <x-pixelarticons-check-double class="w-5 h-5 text-orange-500" />
 @endif
     <span>{{ ClaimSetType::toString($claim['SetType']) }}</span>
 </div>
