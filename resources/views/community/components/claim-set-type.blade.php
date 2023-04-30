812f5083ff30462ae495f32aa5237e1a0b5d14cb
EmoonX <gustavo.chicato@usp.br>
2023-04-29 21:16:08 -0300

    Better code with Blade conditionals

--- resources/views/community/components/claim-set-type.blade.php
+++ resources/views/community/components/claim-set-type.blade.php
@@ -1,16 +1,13 @@
 <?php
 
 use LegacyApp\Community\Enums\ClaimSetType;
-
-[$claimSetTypeStr, $claimSetTypeIcon] = ['', ''];
-if ($claim['SetType'] === ClaimSetType::NewSet) {
-    $claimSetTypeStr = ClaimSetType::toString(ClaimSetType::NewSet);
-} else {
-    $claimSetTypeStr = ClaimSetType::toString(ClaimSetType::Revision);
-}
 ?>
 
 <div class="flex items-center gap-1 text-xs">
+@if ($claim['SetType'] == ClaimSetType::NewSet)    
     <x-pixelarticons-lock-open class="w-5 h-5" />
-    <span>{{ $claimSetTypeStr }}</span>
+@else
+    <x-pixelarticons-lock-open class="w-5 h-5" />
+@endif
+    <span>{{ ClaimSetType::toString($claim['SetType']) }}</span>
 </div>
