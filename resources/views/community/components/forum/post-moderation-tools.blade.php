76f15f96b8d04b6484fd2a71abf18e50d0f9f10e
wescopeland <wlcopeland1@gmail.com>
2023-04-30 12:56:00 -0400

    chore: lint

--- resources/views/community/components/forum/post-moderation-tools.blade.php
+++ resources/views/community/components/forum/post-moderation-tools.blade.php
@@ -1,5 +1,7 @@
 <?php
 use LegacyApp\Community\Enums\UserAction;
+
+$updateForumPostPermissions = UserAction::UpdateForumPostPermissions;
 ?>
 
 @props([
@@ -12,7 +14,7 @@ use LegacyApp\Community\Enums\UserAction;
     onsubmit="return confirm('Authorise this user and all their posts?')"
 >
     {{ csrf_field() }}
-    <input type='hidden' name='property' value="{{ UserAction::UpdateForumPostPermissions }}" />
+    <input type='hidden' name='property' value="{{ $updateForumPostPermissions }}" />
     <input type='hidden' name='target' value="{{ $commentAuthor }}" />
     <input type='hidden' name='value' value='1' />
     <button class='btn p-1 lg:text-xs'>Authorise</button>
@@ -24,7 +26,7 @@ use LegacyApp\Community\Enums\UserAction;
     onsubmit="return confirm('Permanently Block (spam)?')"
 >
     {{ csrf_field() }}
-    <input type='hidden' name='property' value="{{ UserAction::UpdateForumPostPermissions }}" />
+    <input type='hidden' name='property' value="{{ $updateForumPostPermissions }}" />
     <input type='hidden' name='target' value="{{ $commentAuthor }}" />
     <input type='hidden' name='value' value='0' />
     <button class='btn btn-danger p-1 lg:text-xs'>Block</button>
