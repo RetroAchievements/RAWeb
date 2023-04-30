9b54121ca5fde866d8fb3998eb1b2d0a19a9595c
EmoonX <gustavo.chicato@usp.br>
2023-04-30 12:59:32 -0300

    Swap orange for cyan and brighter colors

--- resources/views/community/components/claim-set-type.blade.php
+++ resources/views/community/components/claim-set-type.blade.php
@@ -4,9 +4,9 @@ use LegacyApp\Community\Enums\ClaimSetType;
 
 <div class="flex items-center gap-1 text-xs">
 @if ($claimSetType == ClaimSetType::NewSet)
-    <x-pixelarticons-check class="w-5 h-5 text-lime-500" />
+    <x-pixelarticons-check class="w-5 h-5 text-lime-400" />
 @else
-    <x-pixelarticons-check-double class="w-5 h-5 text-amber-600" />
+    <x-pixelarticons-check-double class="w-5 h-5 text-cyan-300" />
 @endif
     <span>{{ ClaimSetType::toString($claimSetType) }}</span>
 </div>
