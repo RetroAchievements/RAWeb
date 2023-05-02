9149fec52f7dc5be2b9c7c3cbf08bbd8e910cfa5
wescopeland <wlcopeland1@gmail.com>
2023-05-02 19:52:56 -0400

    fix: disable checkbox autocomplete

--- resources/views/platform/components/game/hide-earned-checkbox.blade.php
+++ resources/views/platform/components/game/hide-earned-checkbox.blade.php
@@ -2,6 +2,7 @@
     <label class="flex items-center gap-x-1">
         <input 
             type="checkbox"
+            autocomplete="off"
             @change="toggleUnlockedRows"
         >
             Hide unlocked achievements
