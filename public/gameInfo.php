c479e50b2ee5348ecbc0a57dc0b02b930722db19
wescopeland <wlcopeland1@gmail.com>
2023-05-06 20:25:42 -0400

    fix(game): rename Collections to Hubs

--- public/gameInfo.php
+++ public/gameInfo.php
@@ -1438,7 +1438,7 @@ sanitize_outputs(
             }
 
             if (!empty($gameHubs)) {
-                RenderGameAlts($gameHubs, 'Collections');
+                RenderGameAlts($gameHubs, 'Hubs');
             }
 
             if ($user !== null && $numAchievements > 0) {
