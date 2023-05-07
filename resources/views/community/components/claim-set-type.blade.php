39f0636da721d5e268f181038604f0a18d7d62e5
EmoonX <gustavo.chicato@usp.br>
2023-05-07 18:29:07 -0300

    Small decrease in icon size

--- resources/views/community/components/claim-set-type.blade.php
+++ resources/views/community/components/claim-set-type.blade.php
@@ -4,9 +4,9 @@ use LegacyApp\Community\Enums\ClaimSetType;
 
 <div class="flex items-center gap-1 text-xs">
 @if ($claimSetType == ClaimSetType::NewSet)
-    <x-pixelarticons-card-plus class="w-4 h-4 text-green-500" />
+    <x-pixelarticons-card-plus class="w-4 h-3 text-green-500" />
 @else
-    <x-pixelarticons-edit class="w-4 h-4 text-cyan-400" />
+    <x-pixelarticons-edit class="w-4 h-3 text-cyan-400" />
 @endif
     <span>{{ ClaimSetType::toString($claimSetType) }}</span>
 </div>
