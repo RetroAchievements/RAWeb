8db2a68f6601e5de7adc9aa70c53a6aaecad2069
wescopeland <wlcopeland1@gmail.com>
2023-05-10 19:40:10 -0400

    fix: remediate templating issues

--- resources/views/community/components/request-list/meta-request-status-filter.blade.php
+++ resources/views/community/components/request-list/meta-request-status-filter.blade.php
@@ -1,41 +1,17 @@
 <label class="text-xs font-bold md:-mb-6">Request status</label>
 <div class="space-x-6 flex" id="filter-by-request-status">
     <label class="cursor-pointer flex items-center gap-x-1 text-xs">
-        <input
-            type="radio"
-            id="all-requests"
-            name="request-status"
-            value="0"
-            {{ !$selectedRequestStatus ? 'checked' : '' }}
-            @change="handleRequestStatusChanged
-        >
-
+        <input type="radio" id="all-requests" name="request-status" value="0" {{ !$selectedRequestStatus ? 'checked' : '' }} @change="handleRequestStatusChanged">
         All
     </label>
 
     <label class="cursor-pointer flex items-center gap-x-1 text-xs">
-        <input 
-            type="radio"
-            id="all-requests"
-            name="request-status"
-            value="1"
-            {{ $selectedRequestStatus == 1 ? 'checked' : '' }}
-            @change="handleRequestStatusChanged
-        >
-
+        <input type="radio" id="all-requests" name="request-status" value="1" {{ $selectedRequestStatus == 1 ? 'checked' : '' }} @change="handleRequestStatusChanged">
         Claimed
     </label>
 
     <label class="cursor-pointer flex items-center gap-x-1 text-xs">
-        <input
-            type="radio"
-            id="all-requests"
-            name="request-status"
-            value="2"
-            {{ $selectedRequestStatus == 2 ? 'checked' : '' }}
-            @change="handleRequestStatusChanged"
-        >
-        
+        <input type="radio" id="all-requests" name="request-status" value="2" {{ $selectedRequestStatus == 2 ? 'checked' : '' }} @change="handleRequestStatusChanged">
         Unclaimed
     </label>
 </div>
