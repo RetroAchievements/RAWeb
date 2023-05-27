084dd9538d1845ef0fd87c03e488a2a64e773678
wescopeland <wlcopeland1@gmail.com>
2023-05-27 19:10:50 -0400

    chore: re-trigger ci

--- resources/views/platform/components/game/heading.blade.php
+++ resources/views/platform/components/game/heading.blade.php
@@ -11,7 +11,6 @@ $renderedTitle = renderGameTitle(e($gameTitle));
 
 <h1 class="text-h3">
     <span class="block mb-1">{!! $renderedTitle !!}</span>
-    
     <div class="flex items-center gap-x-1">
         <img src="{{ $iconUrl }}" width="24" height="24" alt="Console icon">
         <span class="block text-sm tracking-tighter">{{ $consoleName }}</span>
