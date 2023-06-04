826b224455eea72a2c1f4cb28cbd6108c6328ba8
luchaos <luchaos@gmail.com>
2023-06-04 12:53:25 +0200

    Fix user id case for HasAuthor trait

--- app/Community/Components/UserActivityFeed.php
+++ app/Community/Components/UserActivityFeed.php
@@ -47,7 +47,7 @@ class UserActivityFeed extends Grid
             AllowedFilter::callback('users', function (Builder $query, $value) {
                 if (request()->user() && $value === 'following') {
                     $followingIds = request()->user()->following()->get(['id'])->pluck('id');
-                    $followingIds[] = request()->user()->id;
+                    $followingIds[] = request()->user()->ID;
                     $query->whereIn('user_id', $followingIds);
                 }
             }),
