c1f29b841d2b36322e0477da07d04359745ddca4
wescopeland <wlcopeland1@gmail.com>
2023-06-22 08:47:06 -0400

    docs(autoExpandTextInput): add some comments

--- resources/js/utils/autoExpandTextInput.ts
+++ resources/js/utils/autoExpandTextInput.ts
@@ -1,11 +1,17 @@
 export function autoExpandTextInput(textInputEl: HTMLTextAreaElement | HTMLInputElement) {
+  // When we do resize the field, add a small buffer so a scrollbar doesn't
+  // appear in the user's browser.
+  const SCROLL_BAR_BUFFER_SIZE = 2;
+
+  // We need to preserve a `minHeight` attribute on the input field so we
+  // don't inadvertently shrink the field.
   if (!textInputEl.dataset.minHeight) {
-    textInputEl.dataset.minHeight = String(textInputEl.offsetHeight + 2);
+    textInputEl.dataset.minHeight = String(textInputEl.offsetHeight + SCROLL_BAR_BUFFER_SIZE);
   } else {
     const preservedHeight = Number(textInputEl.dataset.minHeight);
 
     if (Math.max(textInputEl.scrollHeight, preservedHeight) === textInputEl.scrollHeight) {
-      textInputEl.style.height = `${textInputEl.scrollHeight + 2}px`;
+      textInputEl.style.height = `${textInputEl.scrollHeight + SCROLL_BAR_BUFFER_SIZE}px`;
     }
   }
 }
