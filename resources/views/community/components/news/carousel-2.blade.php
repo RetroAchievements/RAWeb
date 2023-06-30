ad7521e09c5c2a91cc78de9ae3a257b998da1660
Wes Copeland <wlcopeland1@gmail.com>
2023-06-30 17:06:48 -0400

    feat(news): rewrite the carousel and improve ux (#1513)

---
+++ resources/views/community/components/news/carousel-2.blade.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Community\Models\News;
+
+$totalNewsCount = 10;
+
+$newsData = News::orderByDesc('ID')->take($totalNewsCount)->get();
+if ($newsData->isEmpty()) {
+    return;
+}
+?>
+
+<div class="mb-6" aria-live="polite" aria-atomic="true" x-data="newsCarousel()">
+    <h2 class="sr-only">News</h2>
+
+    <div 
+        class="mt-2 relative h-[300px] max-h-[300px] sm:h-[270px] sm:max-h-[270px]"
+        @mouseenter="pause()"
+        @mouseleave="resume()"
+    >
+        <div
+            id="news-carousel-image-list"
+            class="group h-[300px] sm:h-auto max-h-[300px] sm:max-h-[270px] flex snap-x snap-mandatory overflow-x-scroll sm:overflow-x-hidden scroll-smooth rounded-lg transition"
+            @touchstart="pause()"
+            @touchend="updateActiveIndex()"
+        >
+            @foreach ($newsData as $index => $news)
+                <x-news.carousel-item :news="$news" :index="$index" :totalCount="$totalNewsCount" />
+            @endforeach
+        </div>
+
+        <x-news.carousel-scroll-buttons />
+    </div>
+
+    <div class="mt-4 flex justify-center gap-x-1">
+        @foreach ($newsData as $index => $news)
+            <x-news.carousel-position-indicator :index="$index" />
+        @endforeach
+    </div>
+</div>
