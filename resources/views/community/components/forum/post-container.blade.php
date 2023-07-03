b40852258f684d7db4a7b84e00cb762212db52f7
wescopeland <wlcopeland1@gmail.com>
2023-07-03 12:53:42 -0400

    fix(viewtopic): add 64px offset for deep links

--- resources/views/community/components/forum/post-container.blade.php
+++ resources/views/community/components/forum/post-container.blade.php
@@ -3,10 +3,13 @@
     'isHighlighted' => false,
 ])
 
-<div
-    id="{{ $commentId }}"
-    class='{{ $isHighlighted ? 'highlight' : '' }} relative w-[calc(100%+16px)] sm:w-full -mx-2 sm:mx-0 lg:flex rounded-lg mt-3 odd:bg-embed bg-embed-highlight px-1 pb-3 pt-2'
-    style="word-break: break-word;"
->
-    {{ $slot }}
+<div class="relative">
+    <div id="{{ $commentId }}" class="absolute left-0 h-px w-px" style="top: -64px;"></div>
+
+    <div
+        class='{{ $isHighlighted ? 'highlight' : '' }} relative w-[calc(100%+16px)] sm:w-full -mx-2 sm:mx-0 lg:flex rounded-lg mt-3 odd:bg-embed bg-embed-highlight px-1 pb-3 pt-2'
+        style="word-break: break-word;"
+    >
+        {{ $slot }}
+    </div>
 </div>
