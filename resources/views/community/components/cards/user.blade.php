46c2cd05cb358f5514f1a89d83b5f20e33adefbd
wescopeland <wlcopeland1@gmail.com>
2023-07-08 15:01:21 -0400

    fix: align display to forum post metadata

--- resources/views/community/components/cards/user.blade.php
+++ resources/views/community/components/cards/user.blade.php
@@ -73,75 +73,73 @@ if ($rankType !== 'Untracked') {
     $userRankPctLabel = $userRank > 100 ? "(Top $rankPct%)" : "";
 }
 
-$canShowUserRole = $userPermissions !== Permissions::Registered;
+$canShowUserRole = $userPermissions > Permissions::JuniorDeveloper;
 $userRoleLabel = Permissions::toString($userPermissions);
 $useExtraNamePadding = $canShowUserRole && ((mb_strlen($userRoleLabel) >= 14 && mb_strlen($userName) >= 14) || mb_strlen($userName) >= 16)
 ?>
 
-@if($userPermissions >= Permissions::Unregistered)
-    <x-tooltip-card imgSrc="{{ $userAvatarUrl }}">
-        <div class="relative h-full text-2xs">
-            <!-- Role -->
-            @if($canShowUserRole)
-                <div class="absolute top-[-14px] right-[-21px]">
-                    <p class="text-2xs tracking-tighter flex items-center justify-center pl-2 pr-5 pt-2 bg-menu-link text-box-bg rounded">
-                        {{ $userRoleLabel }}
-                    </p>
-                </div>
-            @endif        
-            
-            <!-- Username -->
-            <p class="font-bold text-lg -mt-1 {{ $useExtraNamePadding ? "pt-3" : "" }}">{{ $userName }}</p>
-
-            <!-- Motto -->
-            @if($userMotto !== null && mb_strlen($userMotto) > 2)
-                <div class="mb-1 rounded bg-bg text-text italic p-1 text-2xs hyphens-auto">
-                    <p>{{ $userMotto }}</p>
-                </div>
-            @endif
-
-            <!-- Points -->
-            @if($userHardcorePoints > $userSoftcorePoints)
-                <p>
-                    <span class="font-bold">Points:</span>
-                    {{ localized_number($userHardcorePoints) }}
-                    ({{ localized_number($userRetroPoints) }})
+<x-tooltip-card imgSrc="{{ $userAvatarUrl }}">
+    <div class="relative h-full text-2xs">
+        <!-- Role -->
+        @if($canShowUserRole)
+            <div class="absolute top-[-14px] right-[-21px]">
+                <p class="text-2xs tracking-tighter flex items-center justify-center pl-2 pr-5 pt-2 bg-menu-link text-box-bg rounded">
+                    {{ $userRoleLabel }}
                 </p>
-            @elseif($userSoftcorePoints > 0)
-                <p>
-                    <span class="font-bold">Softcore Points:</span>
-                    {{ localized_number($userSoftcorePoints) }}
-                </p>
-            @else
-                <p><span class="font-bold">Points:</span> 0</p>
-            @endif
+            </div>
+        @endif        
+        
+        <!-- Username -->
+        <p class="font-bold text-lg -mt-1 {{ $useExtraNamePadding ? "pt-3" : "" }}">{{ $userName }}</p>
 
-            <!-- Site Rank -->
+        <!-- Motto -->
+        @if($userMotto !== null && mb_strlen($userMotto) > 2)
+            <div class="mb-1 rounded bg-bg text-text italic p-1 text-2xs hyphens-auto">
+                <p>{{ $userMotto }}</p>
+            </div>
+        @endif
+
+        <!-- Points -->
+        @if($userHardcorePoints > $userSoftcorePoints)
             <p>
-                <span class="font-bold">{{ $userRankLabel }}</span>
-                @if($userUntracked)
-                    <span>Untracked</span>
-                @else
-                    {{ $userRank === 0 ? "Needs at least " . Rank::MIN_POINTS . " points" : "#" . localized_number($userRank) }}
-                    {{ $userRankPctLabel }}
-                @endif
+                <span class="font-bold">Points:</span>
+                {{ localized_number($userHardcorePoints) }}
+                ({{ localized_number($userRetroPoints) }})
             </p>
+        @elseif($userSoftcorePoints > 0)
+            <p>
+                <span class="font-bold">Softcore Points:</span>
+                {{ localized_number($userSoftcorePoints) }}
+            </p>
+        @else
+            <p><span class="font-bold">Points:</span> 0</p>
+        @endif
 
-            <!-- Last Activity -->
-            @if($userLastActivity)
-                <p>
-                    <span class="font-bold">Last Activity:</span>
-                    {{ $userLastActivity }}
-                </p>
+        <!-- Site Rank -->
+        <p>
+            <span class="font-bold">{{ $userRankLabel }}</span>
+            @if($userUntracked)
+                <span>Untracked</span>
+            @else
+                {{ $userRank === 0 ? "Needs at least " . Rank::MIN_POINTS . " points" : "#" . localized_number($userRank) }}
+                {{ $userRankPctLabel }}
             @endif
+        </p>
 
-            <!-- Member Since -->
-            @if($userMemberSince)
-                <p>
-                    <span class="font-bold">Member Since:</span>
-                    {{ getNiceDate(strtotime($userMemberSince), $justDay = true) }}
-                </p>
-            @endif
-        </div>
-    </x-tooltip-card>
-@endif
+        <!-- Last Activity -->
+        @if($userLastActivity)
+            <p>
+                <span class="font-bold">Last Activity:</span>
+                {{ $userLastActivity }}
+            </p>
+        @endif
+
+        <!-- Member Since -->
+        @if($userMemberSince)
+            <p>
+                <span class="font-bold">Member Since:</span>
+                {{ getNiceDate(strtotime($userMemberSince), $justDay = true) }}
+            </p>
+        @endif
+    </div>
+</x-tooltip-card>
