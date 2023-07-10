3802060f7dd43a244eb1f664844ec9c119396e7c
wescopeland <wlcopeland1@gmail.com>
2023-07-10 18:01:55 -0400

    feat: add more game id diversity

--- resources/views/content/welcome.blade.php
+++ resources/views/content/welcome.blade.php
@@ -1,3 +1,23 @@
+<?php
+    $randomGameIds = [
+        1, // Sonic the Hedgehog (Mega Drive)
+        3, // Streets of Rage 2 (Mega Drive)
+        1451, // Mega Man 2 (NES)
+        1995, // Super Mario Bros. 3 (NES)
+        335, // Legend of Zelda, The: A Link to the Past (SNES)
+        446, // Donkey Kong Country 2: Diddy's Kong Quest (SNES)
+        724, // Pokemon Red | Pokemon Blue (GB/C)
+        5371, // Legend of Zelda, The: Link's Awakening DX (GB/C)
+        10078, // Mario Kart 64 (N64)
+        10210, // Banjo-Kazooie (N64)
+        10434, // Crash Bandicoot (PSX)
+        11242, // Final Fantasy VII (PSX)
+        11191, // Pitfall! (Atari 2600)
+        2831, // Metal Gear Solid 3: Subsistence (PS2)
+        2721, // Dragon Quest VIII: Journey of the Cursed King (PS2)
+    ];
+?>
+
 <x-section>
     <div class="bg-embed p-4 border-text rounded">
         <h2>{{ __('Welcome!') }}</h2>
@@ -11,7 +31,7 @@
         </p>
 
         <p>
-            <a href="{{ route('game.show', 1) }}">Which of these achievements do you think you can get?</a>
+            <a href="{{ route('game.show', $randomGameIds[array_rand($randomGameIds)]) }}">Which of these achievements do you think you can get?</a>
         </p>
     </div>
 </x-section>
