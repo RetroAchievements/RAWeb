8e88211fc898c7fdf7ad85bde6c3567bf5ace0b0
wescopeland <wlcopeland1@gmail.com>
2023-07-11 17:19:15 -0400

    fix: address pr feedback

--- resources/views/community/components/event/aotw.blade.php
+++ resources/views/community/components/event/aotw.blade.php
@@ -13,27 +13,33 @@ $achievementPoints = $achievement->Points;
 $achievementRetroPoints = $achievement->TrueRatio;
 $achievementBadgeName = $achievement->BadgeName;
 
+$renderedAchievementTitle = renderAchievementTitle($achievementName);
 $renderedGameTitle = renderGameTitle($game->Title);
 $achievementIconSrc = media_asset("/Badge/$achievementBadgeName.png");
 $gameSystemIconUrl = getSystemIconUrl($game->ConsoleID);
+
+$achievementUrl = route('achievement.show', $achievement->ID);
 ?>
 
 <div class="component">
     <h3>Achievement of the Week</h3>
 
     <div class="bg-embed p-4 rounded border border-embed-highlight">
-        <a href="{{ route('achievement.show', $achievement->ID) }}" class="group flex gap-x-2 mb-3 text-text transition-transform hover:-translate-y-0.5">
-            <img src="{{ $achievementIconSrc }}" alt="Achievement of the week badge" width="64" height="64" class="w-16 h-16">
+        <div class="flex gap-x-2 mb-3 text-text">
+            <a href={{ $achievementUrl }}>
+                <img src="{{ $achievementIconSrc }}" alt="Achievement of the week badge" width="64" height="64" class="w-16 h-16">
+            </a>
+
             <div>
-                <p class="font-semibold leading-4 text-link group-hover:text-link-hover">{{ $achievement->Title }}</p>
+                <a href={{ $achievementUrl }} class="font-semibold leading-4 text-link">{!! $renderedAchievementTitle !!}</a>
                 <p class="text-xs mb-1">5 <span class="TrueRatio">(12)</span> Points</p>
                 <p class="text-xs">{{ $achievement->Description }}</p>
             </div>
-        </a>
+        </div>
 
         <hr class="border-embed-highlight mt-2 mb-3">
 
-        <a href="{{ route('game.show', $achievement->GameID) }}" class="group flex gap-x-2 text-text transition-transform hover:translate-x-0.5">
+        <a href="{{ route('game.show', $achievement->GameID) }}" class="group flex gap-x-2">
             <img src="{{ media_asset($game->ImageIcon) }}" alt="Achievement of the week game badge" width="32" height="32" class="w-8 h-8">
             <div class="-mt-1">
                 <p class="font-semibold mb-0.5 text-xs text-link group-hover:text-link-hover">{!! $renderedGameTitle !!}</p>
