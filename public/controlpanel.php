c1a9d2e7df133b3896b5a27223795903087f6845
wescopeland <wlcopeland1@gmail.com>
2023-07-15 17:56:31 -0400

    fix: address pr feedback

--- public/controlpanel.php
+++ public/controlpanel.php
@@ -28,7 +28,7 @@ function RenderUserPref(
     int $userPref,
     bool $setIfTrue,
     ?string $state = null,
-    ?int $targetLoadingIcon = 1
+    int $targetLoadingIcon = 1,
 ): void {
     echo "<input id='UserPreference$userPref' type='checkbox' ";
     echo "onchange='DoChangeUserPrefs($targetLoadingIcon); return false;' value='1'";
