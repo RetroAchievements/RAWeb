d8fa19e5c706540e7eb7441d22c4975a2b7f0977
Wes Copeland <wlcopeland1@gmail.com>
2023-07-22 19:09:46 -0400

    fix(demo): remediate xl breakpoint left container width

--- resources/views/layouts/components/main.blade.php
+++ resources/views/layouts/components/main.blade.php
@@ -2,7 +2,7 @@
     <main class="{{ $class ?? 'mb-5' }}" data-scroll-target>
         @if(trim($sidebar ?? false))
             <div class="lg:grid grid-flow-col gap-4">
-                <article class="mb-3 lg:col-span-2 {{ $fluid ? '' : 'lg:max-w-[668px]'}} {{ $sidebarPosition === 'right' ? 'order-1' : 'order-2'}}">
+                <article class="mb-3 lg:col-span-2 {{ $fluid ? '' : 'lg:max-w-[668px] xl:max-w-[1180px]' }} {{ $sidebarPosition === 'right' ? 'order-1' : 'order-2'}}">
                     {{ $slot }}
                 </article>
                 <aside class="lg:col-span-1 {{ $sidebarPosition === 'right' ? 'order-2' : 'order-1'}}">
