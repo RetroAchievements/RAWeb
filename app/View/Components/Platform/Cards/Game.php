fb09c515b70af4c341dbb4708c4855d6ba591017
Wes Copeland <wlcopeland1@gmail.com>
2023-07-29 10:37:46 -0400

    chore: attempt to fix ci

--- app/View/Components/Platform/Cards/Game.php
+++ app/View/Components/Platform/Cards/Game.php
@@ -202,7 +202,7 @@ class Game extends Component
 
         [$highestProgressionStatus, $highestProgressionAwardDate] = $this->buildCardUserProgressionData($userGameProgressionAwards);
 
-        $activeClaims = array_filter($rawGameData['Claims'], fn ($claim) => $claim['Status'] === ClaimStatus::Active);
+        $activeClaims = array_filter($rawGameData['Claims'], fn ($claim) => $claim['Status'] == ClaimStatus::Active);
         $activeDeveloperUsernames = array_map(fn ($activeClaim) => $activeClaim['User'], array_values($activeClaims));
         $activeDevelopersLabel = $this->buildActiveDevelopersLabel($activeDeveloperUsernames);
 
