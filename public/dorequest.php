3144bec7abf57811175221766da978123b36857b
Wes Copeland <wlcopeland1@gmail.com>
2023-08-11 18:40:56 -0400

    fix: remediate achievement upload issue (#1744)
    * fix: remediate achievement upload issue
    
    * chore: remove explicit null

--- public/dorequest.php
+++ public/dorequest.php
@@ -390,7 +390,7 @@ switch ($requestType) {
             points: (int) request()->input('z', 0),
             mem: request()->input('m'),
             flag: (int) request()->input('f', AchievementFlag::Unofficial),
-            type: request()->input('t'),
+            type: request()->input('x'),
             idInOut: $achievementID,
             badge: request()->input('b'),
             errorOut: $errorOut
