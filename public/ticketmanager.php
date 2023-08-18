9efd54b65d036b1a0f03ce0cbaf25a8f452f876f
Searo <searo1205@gmail.com>
2023-08-17 21:18:04 -0400

    Add achievement type filtering to Ticket Manager (#1767)
    * Add achievement type filtering to Ticket Manager
    
    * Remove extra line
    
    * Rename Type to Achievement Type

--- public/ticketmanager.php
+++ public/ticketmanager.php
@@ -320,6 +320,12 @@ RenderContentStart($pageTitle);
                 echo $linkFilter('Junior', TicketFilters::DevJunior);
                 echo "</div>";
 
+                // Progression Filter
+                echo "<div>";
+                echo "<b>Achievement Type:</b> ";
+                echo $linkFilter('Progression Only', TicketFilters::ProgressionOnly);
+                echo "</div>";
+
                 // Resolved By Filter
                 if ($closedTickets || $resolvedTickets) {
                     echo "<div>";
@@ -410,6 +416,7 @@ RenderContentStart($pageTitle);
                     $achDesc = $nextTicket['AchievementDesc'];
                     $achAuthor = $nextTicket['AchievementAuthor'];
                     $achPoints = $nextTicket['Points'];
+                    $achType = $nextTicket['AchievementType'];
                     $achBadgeName = $nextTicket['BadgeName'];
                     $gameID = $nextTicket['GameID'];
                     $gameTitle = $nextTicket['GameTitle'];
@@ -431,6 +438,7 @@ RenderContentStart($pageTitle);
                         $achTitle,
                         $achDesc,
                         $achAuthor,
+                        $achType,
                         $gameTitle,
                         $consoleName,
                         $reportNotes,
@@ -495,6 +503,7 @@ RenderContentStart($pageTitle);
                 $achDesc = $nextTicket['AchievementDesc'];
                 $achAuthor = $nextTicket['AchievementAuthor'];
                 $achPoints = $nextTicket['Points'];
+                $achType = $nextTicket['AchievementType'];
                 $achBadgeName = $nextTicket['BadgeName'];
                 $gameID = $nextTicket['GameID'];
                 $gameTitle = $nextTicket['GameTitle'];
@@ -516,6 +525,7 @@ RenderContentStart($pageTitle);
                     $achTitle,
                     $achDesc,
                     $achAuthor,
+                    $achType,
                     $gameTitle,
                     $consoleName,
                     $mode,
@@ -598,6 +608,18 @@ RenderContentStart($pageTitle);
                     echo "</td>";
                     echo "<td colspan='7'>";
                     echo "<b>$reportModes[$mode]</b>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+
+                if (isset($achType)) {
+                    $achTypeLabel = $achType ? __('achievement-type.' . $achType) : 'None';
+                    echo "<tr>";
+                    echo "<td>";
+                    echo "Achievement Type: ";
+                    echo "</td>";
+                    echo "<td colspan='7'>";
+                    echo "<b>$achTypeLabel</b>";
                     echo "</td>";
                     echo "</tr>";
                 }
