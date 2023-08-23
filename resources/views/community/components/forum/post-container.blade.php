5d1b06e540548c81989b3806e55a8f6490c3970d
Wes Copeland <wlcopeland1@gmail.com>
2023-08-23 18:50:15 -0400

    fix(forum): remediate deep links anchors (#1788)

--- resources/views/community/components/forum/post-container.blade.php
+++ resources/views/community/components/forum/post-container.blade.php
@@ -5,7 +5,7 @@
 ])
 
 <div class="relative {{ $isPreview ? "my-2" : "" }}">
-    <div {{ $commentId ? 'id="$commentId"' : ''}} class="absolute left-0 h-px w-px" style="top: -64px;"></div>
+    <div {{ $commentId ? "id=" . $commentId : "" }} class="absolute left-0 h-px w-px" style="top: -64px;"></div>
 
     <div
         class='{{ $isHighlighted ? 'highlight' : '' }} {{ $isPreview ? 'py-2' : 'pb-3 pt-2' }} relative w-[calc(100%+16px)] sm:w-full -mx-2 sm:mx-0 lg:flex rounded-lg mt-3 odd:bg-embed bg-embed-highlight px-1'
