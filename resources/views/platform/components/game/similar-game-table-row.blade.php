93f8f20f11effd64dc70f622936f57eb8dc36a31
Wes Copeland <wlcopeland1@gmail.com>
2023-08-25 09:03:10 -0400

    fix: address pr feedback

--- resources/views/platform/components/game/similar-game-table-row.blade.php
+++ resources/views/platform/components/game/similar-game-table-row.blade.php
@@ -18,18 +18,12 @@
         />
     </td>
 
-    @if ($isFullyFeaturedGame)
+    @if ($isFullyFeaturedGame && $totalPoints > 0)
         <td>
             <div class="flex flex-col items-end">
                 <p class="text-2xs whitespace-nowrap {{ $totalPoints === 0 ? 'text-text-muted' : '' }}">
-                    <span @if ($totalPoints > 0) class="font-semibold" @endif>
-                        @if ($totalPoints > 0)
-                            {{ localized_number($totalPoints) }} points
-                        @else
-                            <span title="This game doesn't have any achievements yet." class="cursor-help">
-                                N/A
-                            </span>
-                        @endif
+                    <span>
+                        {{ localized_number($totalPoints) }} points
                     </span>
                 </p>
 
@@ -43,6 +37,6 @@
             </div>
         </td>
     @else
-        <td></td>
+        <td class="min-w-[86px]"></td>
     @endif
 </tr>
