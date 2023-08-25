9344426bfb4ec09b1edffdcc33083f1924b156d5
Wes Copeland <wlcopeland1@gmail.com>
2023-08-25 09:20:33 -0400

    chore: remove logging

--- public/request/comment/create.php
+++ public/request/comment/create.php
@@ -16,8 +16,6 @@ $input = Validator::validate(Arr::wrap(request()->post()), [
     'commentable_type' => 'required|integer',
 ]);
 
-dd(mb_strlen($input['body']));
-
 $articleID = (int) $input['commentable_id'];
 $articleType = (int) $input['commentable_type'];
 
