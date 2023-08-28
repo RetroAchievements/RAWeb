16833f8fe7573d165ef52f2844900588d0340dab
Jamiras <jamirasmt@gmail.com>
2023-08-28 12:50:30 -0600

    use real button for toggle

--- resources/views/platform/components/game/heading.blade.php
+++ resources/views/platform/components/game/heading.blade.php
@@ -12,14 +12,19 @@ use App\Community\Enums\UserGameListType;
 // TODO: Migrate renderGameTitle to a Blade component.
 $renderedTitle = renderGameTitle($gameTitle);
 
+$addButtonTooltip = 'Add to Want to Play list';
+$removeButtonTooltip = 'Remove from Want to Play list';
 $addVisibility = '';
 $removeVisibility = '';
 $onLists = !empty($user) ? getUserGameListsContaining($user, $gameID) : [];
 
+$buttonTooltip = '';
 if (in_array(UserGameListType::Play, $onLists)) {
     $addVisibility = 'hidden';
+    $buttonTooltip = $removeButtonTooltip;
 } else {
     $removeVisibility = 'hidden';
+    $buttonTooltip = $addButtonTooltip;
 }
 ?>
 
@@ -28,15 +33,18 @@ if (in_array(UserGameListType::Play, $onLists)) {
 
     @if (!empty($user))
     <div class="md:float-right">
-        <div id='add-to-play-list' class="w-[18px] h-[18px] {{ $addVisibility }}" title="Add to Want to Play list" cursor="pointer"
-            onClick="toggleListItem('{{ UserGameListType::Play}}')">
-            <x-icon.add-to-list />
-        </div>
-
-        <div id='remove-from-play-list' class="w-[18px] h-[18px] {{ $removeVisibility }}" title="Remove from Want to Play list" cursor="pointer"
-            onClick="toggleListItem('{{ UserGameListType::Play}}')">
-            <x-icon.remove-from-list />
-        </div>
+        <button id='play-list-button' class='btn' type='button' title='{{ $buttonTooltip }}'
+                onClick="toggleListItem('{{ UserGameListType::Play}}')">
+            <div class="flex items-center gap-x-1">
+                <div id='add-to-play-list' class="md:float-left w-[18px] h-[18px] {{ $addVisibility }}">
+                    <x-icon.add-to-list />
+                </div>
+                <div id='remove-from-play-list' class="w-[18px] h-[18px] {{ $removeVisibility }}">
+                    <x-icon.remove-from-list />
+                </div>
+                <span class="block text-sm tracking-tighter">Want to Play</span>
+            </div>
+        </button>
     </div>
     @endif
 
@@ -57,6 +65,11 @@ if (in_array(UserGameListType::Play, $onLists)) {
         .done(function () {
             $("#add-to-" + type + "-list").toggle();
             $("#remove-from-" + type + "-list").toggle();
+            if ($("#add-to-" + type + "-list").is(':visible')) {
+                $("#" + type + "-list-button").prop('title', '{{ $addButtonTooltip }}');
+            } else {
+                $("#" + type + "-list-button").prop('title', '{{ $removeButtonTooltip }}');
+            }
         });
     }
 </script>
