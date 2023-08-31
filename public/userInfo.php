e40116918820550fc2c5d3acd343fa3cbef4c236
Wes Copeland <wlcopeland1@gmail.com>
2023-08-31 19:21:14 -0400

    perf(user): hide completion progress behind authentication

--- public/userInfo.php
+++ public/userInfo.php
@@ -52,7 +52,7 @@ $totalHardcoreAchievements = 0;
 $totalSoftcoreAchievements = 0;
 
 // Get user's list of played games and pct completion
-$userCompletedGamesList = getUsersCompletedGamesAndMax($userPage);
+$userCompletedGamesList = $user ? getUsersCompletedGamesAndMax($userPage) : [];
 
 $excludedConsoles = ["Hubs", "Events"];
 
@@ -630,7 +630,10 @@ RenderContentStart($userPage);
         $prefersHiddenUserCompletedSets = request()->cookie('prefers_hidden_user_completed_sets') === 'true';
 
         RenderSiteAwards(getUsersSiteAwards($userPage), $userPage);
-        RenderCompletedGamesList($userCompletedGamesList, $userPage, $prefersHiddenUserCompletedSets);
+
+        if (count($userCompletedGamesList) >= 1) {
+            RenderCompletedGamesList($userCompletedGamesList, $userPage, $prefersHiddenUserCompletedSets);
+        }
 
         echo "<div id='achdistribution' class='component'>";
         echo "<h3>Recent Progress</h3>";
