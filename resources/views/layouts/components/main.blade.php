a0b455acfe6d88f6b069333ea3442002efc0a526
luchaos <luchaos@gmail.com>
2023-09-03 20:26:37 +0200

    Sidebar stays at 340px

--- resources/views/layouts/components/main.blade.php
+++ resources/views/layouts/components/main.blade.php
@@ -1,6 +1,5 @@
 <x-container :fluid="$fluid ?? false">
     <main class="mb-5 {{ trim($sidebar ?? false) ? 'with-sidebar' : '' }}" data-scroll-target>
-{{--         grid-template-columns: 1.329fr 0.671fr --}}
         @if(trim($sidebar ?? false))
             <article class="{{ $sidebarPosition === 'right' ? 'order-2' : 'order-1'}}">
                 {{ $slot }}
