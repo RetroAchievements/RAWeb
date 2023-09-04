ef6404599bfb98b0eb126d3a7f802f828b468125
Jamiras <32680403+Jamiras@users.noreply.github.com>
2023-09-04 07:25:54 -0600

    attempt to address eslint warnings (#1823)

--- resources/js/utils/helpers.ts
+++ resources/js/utils/helpers.ts
@@ -1,6 +1,6 @@
 export const config = (key: string) => window?.cfg?.[key];
 
 export const asset = (uri: string) => {
-  const assetsUrl = config('assetsUrl') ?? (window as any).assetUrl;
+  const assetsUrl = config('assetsUrl') ?? window.assetUrl;
   return `${assetsUrl}${uri}`;
 };
