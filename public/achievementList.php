b3c632b11fb909167d50cdd4309c79fd1c31abf9
luchaos <luchaos@gmail.com>
2023-09-04 21:39:23 +0200

    feat(layout): enable wide layout (#1821)
    * feat(layout): enable wide layout
    
    * Remove unused wide layout attribute from body
    
    * fix debug positioning for full-width debugbar
    
    * Eliminate fullcontainer, leftcontainer, and rightcontainer
    
    * Remove 2xl breakpoint
    
    * Revert logo orientation
    
    * Remove dropdown focus-within
    
    * Sidebar stays at 340px
    
    * Fix heading underline resets
    
    * Set sidebar width to 380px
    
    * feat: optimize for XL
    
    * chore: lint
    
    * fix: add remaining changes
    
    * fix: address pr feedback
    
    * fix: address pr feedback
    
    ---------
    
    Co-authored-by: Wes Copeland <wlcopeland1@gmail.com>

--- public/achievementList.php
+++ public/achievementList.php
@@ -2,6 +2,7 @@
 
 use App\Platform\Enums\AchievementFlag;
 use App\Platform\Models\System;
+use Illuminate\Support\Facades\Blade;
 
 $consoleList = System::get(['ID', 'Name'])->keyBy('ID')->map(fn ($system) => $system['Name']);
 $consoleIDInput = (int) request()->input('z', 0);
@@ -42,209 +43,206 @@ if ($consoleIDInput !== 0) {
 
 RenderContentStart("Achievement List" . $requestedConsole);
 ?>
-<div id='mainpage'>
-    <div id='fullcontainer'>
-        <?php
-        echo "<div class='navpath'>";
-        if ($requestedConsole == "") {
-            echo "<b>Achievement List</b>";
-        } // NB. This will be a stub page
-        echo "</div>";
+<article>
+    <?php
+    echo "<div class='navpath'>";
+    if ($requestedConsole == "") {
+        echo "<b>Achievement List</b>";
+    } // NB. This will be a stub page
+    echo "</div>";
 
-        echo "<div class='detaillist'>";
+    echo "<div class='detaillist'>";
 
-        echo "<h3>";
-        if ($dev != null) {
-            echo "<a href='/user/$dev'>$dev</a>'s ";
-        }
-        echo "Achievement List</h3>";
+    echo "<h3>";
+    if ($dev != null) {
+        echo "<a href='/user/$dev'>$dev</a>'s ";
+    }
+    echo "Achievement List</h3>";
 
-        echo "<div class='flex flex-wrap justify-between'>";
-        echo "<div>";
+    echo "<div class='flex flex-wrap justify-between'>";
+    echo "<div>";
 
-        echo $params !== AchievementFlag::OfficialCore ? "<a href='/achievementList.php?s=$sortBy&p=" . AchievementFlag::OfficialCore . "$dev_param'>" : "<b>";
-        echo "Achievements in Core Sets";
-        echo $params !== AchievementFlag::OfficialCore ? "</a>" : "</b>";
+    echo $params !== AchievementFlag::OfficialCore ? "<a href='/achievementList.php?s=$sortBy&p=" . AchievementFlag::OfficialCore . "$dev_param'>" : "<b>";
+    echo "Achievements in Core Sets";
+    echo $params !== AchievementFlag::OfficialCore ? "</a>" : "</b>";
+    echo "<br>";
+
+    if ($user !== null) {
+        echo $params !== AchievementFlag::Unofficial ? "<a href='/achievementList.php?s=$sortBy&p=" . AchievementFlag::Unofficial . "$dev_param'>" : "<b>";
+        echo "Achievements in Unofficial Sets";
+        echo $params !== AchievementFlag::Unofficial ? "</a>" : "</b>";
         echo "<br>";
 
-        if ($user !== null) {
-            echo $params !== AchievementFlag::Unofficial ? "<a href='/achievementList.php?s=$sortBy&p=" . AchievementFlag::Unofficial . "$dev_param'>" : "<b>";
-            echo "Achievements in Unofficial Sets";
-            echo $params !== AchievementFlag::Unofficial ? "</a>" : "</b>";
-            echo "<br>";
+        echo $params !== 1 ? "<a href='/achievementList.php?s=$sortBy&p=1$dev_param'>" : "<b>";
+        echo "My Unlocked Achievements";
+        echo $params !== 1 ? "</a>" : "</b>";
+        echo "<br>";
 
-            echo $params !== 1 ? "<a href='/achievementList.php?s=$sortBy&p=1$dev_param'>" : "<b>";
-            echo "My Unlocked Achievements";
-            echo $params !== 1 ? "</a>" : "</b>";
-            echo "<br>";
+        // echo $params !== 2 ? "<a href='/achievementList.php?s=$sortBy&p=2$dev_param'>" : "<b>";
+        // echo "Achievements I haven't yet unlocked";
+        // echo $params !== 2 ? "</a>" : "</b>";
+        // echo "<br>";
+    }
+    echo "</div>";
 
-            // echo $params !== 2 ? "<a href='/achievementList.php?s=$sortBy&p=2$dev_param'>" : "<b>";
-            // echo "Achievements I haven't yet unlocked";
-            // echo $params !== 2 ? "</a>" : "</b>";
-            // echo "<br>";
-        }
+    if ($user !== null) {
+        echo "<div>";
+        echo "Filter by developer:<br>";
+        echo "<form action='/achievementList.php'>";
+        echo "<input type='hidden' name='s' value='$sortBy'>";
+        echo "<input type='hidden' name='p' value='$params'>";
+        echo "<input size='28' name='d' type='text' value='$dev'>";
+        echo "&nbsp;<button class='btn'>Select</button>";
+        echo "</form>";
         echo "</div>";
+    }
 
-        if ($user !== null) {
-            echo "<div>";
-            echo "Filter by developer:<br>";
-            echo "<form action='/achievementList.php'>";
-            echo "<input type='hidden' name='s' value='$sortBy'>";
-            echo "<input type='hidden' name='p' value='$params'>";
-            echo "<input size='28' name='d' type='text' value='$dev'>";
-            echo "&nbsp;<button class='btn'>Select</button>";
-            echo "</form>";
-            echo "</div>";
-        }
+    echo "</div>";
 
-        echo "</div>";
+    echo "<div class='float-right'>* = ordered by</div>";
+    echo "<br style='clear:both;' />";
 
-        echo "<div class='float-right'>* = ordered by</div>";
-        echo "<br style='clear:both;' />";
+    echo "<div class='table-wrapper'><table class='table-highlight'><tbody>";
 
-        echo "<div class='table-wrapper'><table class='table-highlight'><tbody>";
+    $sort1 = ($sortBy == 1) ? 11 : 1;
+    $sort2 = ($sortBy == 2) ? 12 : 2;
+    $sort3 = ($sortBy == 13) ? 3 : 13;
+    $sort4 = ($sortBy == 4) ? 14 : 4;
+    $sort5 = ($sortBy == 5) ? 15 : 5;
+    $sort6 = ($sortBy == 6) ? 16 : 6;
+    $sort7 = ($sortBy == 17) ? 7 : 17;
+    $sort8 = ($sortBy == 18) ? 8 : 18;
+    $sort9 = ($sortBy == 19) ? 9 : 19;
 
-        $sort1 = ($sortBy == 1) ? 11 : 1;
-        $sort2 = ($sortBy == 2) ? 12 : 2;
-        $sort3 = ($sortBy == 13) ? 3 : 13;
-        $sort4 = ($sortBy == 4) ? 14 : 4;
-        $sort5 = ($sortBy == 5) ? 15 : 5;
-        $sort6 = ($sortBy == 6) ? 16 : 6;
-        $sort7 = ($sortBy == 17) ? 7 : 17;
-        $sort8 = ($sortBy == 18) ? 8 : 18;
-        $sort9 = ($sortBy == 19) ? 9 : 19;
+    $mark1 = ($sortBy % 10 == 1) ? '&nbsp;*' : '';
+    $mark2 = ($sortBy % 10 == 2) ? '&nbsp;*' : '';
+    $mark3 = ($sortBy % 10 == 3) ? '&nbsp;*' : '';
+    $mark4 = ($sortBy % 10 == 4) ? '&nbsp;*' : '';
+    $mark5 = ($sortBy % 10 == 5) ? '&nbsp;*' : '';
+    $mark6 = ($sortBy % 10 == 6) ? '&nbsp;*' : '';
+    $mark7 = ($sortBy % 10 == 7) ? '&nbsp;*' : '';
+    $mark8 = ($sortBy % 10 == 8) ? '&nbsp;*' : '';
+    $mark9 = ($sortBy % 10 == 9) ? '&nbsp;*' : '';
 
-        $mark1 = ($sortBy % 10 == 1) ? '&nbsp;*' : '';
-        $mark2 = ($sortBy % 10 == 2) ? '&nbsp;*' : '';
-        $mark3 = ($sortBy % 10 == 3) ? '&nbsp;*' : '';
-        $mark4 = ($sortBy % 10 == 4) ? '&nbsp;*' : '';
-        $mark5 = ($sortBy % 10 == 5) ? '&nbsp;*' : '';
-        $mark6 = ($sortBy % 10 == 6) ? '&nbsp;*' : '';
-        $mark7 = ($sortBy % 10 == 7) ? '&nbsp;*' : '';
-        $mark8 = ($sortBy % 10 == 8) ? '&nbsp;*' : '';
-        $mark9 = ($sortBy % 10 == 9) ? '&nbsp;*' : '';
+    echo "<tr class='do-not-highlight'>";
 
-        echo "<tr class='do-not-highlight'>";
+    echo "<th class='pr-0'></th>";
+    echo "<th>";
+    echo "<a href='/achievementList.php?s=$sort1&p=$params$dev_param'>Title</a>$mark1";
+    echo " / ";
+    echo "<a href='/achievementList.php?s=$sort2&p=$params$dev_param'>Description</a>$mark2";
+    echo "</th>";
 
-        echo "<th class='pr-0'></th>";
-        echo "<th>";
-        echo "<a href='/achievementList.php?s=$sort1&p=$params$dev_param'>Title</a>$mark1";
-        echo " / ";
-        echo "<a href='/achievementList.php?s=$sort2&p=$params$dev_param'>Description</a>$mark2";
+    if (!$mobileBrowser) {
+        echo "<th class='whitespace-nowrap'>";
+        echo "<a href='/achievementList.php?s=$sort3&p=$params$dev_param'>Points</a>$mark3 ";
+        echo "<br><span class='TrueRatio'>(<a href='/achievementList.php?s=$sort4&p=$params$dev_param'>RetroPoints</a>$mark4)</span>";
         echo "</th>";
+        echo "<th><a href='/achievementList.php?s=$sort5&p=$params$dev_param'>Author</a>$mark5</th>";
+    }
+
+    echo "<th><a href='/achievementList.php?s=$sort6&p=$params$dev_param'>Game</a>$mark6</th>";
+
+    if (!$isOwnEarnedAchievementsList) {
+        echo "<th><a href='/achievementList.php?s=$sort7&p=$params$dev_param'>Added</a>$mark7</th>";
 
         if (!$mobileBrowser) {
-            echo "<th class='whitespace-nowrap'>";
-            echo "<a href='/achievementList.php?s=$sort3&p=$params$dev_param'>Points</a>$mark3 ";
-            echo "<br><span class='TrueRatio'>(<a href='/achievementList.php?s=$sort4&p=$params$dev_param'>Retro Ratio</a>$mark4)</span>";
-            echo "</th>";
-            echo "<th><a href='/achievementList.php?s=$sort5&p=$params$dev_param'>Author</a>$mark5</th>";
+            echo "<th><a href='/achievementList.php?s=$sort8&p=$params$dev_param'>Modified</a>$mark8</th>";
+        }
+    } else {
+        echo "<th><a href='/achievementList.php?s=$sort9&p=$params$dev_param'>Awarded</a>$mark9</th>";
+    }
+
+    echo "</tr>";
+
+    foreach ($achData as $achEntry) {
+        $achID = $achEntry['ID'];
+        $achTitle = $achEntry['AchievementTitle'];
+        $achDesc = $achEntry['Description'];
+        $achPoints = $achEntry['Points'];
+        $achTruePoints = $achEntry['TrueRatio'];
+        $achAuthor = $achEntry['Author'];
+        $achDateCreated = $achEntry['DateCreated'];
+        $achDateModified = $achEntry['DateModified'];
+        $achBadgeName = $achEntry['BadgeName'];
+        $gameID = $achEntry['GameID'];
+        $gameIcon = $achEntry['GameIcon'];
+        $gameTitle = $achEntry['GameTitle'];
+        $consoleID = $achEntry['ConsoleID'];
+        $consoleName = $achEntry['ConsoleName'];
+        $achAwardedDate = isset($achEntry['AwardedDate']) ? $achEntry['AwardedDate'] : "";
+
+        sanitize_outputs(
+            $achTitle,
+            $achDesc,
+            $achAuthor,
+            $gameTitle,
+            $consoleName
+        );
+
+        echo "<tr>";
+
+        echo "<td class='pr-0'>";
+        echo achievementAvatar($achEntry, label: false);
+        echo "</td>";
+        echo "<td class='w-full xl:w-[50%]'>";
+        echo achievementAvatar($achEntry, icon: false);
+        echo "<br>$achDesc";
+        echo "</td>";
+
+        if (!$mobileBrowser) {
+            echo "<td>";
+            echo "$achPoints ";
+            echo Blade::render("<x-points-weighted-container>(" . localized_number($achTruePoints) . ")</x-points-weighted-container>");
+            echo "</td>";
+
+            echo "<td>";
+            echo userAvatar($achAuthor, label: false);
+            echo "</td>";
         }
 
-        echo "<th><a href='/achievementList.php?s=$sort6&p=$params$dev_param'>Game</a>$mark6</th>";
+        echo "<td>";
+        echo gameAvatar($achEntry, label: false);
+        echo "</td>";
 
         if (!$isOwnEarnedAchievementsList) {
-            echo "<th><a href='/achievementList.php?s=$sort7&p=$params$dev_param'>Added</a>$mark7</th>";
+            echo "<td>";
+            echo "<span class='smalldate'>" . getNiceDate(strtotime($achDateCreated)) . "</span>";
+            echo "</td>";
 
             if (!$mobileBrowser) {
-                echo "<th><a href='/achievementList.php?s=$sort8&p=$params$dev_param'>Modified</a>$mark8</th>";
+                echo "<td>";
+                echo "<span class='smalldate'>" . getNiceDate(strtotime($achDateModified)) . "</span>";
+                echo "</td>";
             }
         } else {
-            echo "<th><a href='/achievementList.php?s=$sort9&p=$params$dev_param'>Awarded</a>$mark9</th>";
-        }
-
-        echo "</tr>";
-
-        foreach ($achData as $achEntry) {
-            $achID = $achEntry['ID'];
-            $achTitle = $achEntry['AchievementTitle'];
-            $achDesc = $achEntry['Description'];
-            $achPoints = $achEntry['Points'];
-            $achTruePoints = $achEntry['TrueRatio'];
-            $achAuthor = $achEntry['Author'];
-            $achDateCreated = $achEntry['DateCreated'];
-            $achDateModified = $achEntry['DateModified'];
-            $achBadgeName = $achEntry['BadgeName'];
-            $gameID = $achEntry['GameID'];
-            $gameIcon = $achEntry['GameIcon'];
-            $gameTitle = $achEntry['GameTitle'];
-            $consoleID = $achEntry['ConsoleID'];
-            $consoleName = $achEntry['ConsoleName'];
-            $achAwardedDate = isset($achEntry['AwardedDate']) ? $achEntry['AwardedDate'] : "";
-
-            sanitize_outputs(
-                $achTitle,
-                $achDesc,
-                $achAuthor,
-                $gameTitle,
-                $consoleName
-            );
-
-            echo "<tr>";
-
-            echo "<td class='pr-0'>";
-            echo achievementAvatar($achEntry, label: false);
-            echo "</td>";
-            echo "<td class='w-full'>";
-            echo achievementAvatar($achEntry, icon: false);
-            echo "<br>$achDesc";
-            echo "</td>";
-
-            if (!$mobileBrowser) {
-                echo "<td>";
-                echo "$achPoints ";
-                echo "<span class='TrueRatio'>($achTruePoints)</span>";
-                echo "</td>";
-
-                echo "<td>";
-                echo userAvatar($achAuthor, label: false);
-                echo "</td>";
+            $renderAwardedDate = "Unknown";
+            if (strlen($achAwardedDate) > 0) {
+                $renderAwardedDate = getNiceDate(strtotime($achAwardedDate));
             }
 
             echo "<td>";
-            echo gameAvatar($achEntry, label: false);
+            echo "<span class='smalldate'>$renderAwardedDate</span>";
             echo "</td>";
-
-            if (!$isOwnEarnedAchievementsList) {
-                echo "<td>";
-                echo "<span class='smalldate'>" . getNiceDate(strtotime($achDateCreated)) . "</span>";
-                echo "</td>";
-
-                if (!$mobileBrowser) {
-                    echo "<td>";
-                    echo "<span class='smalldate'>" . getNiceDate(strtotime($achDateModified)) . "</span>";
-                    echo "</td>";
-                }
-            } else {
-                $renderAwardedDate = "Unknown";
-                if (strlen($achAwardedDate) > 0) {
-                    $renderAwardedDate = getNiceDate(strtotime($achAwardedDate));
-                }
-
-                echo "<td>";
-                echo "<span class='smalldate'>$renderAwardedDate</span>";
-                echo "</td>";
-            }
-
-            echo "</tr>";
         }
 
-        echo "</tbody></table></div>";
-        echo "</div>";
+        echo "</tr>";
+    }
 
-        echo "<div class='float-right row'>";
-        if ($offset > 0) {
-            $prevOffset = $offset - $maxCount;
-            echo "<a href='/achievementList.php?s=$sortBy&o=$prevOffset&p=$params$dev_param'>&lt; Previous $maxCount</a> - ";
-        }
-        if ($achData->count() === $maxCount) {
-            // Max number fetched, i.e. there are more. Can goto next 25.
-            $nextOffset = $offset + $maxCount;
-            echo "<a href='/achievementList.php?s=$sortBy&o=$nextOffset&p=$params$dev_param'>Next $maxCount &gt;</a>";
-        }
-        echo "</div>";
-        ?>
-        <br>
-    </div>
-</div>
+    echo "</tbody></table></div>";
+    echo "</div>";
+
+    echo "<div class='text-right'>";
+    if ($offset > 0) {
+        $prevOffset = $offset - $maxCount;
+        echo "<a href='/achievementList.php?s=$sortBy&o=$prevOffset&p=$params$dev_param'>&lt; Previous $maxCount</a> - ";
+    }
+    if ($achData->count() === $maxCount) {
+        // Max number fetched, i.e. there are more. Can goto next 25.
+        $nextOffset = $offset + $maxCount;
+        echo "<a href='/achievementList.php?s=$sortBy&o=$nextOffset&p=$params$dev_param'>Next $maxCount &gt;</a>";
+    }
+    echo "</div>";
+    ?>
+</article>
 <?php RenderContentEnd(); ?>
--- public/inbox.php
+++ public/inbox.php
@@ -50,140 +50,138 @@ function MarkAsUnread(msgID) {
     });
 }
 </script>
-<div id="mainpage">
-    <div id='fullcontainer'>
-        <div id="globalfeed">
-            <?php
-            if ($outbox) {
-                echo "<h2>Outbox</h2>";
+<article>
+    <div id="globalfeed">
+        <?php
+        if ($outbox) {
+            echo "<h2>Outbox</h2>";
 
-                echo "<div class='mb-5'>You have <b>$totalMessageCount</b> sent messages.</div>";
+            echo "<div class='mb-5'>You have <b>$totalMessageCount</b> sent messages.</div>";
 
-                echo "<div class='flex justify-between mb-5'>";
-                echo "<a class='btn btn-link' href='/inbox.php'>Inbox</a>";
-                echo "<div class='flex gap-2'>";
-                echo "<a class='btn btn-link' href='/createmessage.php'>Create New Message</a>";
-                echo "</div>";
-                echo "</div>";
-            } else {
-                echo "<h2>Inbox</h2>";
+            echo "<div class='flex justify-between mb-5'>";
+            echo "<a class='btn btn-link' href='/inbox.php'>Inbox</a>";
+            echo "<div class='flex gap-2'>";
+            echo "<a class='btn btn-link' href='/createmessage.php'>Create New Message</a>";
+            echo "</div>";
+            echo "</div>";
+        } else {
+            echo "<h2>Inbox</h2>";
 
-                echo "<div class='mb-5'>";
-                echo "<div>You have <b>$unreadMessageCount</b> unread of <b>$totalMessageCount</b> total messages.</div>";
-                echo "</div>";
-
-                echo "<div class='flex justify-between mb-5'>";
-                echo "<a class='btn btn-link' href='/inbox.php?s=1'>Outbox</a>";
-                echo "<div class='flex gap-2'>";
-                if ($unreadOnly) {
-                    echo "<a class='btn btn-link' href='/inbox.php'>View All Messages</a>";
-                } else {
-                    echo "<a class='btn btn-link' href='/inbox.php?u=1'>View Unread Only</a>";
-                }
-                echo "<a class='btn btn-link' href='/createmessage.php'>Create New Message</a>";
-                echo "</div>";
-                echo "</div>";
-            }
-
-            echo "<table class='table-highlight'><tbody>";
-
-            echo "<tr class='do-not-highlight'>";
-            echo "<th>Date</th>";
-            if ($outbox) {
-                echo "<th colspan='2' style='min-width:150px'>To</th>";
-            } else {
-                echo "<th colspan='2' style='min-width:150px'>From</th>";
-            }
-            echo "<th style='width:100%'>Title</th>";
-            echo "</tr>";
-
-            $totalMsgs = count($allMessages);
-
-            for ($i = 0; $i < $totalMsgs; $i++) {
-                $msgID = $allMessages[$i]['ID'];
-                $msgTime = $allMessages[$i]['TimeSent'];
-                $msgSentAtNice = date("d/m/y, H:i ", strtotime($msgTime));
-                // $msgTo      	= $allMessages[$i]['UserTo'];
-                $msgTitle = $allMessages[$i]['Title'];
-                $msgPayload = $allMessages[$i]['Payload'];
-                $msgType = $allMessages[$i]['Type'];
-
-                if ($outbox) {
-                    $msgUser = $allMessages[$i]['UserTo'];
-                    $msgUnread = false;
-                } else {
-                    $msgUser = $allMessages[$i]['UserFrom'];
-                    $msgUnread = ($allMessages[$i]['Unread'] == 1);
-                }
-
-                sanitize_outputs(
-                    $msgUser,
-                    $msgTitle,
-                    $msgPayload,
-                );
-
-                $msgPayload = Shortcode::render($msgPayload);
-
-                $styleAlt = $i % 2 == 1 ? "alt" : "";
-
-                echo "<tr class='$styleAlt'>";
-
-                echo "<td style='width:15%'>";
-                echo "<span id='msgInlineTitleDate$msgID' title='$msgTime'>$msgSentAtNice</span>";
-                echo "</td>";
-
-                echo "<td style='width:34px'>";
-                echo userAvatar($msgUser, label: false);
-                echo "</td>";
-                echo "<td>";
-                echo userAvatar($msgUser, icon: false);
-                echo "</td>";
-
-                // echo "<td>" . $msgTo . "</td>";
-
-                echo "<td class='cursor-pointer " . ($msgUnread ? 'message-unread' : '') . "' id='msgInlineTitle$msgID' onclick='ReadMessage($msgID)'>";
-                echo $msgTitle;
-                echo "</td>";
-
-                echo "</tr>";
-
-                echo "<tr id='msgInline$msgID' class='$styleAlt msgPayload'>";
-                echo "<td colspan='4'>";
-                echo "<div class='comment'>$msgPayload</div>";
-
-                if (!$outbox) {
-                    echo "<div class='flex justify-end gap-2'>";
-
-                    echo "<form action='/request/message/delete.php' method='post' onsubmit='return confirm(\"Are you sure you want to permanently delete this message?\")'>";
-                    echo csrf_field();
-                    echo "<input type='hidden' name='message' value='$msgID'>";
-                    echo "<button class='btn btn-danger'>Delete</button>";
-                    echo "</form>";
-
-                    echo "<button type='button' class='btn' onclick='MarkAsUnread($msgID)'>Mark as unread</button>";
-
-                    echo "<a class='btn' href='/createmessage.php?t=$msgUser&i=$msgID'>Reply</a>";
-
-                    echo "</div>";
-                }
-
-                echo "</td>";
-                echo "</tr>";
-            }
-
-            echo "</tbody></table>";
-
-            // Get message count and build paginator URL from current GET parameters
-            $messageCount = $unreadOnly ? $unreadMessageCount : $totalMessageCount;
-            unset($_GET['o']);
-            $urlPrefix = '/inbox.php?' . http_build_query($_GET) . '&o=';
-
-            echo "<div class='float-right'>";
-            RenderPaginator($messageCount, $displayCount, $offset, $urlPrefix);
+            echo "<div class='mb-5'>";
+            echo "<div>You have <b>$unreadMessageCount</b> unread of <b>$totalMessageCount</b> total messages.</div>";
             echo "</div>";
 
-            ?>
-        </div>
+            echo "<div class='flex justify-between mb-5'>";
+            echo "<a class='btn btn-link' href='/inbox.php?s=1'>Outbox</a>";
+            echo "<div class='flex gap-2'>";
+            if ($unreadOnly) {
+                echo "<a class='btn btn-link' href='/inbox.php'>View All Messages</a>";
+            } else {
+                echo "<a class='btn btn-link' href='/inbox.php?u=1'>View Unread Only</a>";
+            }
+            echo "<a class='btn btn-link' href='/createmessage.php'>Create New Message</a>";
+            echo "</div>";
+            echo "</div>";
+        }
+
+        echo "<table class='table-highlight'><tbody>";
+
+        echo "<tr class='do-not-highlight'>";
+        echo "<th>Date</th>";
+        if ($outbox) {
+            echo "<th colspan='2' style='min-width:150px'>To</th>";
+        } else {
+            echo "<th colspan='2' style='min-width:150px'>From</th>";
+        }
+        echo "<th style='width:100%'>Title</th>";
+        echo "</tr>";
+
+        $totalMsgs = count($allMessages);
+
+        for ($i = 0; $i < $totalMsgs; $i++) {
+            $msgID = $allMessages[$i]['ID'];
+            $msgTime = $allMessages[$i]['TimeSent'];
+            $msgSentAtNice = date("d/m/y, H:i ", strtotime($msgTime));
+            // $msgTo      	= $allMessages[$i]['UserTo'];
+            $msgTitle = $allMessages[$i]['Title'];
+            $msgPayload = $allMessages[$i]['Payload'];
+            $msgType = $allMessages[$i]['Type'];
+
+            if ($outbox) {
+                $msgUser = $allMessages[$i]['UserTo'];
+                $msgUnread = false;
+            } else {
+                $msgUser = $allMessages[$i]['UserFrom'];
+                $msgUnread = ($allMessages[$i]['Unread'] == 1);
+            }
+
+            sanitize_outputs(
+                $msgUser,
+                $msgTitle,
+                $msgPayload,
+            );
+
+            $msgPayload = Shortcode::render($msgPayload);
+
+            $styleAlt = $i % 2 == 1 ? "alt" : "";
+
+            echo "<tr class='$styleAlt'>";
+
+            echo "<td style='width:15%'>";
+            echo "<span id='msgInlineTitleDate$msgID' title='$msgTime'>$msgSentAtNice</span>";
+            echo "</td>";
+
+            echo "<td style='width:34px'>";
+            echo userAvatar($msgUser, label: false);
+            echo "</td>";
+            echo "<td>";
+            echo userAvatar($msgUser, icon: false);
+            echo "</td>";
+
+            // echo "<td>" . $msgTo . "</td>";
+
+            echo "<td class='cursor-pointer " . ($msgUnread ? 'message-unread' : '') . "' id='msgInlineTitle$msgID' onclick='ReadMessage($msgID)'>";
+            echo $msgTitle;
+            echo "</td>";
+
+            echo "</tr>";
+
+            echo "<tr id='msgInline$msgID' class='$styleAlt msgPayload'>";
+            echo "<td colspan='4'>";
+            echo "<div class='comment'>$msgPayload</div>";
+
+            if (!$outbox) {
+                echo "<div class='flex justify-end gap-2'>";
+
+                echo "<form action='/request/message/delete.php' method='post' onsubmit='return confirm(\"Are you sure you want to permanently delete this message?\")'>";
+                echo csrf_field();
+                echo "<input type='hidden' name='message' value='$msgID'>";
+                echo "<button class='btn btn-danger'>Delete</button>";
+                echo "</form>";
+
+                echo "<button type='button' class='btn' onclick='MarkAsUnread($msgID)'>Mark as unread</button>";
+
+                echo "<a class='btn' href='/createmessage.php?t=$msgUser&i=$msgID'>Reply</a>";
+
+                echo "</div>";
+            }
+
+            echo "</td>";
+            echo "</tr>";
+        }
+
+        echo "</tbody></table>";
+
+        // Get message count and build paginator URL from current GET parameters
+        $messageCount = $unreadOnly ? $unreadMessageCount : $totalMessageCount;
+        unset($_GET['o']);
+        $urlPrefix = '/inbox.php?' . http_build_query($_GET) . '&o=';
+
+        echo "<div class='text-right'>";
+        RenderPaginator($messageCount, $displayCount, $offset, $urlPrefix);
+        echo "</div>";
+
+        ?>
     </div>
-</div>
+</article>
 <?php RenderContentEnd(); ?>
