6e79e3f3103cbc72a7752e5c08cb56314c4b51ab
luchaos <luchaos@gmail.com>
2023-09-05 18:40:49 +0200

    Remove retro logo switch code

--- resources/views/layouts/components/brand-top.blade.php
+++ resources/views/layouts/components/brand-top.blade.php
@@ -1,37 +1,21 @@
 <x-section class="brand-top bg-embedded">
     <x-container>
-        <div class="hidden lg:block">
-            @if(request()->cookie('logo') === 'retro')
-                <div class="logo-container py-4">
-                    <div class="text-center">
-                        <a href="{{ route('home') }}">
-                            <img class="logo-retro w-full"
-                                 fetchpriority="high"
-                                 src="{{ asset('assets/images/ra-logo-sm.webp') }}"
-                                 alt="{{ config('app.name') }}"
-                            >
-                        </a>
+        <div class="logo-container hidden lg:flex justify-start pt-6 pb-4 pl-4">
+            <a href="{{ route('home') }}" class="text-inverse">
+                <div class="flex flex-row">
+                    <img class="logo"
+                         fetchpriority="high"
+                         src="{{ asset('assets/images/ra-icon.webp') }}"
+                         alt="{{ config('app.name') }}"
+                    >
+                    <div class="hidden md:flex flex-col justify-end ml-2">
+                        <h1 class="mb-1">{{ config('app.name') }}</h1>
+                        <div class="ml-1 description">
+                            {{ __('app.description') }}
+                        </div>
                     </div>
                 </div>
-            @else
-                <div class="logo-container flex justify-start pt-6 pb-4 pl-4">
-                    <a href="{{ route('home') }}" class="text-inverse">
-                        <div class="flex flex-row">
-                            <img class="logo"
-                                 fetchpriority="high"
-                                 src="{{ asset('assets/images/ra-icon.webp') }}"
-                                 alt="{{ config('app.name') }}"
-                            >
-                            <div class="hidden md:flex flex-col justify-end ml-2">
-                                <h1 class="mb-1">{{ config('app.name') }}</h1>
-                                <div class="ml-1 description">
-                                    {{ __('app.description') }}
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            @endif
+            </a>
         </div>
     </x-container>
 </x-section>
