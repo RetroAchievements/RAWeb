40dae400f8b3c1c9619115a4e98266faf5d9e1b6
Wes Copeland <wlcopeland1@gmail.com>
2023-09-06 19:31:47 -0400

    fix: remediate invalid svg attributes errors

--- resources/views/platform/components/game/current-progress/secondary-actions-menu.blade.php
+++ resources/views/platform/components/game/current-progress/secondary-actions-menu.blade.php
@@ -27,7 +27,7 @@ function beginResetProgress() {
         @click="isMenuOpen = true"
     >
         <div class="w-3 h-3">
-            <svg xmlns:xlink="http://www.w3.org/1999/xlink" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg" width="inherit" height="inherit">
+            <svg xmlns:xlink="http://www.w3.org/1999/xlink" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" stroke-width="2px"></path>
                 <path d="M4 6l16 0" fill="none" stroke-width="2px"></path>
                 <path d="M4 12l16 0" fill="none" stroke-width="2px"></path>
