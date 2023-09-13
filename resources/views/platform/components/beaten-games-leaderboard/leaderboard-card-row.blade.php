5907c4d5f6aa7e5170add9beb66dd363b0114868
Wes Copeland <wlcopeland1@gmail.com>
2023-09-12 20:36:05 -0400

    fix: address pr feedback

--- resources/views/platform/components/beaten-games-leaderboard/leaderboard-card-row.blade.php
+++ resources/views/platform/components/beaten-games-leaderboard/leaderboard-card-row.blade.php
@@ -11,7 +11,11 @@ $isHighlighted = $isHighlighted || ($paginatedRow->User === $myUsername);
 <div class="rounded bg-embed p-2 {{ $isHighlighted ? 'my-2' : '' }}" @if ($isHighlighted) style="outline: thin solid;" @endif>
     <div class="flex items-center justify-between">
         <div class="flex gap-x-3 items-center">
-            <p class="text-sm">#{{ localized_number($paginatedRow->rank_number) }}</p>
+            <p class="text-sm">
+                @if (isset($paginatedRow->rank_number))
+                    #{{ localized_number($paginatedRow->rank_number) }}
+                @endif
+            </p>
             {!! userAvatar($paginatedRow->User, iconClass: 'rounded-sm mr-1') !!}
         </div>
 
--- resources/views/platform/components/beaten-games-leaderboard/leaderboard-table-row.blade.php
+++ resources/views/platform/components/beaten-games-leaderboard/leaderboard-table-row.blade.php
@@ -15,7 +15,11 @@ $lastBeatenDate = Carbon::parse($paginatedRow->last_beaten_date)->format('F j Y'
     class="hidden sm:table-row {{ $isHighlighted ? 'rounded' : '' }}"
     @if ($isHighlighted) style="outline: thin solid" @endif
 >
-    <td>#{{ localized_number($paginatedRow->rank_number) }}</td>
+    <td>
+        @if (isset($paginatedRow->rank_number))
+            #{{ localized_number($paginatedRow->rank_number) }}
+        @endif
+    </td>
     <td>{!! userAvatar($paginatedRow->User, iconClass: 'rounded-sm mr-1') !!}</td>
     <td class='py-2.5'>
         <x-game.multiline-avatar
