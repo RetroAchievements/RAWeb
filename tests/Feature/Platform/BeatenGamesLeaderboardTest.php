00482a3557e272a3b2bd9bd702c937692ba4b4e5
Wes Copeland <wlcopeland1@gmail.com>
2023-09-14 08:31:22 -0400

    chore: resolve conflicts

--- tests/Feature/Platform/BeatenGamesLeaderboardTest.php
+++ tests/Feature/Platform/BeatenGamesLeaderboardTest.php
@@ -9,6 +9,7 @@ use App\Platform\Models\System;
 use App\Site\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Carbon;
+use Tests\Feature\Platform\Concerns\TestsPlayerBadges;
 use Tests\TestCase;
 
 class BeatenGamesLeaderboardTest extends TestCase
