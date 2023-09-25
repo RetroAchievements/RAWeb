e3508d9d0ea3dba44be4bcef36c1a0179963949f
Wes Copeland <wlcopeland1@gmail.com>
2023-09-25 17:06:17 -0400

    refactor(game): migrate recent game players to blade

---
+++ tests/Feature/Platform/Components/RecentGamePlayersTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Platform\Components;
+
+use App\Site\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RecentGamePlayersTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testItRendersRichPresenceMessages(): void
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+        $mockDate = Carbon::now();
+        $recentPlayerData = [
+            [
+                'User' => $user->User,
+                'Date' => $mockDate,
+                'Activity' => 'Playing Sonic the Hedgehog',
+            ]
+        ];
+
+        $view = $this->blade('
+            <x-game.recent-game-players
+                :recentPlayerData="$recentPlayerData"
+                gameTitle="Sonic the Hedgehog"
+            />
+        ', [
+            'recentPlayerData' => $recentPlayerData,
+        ]);
+
+        $view->assertSee($user->User);
+        $view->assertSeeText('Playing Sonic the Hedgehog');
+        $view->assertSeeText($mockDate->format('d M Y, g:ia'));
+    }
+
+    public function testItRendersBrokenRichPresenceMessages(): void
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+        $recentPlayerData = [
+            [
+                'User' => $user->User,
+                'Date' => Carbon::now(),
+                'Activity' => 'Unknown macro this should not appear',
+            ]
+        ];
+
+        $view = $this->blade('
+            <x-game.recent-game-players
+                :recentPlayerData="$recentPlayerData"
+                gameTitle="Sonic the Hedgehog"
+            />
+        ', [
+            'recentPlayerData' => $recentPlayerData,
+        ]);
+
+        $view->assertDontSeeText("this should not appear");
+        $view->assertSeeTextInOrder(["⚠️", "Playing Sonic the Hedgehog"]);
+    }
+}
