9e6c4ee9555e64b70c5eef5db543b136e919987a
Jamiras <jamirasmt@gmail.com>
2023-10-13 10:46:52 -0600

    wrap point updates in feature flag

--- app/Platform/Actions/UpdatePlayerMetrics.php
+++ app/Platform/Actions/UpdatePlayerMetrics.php
@@ -20,9 +20,15 @@ class UpdatePlayerMetrics
         $user->completion_percentage_average = $playerGames->average('completion_percentage');
         $user->completion_percentage_average_hardcore = $playerGames->average('completion_percentage_hardcore');
 
-        $user->RAPoints = $playerGames->sum('points_hardcore');
-        $user->RASoftcorePoints = $playerGames->sum('points') - $user->RAPoints;
-        $user->TrueRAPoints = $playerGames->sum('points_weighted');
+        if (config('feature.aggregate_queries')) {
+            $user->RAPoints = $playerGames->sum('points_hardcore');
+            $user->RASoftcorePoints = $playerGames->sum('points') - $user->RAPoints;
+            $user->TrueRAPoints = $playerGames->sum('points_weighted');
+        } else {
+            $user->RAPoints = $user->achievements()->published()->wherePivotNotNull('unlocked_hardcore_at')->sum('Points');
+            $user->RASoftcorePoints = $user->achievements()->published()->wherePivotNull('unlocked_hardcore_at')->sum('Points');
+            $user->TrueRAPoints = $user->achievements()->published()->wherePivotNotNull('unlocked_hardcore_at')->sum('TrueRatio');
+        }
 
         $user->save();
 
