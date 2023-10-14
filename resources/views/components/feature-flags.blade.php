0261fe92ed44f82c9bb834288cac735a2f3a3665
Wes Copeland <wlcopeland1@gmail.com>
2023-10-14 08:36:19 -0400

    feat: allow setting the cookie via a button and js

--- resources/views/components/feature-flags.blade.php
+++ resources/views/components/feature-flags.blade.php
@@ -1,3 +1,17 @@
+<script>
+function handleToggleCookie() {
+    const currentValue = getCookie('feature_aggregate_queries');
+
+    let newValue = true;
+    if (currentValue === 'true') {
+        newValue = false;
+    }
+
+    setCookie('feature_aggregate_queries', `${newValue}`);
+    showStatusSuccess(`feature_aggregate_queries cookie set to ${newValue}.`);
+}
+</script>
+
 <div class="flex justify-between">
     <p>Beaten Games Player-facing UX</p>
     @hasfeature("beat")
@@ -9,6 +23,11 @@
 
 <div class="flex justify-between">
     <p>Aggregate Queries</p>
+
+    <button class="btn" onClick="handleToggleCookie()">
+        Toggle cookie
+    </button>
+
     @hasfeature("aggregate_queries")
         Enabled
     @else
