77b8ba36e5a02e43e5c48586969b61507bc6ab88
Wes Copeland <wlcopeland1@gmail.com>
2023-10-13 20:47:49 -0400

    chore: remove redundant documentation

--- app/Site/Middleware/FeatureFlagMiddleware.php
+++ app/Site/Middleware/FeatureFlagMiddleware.php
@@ -11,10 +11,9 @@ class FeatureFlagMiddleware
 {
     public function handle(Request $request, Closure $next): mixed
     {
-        // Check for the presence of the 'aggregate_queries' cookie.
         $cookieValue = $request->cookie('feature_aggregate_queries');
 
-        // Override the feature flag configuration if the cookie is set to 'true'
+        // Override the feature flag configuration if the cookie is set to 'true'.
         if ($cookieValue === 'true') {
             config(['feature.aggregate_queries' => true]);
         } elseif ($cookieValue === 'false') {
