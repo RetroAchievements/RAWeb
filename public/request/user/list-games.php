f2e7596e420f90006caaed8e6681b019e1adf58f
Jamiras <jamirasmt@gmail.com>
2023-10-26 19:19:21 -0600

    use player_achievements for reset progress dropdown

--- public/request/user/list-games.php
+++ public/request/user/list-games.php
@@ -1,11 +1,29 @@
 <?php
 
+use App\Site\Models\User;
+
 if (!authenticateFromCookie($user, $permissions, $userDetails)) {
     abort(401);
 }
 
-if (getControlPanelUserInfo($user, $userData)) {
-    return response()->json($userData['Played']);
+$userModel = User::firstWhere('User', $user);
+$games = $userModel
+    ->games()
+    ->with('system')
+    ->where('player_games.achievements_unlocked', '>', 0)
+    ->orderBy('Title')
+    ->select(['GameData.ID', 'Title', 'ConsoleID', 'achievements_published', 'player_games.achievements_unlocked'])
+    ->get();
+
+$dataOut = [];
+foreach ($games as $game) {
+    $dataOut[] = [
+        'ID' => $game->ID,
+        'GameTitle' => $game->Title,
+        'ConsoleName' => $game->system->Name,
+        'NumAwarded' => $game->achievements_unlocked,
+        'NumPossible' => $game->achievements_published,
+    ];
 }
 
-abort(400);
+return $dataOut;
