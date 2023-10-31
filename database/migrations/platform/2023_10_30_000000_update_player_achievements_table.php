b3a5d6fd67553a4d4ae5ee67f2d5ec9a26effa68
Wes Copeland <wlcopeland1@gmail.com>
2023-10-31 16:38:52 -0400

    fix: address pr feedback

--- database/migrations/platform/2023_10_30_000000_update_player_achievements_table.php
+++ database/migrations/platform/2023_10_30_000000_update_player_achievements_table.php
@@ -10,17 +10,27 @@ return new class() extends Migration {
     public function up(): void
     {
         Schema::table('player_achievements', function (Blueprint $table) {
-            $table->index(
-                ['user_id', 'unlocked_at', 'unlocked_hardcore_at', 'achievement_id'],
-                'player_achievements_user_date_achievement'
-            );
+            $sm = Schema::getConnection()->getDoctrineSchemaManager();
+            $indexesFound = $sm->listTableIndexes('player_achievements');
+
+            if (!array_key_exists('player_achievements_user_date_achievement', $indexesFound)) {
+                $table->index(
+                    ['user_id', 'unlocked_at', 'unlocked_hardcore_at', 'achievement_id'],
+                    'player_achievements_user_date_achievement'
+                );
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('player_achievements', function (Blueprint $table) {
-            $table->dropIndex('player_achievements_user_date_achievement');
+            $sm = Schema::getConnection()->getDoctrineSchemaManager();
+            $indexesFound = $sm->listTableIndexes('player_achievements');
+
+            if (array_key_exists('player_achievements_user_date_achievement', $indexesFound)) {
+                $table->dropIndex('player_achievements_user_date_achievement');
+            }
         });
     }
 };
