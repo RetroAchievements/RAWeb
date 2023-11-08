6428e81b5e8c165cb0bd31b5f4a684208d4b241e
Wes Copeland <wlcopeland1@gmail.com>
2023-11-08 08:24:46 -0500

    feat(active-players): support OR searches with a pipe operator

--- app/Community/Services/ActivePlayersService.php
+++ app/Community/Services/ActivePlayersService.php
@@ -31,11 +31,21 @@ class ActivePlayersService
         $trendingGames = $this->computeTrendingGames($filteredActivePlayers);
 
         if ($searchValue) {
-            $filteredActivePlayers = $filteredActivePlayers->filter(function ($player) use ($searchValue) {
-                return stripos($player['User'], $searchValue) !== false
-                    || stripos($player['GameTitle'], $searchValue) !== false
-                    || stripos($player['ConsoleName'], $searchValue) !== false
-                    || stripos($player['RichPresenceMsg'], $searchValue) !== false;
+            // Split the search string by the '|' operator to support "OR" searches.
+            $searchTerms = explode('|', $searchValue);
+    
+            $filteredActivePlayers = $filteredActivePlayers->filter(function ($player) use ($searchTerms) {
+                // Check if any of the search terms are present in any of the player's attributes.
+                foreach ($searchTerms as $term) {
+                    if (stripos($player['User'], $term) !== false
+                        || stripos($player['GameTitle'], $term) !== false
+                        || stripos($player['ConsoleName'], $term) !== false
+                        || stripos($player['RichPresenceMsg'], $term) !== false) {
+                        return true;
+                    }
+                }
+                
+                return false;
             });
         }
 
