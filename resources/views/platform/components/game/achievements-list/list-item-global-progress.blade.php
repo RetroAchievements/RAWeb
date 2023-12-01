a1a1db0f6b531d2e64e23c6daaab3e3a951c9d7d
Wes Copeland <wlcopeland1@gmail.com>
2023-12-01 17:15:06 -0500

    fix: address pr feedback

--- resources/views/platform/components/game/achievements-list/list-item-global-progress.blade.php
+++ resources/views/platform/components/game/achievements-list/list-item-global-progress.blade.php
@@ -52,7 +52,7 @@ if ($totalPlayerCount > 0) {
     aria-valuemax="100"
     aria-valuenow="{{ $unlockRate }}"
     aria-labelledby="progress-label-{{ $achievement['ID'] }}"
-    class="w-full h-1 bg-zinc-950 light:bg-zinc-300 rounded flex"
+    class="w-full h-1 bg-zinc-950 light:bg-zinc-300 rounded flex space-x-px"
 >
     {{-- Hardcore completion --}}
     <div
@@ -67,7 +67,7 @@ if ($totalPlayerCount > 0) {
     {{-- Softcore completion --}}
     <div
         style="width: {{ $softcoreProgressBarWidth }}%"
-        class="bg-neutral-400/80 h-full {{ $hardcoreProgressBarWidth === 0 ? "rounded-l" : "" }}"
+        class="bg-neutral-500 h-full {{ $hardcoreProgressBarWidth === 0 ? "rounded-l" : "" }}"
     >
         <span class="sr-only">
             {{ $softcoreUnlockRate }}% of players have earned the achievement in softcore mode
