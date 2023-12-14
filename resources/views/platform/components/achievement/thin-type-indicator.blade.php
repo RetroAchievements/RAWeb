94412bdbb7c5b0a500dccda4495823a4583bd4a6
Wes Copeland <wlcopeland1@gmail.com>
2023-12-14 17:00:24 -0500

    fix: check for user missable pref

--- resources/views/platform/components/achievement/thin-type-indicator.blade.php
+++ resources/views/platform/components/achievement/thin-type-indicator.blade.php
@@ -4,14 +4,24 @@
 
 <?php
 use App\Platform\Enums\AchievementType;
+use App\Site\Enums\UserPreference;
+use Illuminate\Support\Facades\Auth;
 
 $progressionType = AchievementType::Progression;
 $winConditionType = AchievementType::WinCondition;
 $missableType = AchievementType::Missable;
+
+$canShowTypeIndicator = true;
+if ($type === $missableType) {
+    $currentUser = Auth::user();
+    if (isset($currentUser) && BitSet($currentUser->websitePrefs, UserPreference::Game_HideMissableIndicators)) {
+        $canShowTypeIndicator = false;
+    }
+}
 ?>
 
-@if ($type)
-    <div class="flex items-center gap-1 rounded-full bg-embed border-embed-highlight border px-2 py-0.5 max-w-fit whitespace-nowrap">
+@if ($type && $canShowTypeIndicator)
+    <div class="h-[24px] max-h-[24px] flex items-center rounded-full bg-embed border pl-1 pr-2 py-0.5 max-w-fit whitespace-nowrap {{ $type === $missableType ? 'border-dashed border-neutral-600 gap-0.5' : 'border-embed-highlight gap-1' }}">
         <span class="w-[18px] h-[18px]">
             @if ($type === $progressionType)
                 <x-icon.progression />
