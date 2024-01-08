a5524b0058540cc2d5cd81ed8afcf91d4e113227
Jamiras <jamirasmt@gmail.com>
2024-01-07 19:32:53 -0700

    use rounded style for both ends of 100% bar

--- resources/views/components/hardcore-progress.blade.php
+++ resources/views/components/hardcore-progress.blade.php
@@ -4,19 +4,37 @@
     'maxProgress' => 100,
     'tooltip' => '',
 ])
-
 <?php
-    $hardcoreProgressBarWidth = $softcoreProgressBarWidth = 0;
+    $hardcoreProgressBarWidth = $softcoreProgressBarWidth = '0';
+    $hardcoreClass = $softcoreClass = '';
+
     if ($maxProgress > 0) {
-        $hardcoreProgressBarWidth = sprintf("%01.2f", $hardcoreProgress * 100 / $maxProgress);
-        $softcoreProgressBarWidth = sprintf("%01.2f", ($softcoreProgress - $hardcoreProgress) * 100 / $maxProgress);
+        if ($softcoreProgress >= $maxProgress) {
+            $softcoreProgressBarWidth = sprintf("%01.2f", ($maxProgress - $hardcoreProgress) * 100 / $maxProgress);
+            $softcoreClass = 'rounded';
+        } elseif ($softcoreProgress > 0) {
+            $softcoreProgressBarWidth = sprintf("%01.2f", ($softcoreProgress - $hardcoreProgress) * 100 / $maxProgress);
+            $softcoreClass = 'rounded-l';
+        }
+
+        if ($hardcoreProgress >= $maxProgress) {
+            $hardcoreProgressBarWidth = '100.0';
+            $hardcoreClass = 'rounded';
+        } elseif ($hardcoreProgress > 0) {
+            $hardcoreProgressBarWidth = sprintf("%01.2f", $hardcoreProgress * 100 / $maxProgress);
+            $hardcoreClass = 'rounded-l';
+        }
     }
 ?>
 <div role="progressbar" aria-valuemin="0" aria-valuemax="100"
     title="{{ $softcoreProgress }} of {{ $maxProgress }} unlocked"
     class="w-full h-1 bg-embed rounded flex">
+    @if ($hardcoreProgress > 0)
     <div style="width: {{ $hardcoreProgressBarWidth }}%"
-         class="bg-[#cc9900] h-full {{ $hardcoreProgressBarWidth > 0 ? 'rounded-l' : '' }}"></div>
+         class="bg-[#cc9900] h-full {{ $hardcoreClass }}"></div>
+    @endif
+    @if ($softcoreProgress > $hardcoreProgress)
     <div style="width: {{ $softcoreProgressBarWidth }}%"
-         class="bg-[rgb(11,113,193)] h-full {{ $hardcoreProgressBarWidth === 0 ? 'rounded-l' : '' }}"></div>
+         class="bg-[rgb(11,113,193)] h-full {{ $softcoreClass }}"></div>
+    @endif
 </div>
