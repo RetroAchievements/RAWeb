814d9a432c68318121c7168742bd9a27d6cdce1f
Jamiras <jamirasmt@gmail.com>
2024-01-07 19:57:00 -0700

    use overflow-hidden to clip bars to container

--- resources/views/components/hardcore-progress.blade.php
+++ resources/views/components/hardcore-progress.blade.php
@@ -6,35 +6,30 @@
 ])
 <?php
     $hardcoreProgressBarWidth = $softcoreProgressBarWidth = '0';
-    $hardcoreClass = $softcoreClass = '';
 
     if ($maxProgress > 0) {
         if ($softcoreProgress >= $maxProgress) {
             $softcoreProgressBarWidth = sprintf("%01.2f", ($maxProgress - $hardcoreProgress) * 100 / $maxProgress);
-            $softcoreClass = ($hardcoreProgress === 0) ? 'rounded' : 'rounded-r';
         } elseif ($softcoreProgress > 0) {
             $softcoreProgressBarWidth = sprintf("%01.2f", ($softcoreProgress - $hardcoreProgress) * 100 / $maxProgress);
-            $softcoreClass = ($hardcoreProgress === 0) ? 'rounded-l' : '';
         }
 
         if ($hardcoreProgress >= $maxProgress) {
             $hardcoreProgressBarWidth = '100.0';
-            $hardcoreClass = 'rounded';
         } elseif ($hardcoreProgress > 0) {
             $hardcoreProgressBarWidth = sprintf("%01.2f", $hardcoreProgress * 100 / $maxProgress);
-            $hardcoreClass = 'rounded-l';
         }
     }
 ?>
-<div role="progressbar" aria-valuemin="0" aria-valuemax="{{ $maxProgress }}"
-    title="{{ $softcoreProgress }} of {{ $maxProgress }} unlocked"
-    class="w-full h-1 bg-embed rounded flex">
+<div role="progressbar" class="w-full h-1 bg-embed rounded flex overflow-hidden"
+    aria-valuemin="0" aria-valuemax="{{ $maxProgress }}" aria-valuenow="{{ $softcoreProgress }}"
+    title="{{ $tooltip }}">
     @if ($hardcoreProgress > 0)
     <div style="width: {{ $hardcoreProgressBarWidth }}%"
-         class="bg-[#cc9900] h-full {{ $hardcoreClass }}"></div>
+         class="bg-[#cc9900] h-full"></div>
     @endif
     @if ($softcoreProgress > $hardcoreProgress)
     <div style="width: {{ $softcoreProgressBarWidth }}%"
-         class="bg-[rgb(11,113,193)] h-full {{ $softcoreClass }}"></div>
+         class="bg-[rgb(11,113,193)] h-full"></div>
     @endif
 </div>
