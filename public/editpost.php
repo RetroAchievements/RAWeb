5236f8d3b953310b1ff41dbd16c0bd6380944e12
Graham Blair <graham@grahamarthurblair.com>
2024-01-09 18:41:25 -0800

    Add margin to top of the loading bar when previewing in the editpost page

--- public/editpost.php
+++ public/editpost.php
@@ -80,7 +80,7 @@ RenderContentStart("Edit post");
     HTML;
     echo "</td></tr>";
 
-    $loadingIcon = Blade::render('<x-fas-spinner id="preview-loading-icon" class="animate-spin opacity-0 transition-all duration-200" aria-hidden="true" />');
+    $loadingIcon = Blade::render('<x-fas-spinner id="preview-loading-icon" class="animate-spin opacity-0 transition-all duration-200 mt-1" aria-hidden="true" />');
 
     echo <<<HTML
         <tr>
