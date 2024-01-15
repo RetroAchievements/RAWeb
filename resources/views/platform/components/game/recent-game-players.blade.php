19cec66e24e3689a02e22514346b2c3bba6f72db
Jamiras <jamirasmt@gmail.com>
2024-01-15 13:09:26 -0700

    add progress to recent players list on game pages

--- resources/views/platform/components/game/recent-game-players.blade.php
+++ resources/views/platform/components/game/recent-game-players.blade.php
@@ -17,6 +17,9 @@ foreach ($recentPlayerData as $recentPlayer) {
         'Date' => $date,
         'IsBroken' => $isBroken,
         'Activity' => $recentPlayer['Activity'],
+        'NumAwarded' => $recentPlayer['NumAwarded'],
+        'NumAwardedHardcore' => $recentPlayer['NumAwardedHardcore'],
+        'NumAchievements' => $recentPlayer['NumAchievements'],
     ];
 }
 ?>
@@ -28,7 +31,15 @@ foreach ($recentPlayerData as $recentPlayer) {
         <div class="flex flex-col gap-y-0.5 px-2 py-1.5 odd:bg-embed">
             <div class="w-full flex items-center justify-between">
                 {!! userAvatar($recentPlayer['User'], iconClass: 'rounded-sm', iconSize: 20) !!}
-                <p class="smalldate">{{ $recentPlayer['Date'] }}</p>
+                <div class="flex gap-x-1 items-center">
+                    <!-- TODO: use game-progress-bar after 2153 is merged -->
+                    <x-hardcore-progress
+                        softcoreProgress="{{ $recentPlayer['NumAwarded'] }}"
+                        hardcoreProgress="{{ $recentPlayer['NumAwardedHardcore'] }}"
+                        maxProgress="{{ $recentPlayer['NumAchievements'] }}"
+                    />
+                    <p class="smalldate whitespace-nowrap">{{ $recentPlayer['Date'] }}</p>
+                </div>
             </div>
 
             @if ($recentPlayer['IsBroken'])
@@ -48,6 +59,7 @@ foreach ($recentPlayerData as $recentPlayer) {
         <tr>
             <th>Player</th>
             <th>When</th>
+            <th style="width: 10%">Progress</th>
             <th class="w-full">Activity</th>
         </tr>
     </thead>
@@ -57,6 +69,16 @@ foreach ($recentPlayerData as $recentPlayer) {
             <tr>
                 <td class="py-2.5">{!! userAvatar($recentPlayer['User'], iconClass: 'rounded-sm mr-1', iconSize: 28) !!}</td>
                 <td class="whitespace-nowrap smalldate">{{ $recentPlayer['Date'] }}</td>
+
+                <td>
+                    <!-- TODO: use game-progress-bar after 2153 is merged -->
+                    <x-hardcore-progress
+                        softcoreProgress="{{ $recentPlayer['NumAwarded'] }}"
+                        hardcoreProgress="{{ $recentPlayer['NumAwardedHardcore'] }}"
+                        maxProgress="{{ $recentPlayer['NumAchievements'] }}"
+                    />
+                </td>
+
                 <td>
                     @if ($recentPlayer['IsBroken'])
                         <div class="cursor-help" title="{{ $recentPlayer['Activity'] }}">
