d7e73eed15d01e6cb3bcba8ceaa21f9c3a1d56bd
luchaos <luchaos@gmail.com>
2024-01-26 19:15:11 +0100

    fix forum verification not being tied to user moderation

--- resources/views/components/menu/management.blade.php
+++ resources/views/components/menu/management.blade.php
@@ -46,7 +46,7 @@ $tools = $settings['tools'] ?? null;
                     {{--<x-dropdown-item :link="route('news.index')">{{ __res('news') }}</x-dropdown-item>--}}
                     <x-dropdown-item :link="url('submitnews.php')">{{ __res('news') }}</x-dropdown-item>
                 @endcan
-                @can('manage', App\Community\Models\Forum::class)
+                @can('manage', App\Site\Models\User::class)
                     {{--<x-dropdown-item :link="route('forum-topic.verify')">Forum Verification</x-dropdown-item>--}}
                     <x-dropdown-item :link="url('viewforum.php?f=0')">Forum Verification</x-dropdown-item>
                 @endcan
