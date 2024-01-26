ebcb8c31b7b1ca6a1314591bc09bc17fc86845ae
luchaos <luchaos@gmail.com>
2024-01-26 15:49:43 +0100

    add model events tests

---
+++ tests/Feature/Platform/AchievementTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Platform;
+
+use App\Platform\Enums\AchievementFlag;
+use App\Platform\Enums\AchievementPoints;
+use App\Platform\Enums\AchievementType;
+use App\Platform\Events\AchievementCreated;
+use App\Platform\Events\AchievementMoved;
+use App\Platform\Events\AchievementPointsChanged;
+use App\Platform\Events\AchievementPublished;
+use App\Platform\Events\AchievementTypeChanged;
+use App\Platform\Events\AchievementUnpublished;
+use App\Platform\Models\Achievement;
+use App\Platform\Models\Game;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class AchievementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testItRendersListWithEmptyDatabase(): void
+    {
+        $this->get('achievementList.php')->assertSuccessful();
+    }
+
+    public function testItDispatchedModelEventsOnSpecificAttributeChanges(): void
+    {
+        Event::fake([
+            AchievementCreated::class,
+            AchievementPublished::class,
+            AchievementUnpublished::class,
+            AchievementPointsChanged::class,
+            AchievementTypeChanged::class,
+            AchievementMoved::class,
+        ]);
+
+        $game = Game::factory()->create();
+
+        $achievement = Achievement::factory()->for($game)->create();
+        Event::assertDispatched(AchievementCreated::class);
+
+        $achievement->Flags = AchievementFlag::OfficialCore;
+        $achievement->save();
+        Event::assertDispatched(AchievementPublished::class);
+
+        $achievement->Flags = AchievementFlag::Unofficial;
+        $achievement->save();
+        Event::assertDispatched(AchievementUnpublished::class);
+
+        $achievement->Points = AchievementPoints::cases()[4];
+        $achievement->save();
+        Event::assertDispatched(AchievementPointsChanged::class);
+
+        $achievement->type = AchievementType::Progression;
+        $achievement->save();
+        Event::assertDispatched(AchievementTypeChanged::class);
+
+        $achievement->type = AchievementType::WinCondition;
+        $achievement->save();
+        Event::assertDispatched(AchievementTypeChanged::class);
+
+        $game2 = Game::factory()->create();
+        $achievement->GameID = $game2->id;
+        $achievement->save();
+        Event::assertDispatched(AchievementMoved::class);
+    }
+}
