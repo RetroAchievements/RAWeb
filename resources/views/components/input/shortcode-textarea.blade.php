18f367f4baa2648908b46047ba155e5155297d22
Wes Copeland <wlcopeland1@gmail.com>
2024-02-01 21:07:18 -0500

    perf: add .animate-spin just-in-time (#2211)

--- resources/views/components/input/shortcode-textarea.blade.php
+++ resources/views/components/input/shortcode-textarea.blade.php
@@ -40,7 +40,7 @@ $buttonEnabled = $enabled ? ":disabled='!isValid'" : "disabled";
         <span class="textarea-counter" data-textarea-id="commentTextarea">0 / 60000</span>
 
         <div>
-            <x-fas-spinner id="preview-loading-icon" class="animate-spin opacity-0 transition-all duration-200" aria-hidden="true" />
+            <x-fas-spinner id="preview-loading-icon" class="opacity-0 transition-all duration-200" aria-hidden="true" />
             <button id="preview-button" type="button" class="btn" onclick="window.loadPostPreview()" {!! $buttonEnabled !!}>Preview</button>
             <button id="postBtn" class="btn" onclick="this.form.submit(); disableRepost();" {!! $buttonEnabled !!}>Submit</button>
         </div>
