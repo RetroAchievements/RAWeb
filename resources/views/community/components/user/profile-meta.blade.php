34ecf47f51406c69dd509fb2f5328cb555946b33
Wes Copeland <wlcopeland1@gmail.com>
2024-02-03 13:58:04 -0500

    feat(user): make User Stats collapsible

--- resources/views/community/components/user/profile-meta.blade.php
+++ resources/views/community/components/user/profile-meta.blade.php
@@ -19,6 +19,8 @@ use App\Site\Enums\Permissions;
 
 $registeredPermission = Permissions::Registered;
 $jrDevPermission = Permissions::JuniorDeveloper;
+
+$isUserStatsDefaultExpanded = request()->cookie('prefers_hidden_user_profile_stats') !== 'true';
 ?>
 
 <div class="relative">
@@ -30,7 +32,7 @@ $jrDevPermission = Permissions::JuniorDeveloper;
     />
 
     @if ($userMassData['Permissions'] >= $registeredPermission)
-        <div class="flex gap-x-1 items-center mb-2 top-0.5 right-0 sm:hidden md:flex md:flex-col md:items-end md:gap-y-1 md:absolute lg:hidden xl:flex xl:absolute">
+        <div class="flex gap-x-1 items-center mt-2 mb-2 top-5 right-0 sm:hidden md:mt-0 md:flex md:flex-col md:items-end md:gap-y-1 md:absolute lg:hidden xl:flex xl:absolute">
             <x-user.profile.social-interactivity :username="$username" />
             <x-user.profile.follows-you-label :username="$username" />
         </div>
@@ -43,28 +45,67 @@ $jrDevPermission = Permissions::JuniorDeveloper;
     <x-user.profile.last-seen-in :userMassData="$userMassData" />
 @endif
 
-<x-user.profile.player-stats
-    :averageCompletionPercentage="$averageCompletionPercentage"
-    :averageFinishedGames="$averageFinishedGames"
-    :averagePointsPerWeek="$averagePointsPerWeek"
-    :hardcoreRankMeta="$hardcoreRankMeta"
-    :recentPointsEarned="$recentPointsEarned"
-    :softcoreRankMeta="$softcoreRankMeta"
-    :totalHardcoreAchievements="$totalHardcoreAchievements"
-    :totalSoftcoreAchievements="$totalSoftcoreAchievements"
-    :userMassData="$userMassData"
-/>
+<div x-data="{
+    isExpanded: {{ $isUserStatsDefaultExpanded ? 'true' : 'false' }},
+    handleToggle() {
+        const newValue = !this.isExpanded;
 
-@if (!empty($developerStats))
-    <x-user.profile.developer-stats
-        :developerStats="$developerStats"
-        :userClaims="$userClaims"
-        :userMassData="$userMassData"
-        :username="$username"
-    />
-@endif
+        this.isExpanded = newValue;
+        window.setCookie('prefers_hidden_user_profile_stats', String(!newValue));
+    }
+}">
+    <div>
+        <div class="flex w-full justify-between items-center mb-3">
+            <h2 class="text-h4 !mb-0">User Stats</h2>
 
-<x-user.profile.social-stats
-    :socialStats="$socialStats"
-    :username="$username"
-/>
+            <button @click="handleToggle" class="btn transition-transform lg:active:scale-95 duration-75">
+                <div
+                    class="transition-transform @if ($isUserStatsDefaultExpanded) rotate-180 @endif"
+                    :class="{ 'rotate-180': isExpanded }"
+                >
+                    <x-fas-chevron-down />
+                </div>
+            </button>
+        </div>
+    </div>
+
+    <div
+        @if (!$isUserStatsDefaultExpanded) x-cloak @endif
+        x-show="isExpanded"
+        x-transition:enter="ease-in-out duration-300"
+        x-transition:enter-start="opacity-0 max-h-0 -translate-y-1.5 overflow-hidden"
+        x-transition:enter-end="opacity-1 max-h-[1000px] translate-y-0 overflow-hidden"
+        x-transition:leave="ease-in-out duration-200"
+        x-transition:leave-start="opacity-1 max-h-[1000px] overflow-hidden"
+        x-transition:leave-end="opacity-0 max-h-0 overflow-hidden"
+        class="transition-all"
+    >
+        <div class="pb-6">
+            <x-user.profile.player-stats
+                :averageCompletionPercentage="$averageCompletionPercentage"
+                :averageFinishedGames="$averageFinishedGames"
+                :averagePointsPerWeek="$averagePointsPerWeek"
+                :hardcoreRankMeta="$hardcoreRankMeta"
+                :recentPointsEarned="$recentPointsEarned"
+                :softcoreRankMeta="$softcoreRankMeta"
+                :totalHardcoreAchievements="$totalHardcoreAchievements"
+                :totalSoftcoreAchievements="$totalSoftcoreAchievements"
+                :userMassData="$userMassData"
+            />
+
+            @if (!empty($developerStats))
+                <x-user.profile.developer-stats
+                    :developerStats="$developerStats"
+                    :userClaims="$userClaims"
+                    :userMassData="$userMassData"
+                    :username="$username"
+                />
+            @endif
+
+            <x-user.profile.social-stats
+                :socialStats="$socialStats"
+                :username="$username"
+            />
+        </div>
+    </div>
+</div>
