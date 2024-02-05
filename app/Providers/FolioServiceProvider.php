9d41dc57fbdb5983c369270407ffd85dbcbab864
luchaos <luchaos@gmail.com>
2024-02-05 02:29:03 +0100

    add Folio

---
+++ app/Providers/FolioServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Folio\Folio;
+
+class FolioServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Folio::path(resource_path('views/pages'))->middleware([
+            '*' => [
+                'web',
+                'csp',
+            ],
+        ]);
+    }
+}
