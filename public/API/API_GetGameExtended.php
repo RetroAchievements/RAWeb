22de9c428aad52eb1f1429ec0cfe4bfd569a6a5f
drisc <cory@drisc.io>
2024-02-05 04:17:12 +0000

    chore: lint

--- public/API/API_GetGameExtended.php
+++ public/API/API_GetGameExtended.php
@@ -63,16 +63,16 @@ $flag = (int) request()->query('f', (string) AchievementFlag::OfficialCore);
 if (!function_exists('getParentGameId')) {
     function getParentGameId(string $title, int $consoleID, int $gameID): ?int
     {
-    
+
         $result = Game::where('Title', 'LIKE', '%' . $title . '%')->where('ConsoleID', '=', $consoleID)->first();
-    
+
         if ($result->Title = $title) {
             return null;
         } else {
             $matchValue = $result->Title;
             $remainingValue = preg_replace('/(?:^|\s)\[.*Subset - .*\](?:\s|$)/', '', $matchValue);
             $remainingResult = Game::where('Title', $remainingValue)->first();
-    
+
             return (int) $remainingResult->ID ? $remainingResult->ID : null;
         }
     }
