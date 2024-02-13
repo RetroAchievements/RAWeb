eca58ca51055f1edd0fc3e0289c83c365b7a64aa
luchaos <luchaos@gmail.com>
2024-02-13 22:13:06 +0100

    Rename `targetElementId` to `target` in injectShortcode() parameters

--- resources/js/utils/injectShortcode.ts
+++ resources/js/utils/injectShortcode.ts
@@ -3,12 +3,12 @@
  * selected text with the forum shortcode. If there's no selection, the
  * shortcode is inserted at the end of the message.
  *
- * @param target The id of the target textarea.
+ * @param targetElementId The id of the target textarea.
  * @param start The opening tag of the shortcode.
  * @param end The closing tag of the shortcode. Defaults to an empty string.
  */
-export function injectShortcode(target: string, start: string, end = ''): void {
-  const commentTextarea = document.getElementById(target) as HTMLTextAreaElement;
+export function injectShortcode(targetElementId: string, start: string, end = ''): void {
+  const commentTextarea = document.getElementById(targetElementId) as HTMLTextAreaElement;
 
   if (!commentTextarea) {
     return;
