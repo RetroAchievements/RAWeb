ce0db58269bec23b49afb49d6bca7fa6d742d42c
Wes Copeland <wlcopeland1@gmail.com>
2024-03-11 17:36:43 -0400

    chore: delete more unneeded css

--- resources/views/components/user/progression-status/legend.blade.php
+++ resources/views/components/user/progression-status/legend.blade.php
@@ -11,28 +11,28 @@
     </div>
 
     @if ($totalBeatenSoftcoreCount > 0)
-        <div class="sm:order-2">
+        <div>
             <div class="rounded-full w-2 h-2 border border-zinc-400"></div>
             <p class="text-zinc-400 font-bold text-xs">Beaten Softcore</p>
         </div>
     @endif
 
     @if ($totalBeatenHardcoreCount > 0 || ($totalBeatenHardcoreCount === 0 && $totalBeatenSoftcoreCount === 0))
-        <div class="sm:order-3">
+        <div>
             <div class="rounded-full w-2 h-2 bg-zinc-300"></div>
             <p class="text-zinc-300 font-bold text-xs">Beaten</p>
         </div>
     @endif
 
     @if ($totalCompletedCount > 0)
-        <div class="sm:order-4">
+        <div>
             <div class="rounded-full w-2 h-2 border border-yellow-600"></div>
             <p class="text-yellow-600 font-bold text-xs">Completed</p>
         </div>
     @endif
 
     @if ($totalMasteredCount > 0 || ($totalMasteredCount === 0 && $totalCompletedCount === 0))
-        <div class="sm:order-5">
+        <div>
             <div class="rounded-full w-2 h-2 bg-yellow-600"></div>
             <p class="text-[gold] light:text-yellow-600 font-bold text-xs">Mastered</p>
         </div>
