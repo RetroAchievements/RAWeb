073fe30364649f5fb7d352ba592ec26e49916899
Wes Copeland <wlcopeland1@gmail.com>
2024-03-26 18:03:20 -0400

    refactor(Subscription): rename columns and adjust primary/foreign keys (#2298)

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -5,12 +5,36 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Support\Database\Eloquent\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Subscription extends BaseModel
 {
-    // TODO rename Subscription to subscriptions
-    protected $table = 'Subscription';
+    protected $table = 'subscriptions';
 
-    public const CREATED_AT = 'Created';
-    public const UPDATED_AT = 'Updated';
+    protected $fillable = [
+        'subject_type',
+        'subject_id',
+        'user_id',
+        'state',
+    ];
+
+    protected $casts = [
+        'state' => 'boolean',
+    ];
+
+    // == accessors
+
+    // == mutators
+
+    // == relations
+
+    /**
+     * @return BelongsTo<User, Subscription>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'ID');
+    }
+
+    // == scopes
 }
