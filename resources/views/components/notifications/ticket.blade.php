0424fa04da2c9a0d5d811c5987e0bc26550775f6
drisc <cory@drisc.io>
2024-04-24 15:16:12 +0100

    feat(notifications): split ticket notifications into a separate nav item (#2373)

--- resources/views/components/notifications/ticket.blade.php
+++ resources/views/components/notifications/ticket.blade.php
@@ -1,10 +1,19 @@
+@props([
+    'trigger' => '',
+    'count' => 0,
+    'notifications' => null, //Collection
+    'link' => '',
+    'class' => '',
+    'title' => '',
+])
+
 <x-nav-dropdown
     :class="$class ?? ''"
     dropdown-class="dropdown-menu-right dropdown-items-right"
     :title="__res('notification')"
 >
     <x-slot name="trigger">
-        <x-fas-bell />
+        <x-fas-ticket />
         @if($count ?? 0)
             <div class="text-danger absolute translate-x-3 -translate-y-1 text-[8px]">
                 <x-fas-circle />
@@ -18,11 +27,6 @@
             </x-dropdown-item>
         @endforeach
     @else
-        <div class="flex whitespace-nowrap text-muted px-3 py-2">{{ __('No new notifications') }}</div>
+        <div class="flex whitespace-nowrap text-muted px-3 py-2">{{ __('No open tickets or requests') }}</div>
     @endif
-    {{--<x-dropdown-item>--}}
-    {{--<a class="nav-link" href="{{ route('notification.index') }}" title="{{ __res('notification') }}" data-toggle="tooltip">--}}
-    {{-- All notifications --}}
-    {{--</a>--}}
-    {{--</x-dropdown-item>--}}
 </x-nav-dropdown>
