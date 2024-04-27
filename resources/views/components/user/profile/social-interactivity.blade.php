20536cdd05a1356ee6783b9a601e0d3b149d249a
Wes Copeland <wlcopeland1@gmail.com>
2024-04-27 10:10:52 -0400

    refactor(UserRelationship): migrate to eloquent ORM, read from user ids (#2380)

--- resources/views/components/user/profile/social-interactivity.blade.php
+++ resources/views/components/user/profile/social-interactivity.blade.php
@@ -4,37 +4,33 @@ use App\Community\Enums\UserRelationship;
 ?>
 
 @props([
-    'username' => '',
+    'user' => null, // User
 ])
 
-<?php
-$me = auth()->user() ?? null;
+@php
+    $me = auth()->user() ?? null;
+    $myFriendshipType = $me ? $me->getRelationship($user) : UserRelationship::NotFollowing;
+@endphp
 
-$myFriendshipType = UserRelationship::NotFollowing;
-if ($me) {
-    $myFriendshipType = GetFriendship($me->User, $username);
-}
-?>
-
-@if ($me && $me->User !== $username)
+@if ($me && $me->User !== $user->User)
     <div class="flex items-center gap-x-1">
         @if ($myFriendshipType !== UserRelationship::Blocked)
             <form action="/request/user/update-relationship.php" method="POST">
                 {!! csrf_field() !!}
-                <input type="hidden" name="user" value="{{ $username }}">
+                <input type="hidden" name="user" value="{{ $user->User }}">
                 <input type="hidden" name="action" value="{{ UserRelationship::Blocked }}">
                 <button class="btn btn-link">Block</button>
             </form>
         @else
             <form action="/request/user/update-relationship.php" method="POST">
                 {!! csrf_field() !!}
-                <input type="hidden" name="user" value="{{ $username }}">
+                <input type="hidden" name="user" value="{{ $user->User }}">
                 <input type="hidden" name="action" value="{{ UserRelationship::NotFollowing }}">
                 <button class="btn btn-link">Unblock</button>
             </form>
         @endif
 
-        <a class="btn max-h-[24px] flex items-center" href="{{ route('message.create', ['to' => $username]) }}">
+        <a class="btn max-h-[24px] flex items-center" href="{{ route('message.create', ['to' => $user->User]) }}">
             <x-fas-envelope />
             <span class="sr-only">Message</span>
         </a>
@@ -42,14 +38,14 @@ if ($me) {
         @if ($myFriendshipType === UserRelationship::Following)
             <form action="/request/user/update-relationship.php" method="POST">
                 {!! csrf_field() !!}
-                <input type="hidden" name="user" value="{{ $username }}">
+                <input type="hidden" name="user" value="{{ $user->User }}">
                 <input type="hidden" name="action" value="{{ UserRelationship::NotFollowing }}">
                 <button class="btn btn max-h-[24px] flex items-center">Unfollow</button>
             </form>
         @elseif ($myFriendshipType === UserRelationship::NotFollowing)
             <form action="/request/user/update-relationship.php" method="POST">
                 {!! csrf_field() !!}
-                <input type="hidden" name="user" value="{{ $username }}">
+                <input type="hidden" name="user" value="{{ $user->User }}">
                 <input type="hidden" name="action" value="{{ UserRelationship::Following }}">
                 <button class="btn max-h-[24px] flex items-center">
                     Follow
