936ee9b16fe1c34a5fa01da7a5104938ffc49cf8
Wes Copeland <wlcopeland1@gmail.com>
2024-05-14 20:28:17 -0400

    chore: fix tests

--- database/migrations/platform/2024_05_13_000000_update_codenotes_table.php
+++ database/migrations/platform/2024_05_13_000000_update_codenotes_table.php
@@ -11,10 +11,17 @@ return new class() extends Migration {
     {
         Schema::rename('CodeNotes', 'memory_notes');
 
+        // SQLite doesn't support multiple calls to dropColumn / renameColumn in a single modification.
         Schema::table('memory_notes', function (Blueprint $table) {
             $table->renameColumn('Address', 'address');
+        });
+        Schema::table('memory_notes', function (Blueprint $table) {
             $table->renameColumn('Note', 'body');
+        });
+        Schema::table('memory_notes', function (Blueprint $table) {
             $table->renameColumn('Created', 'created_at');
+        });
+        Schema::table('memory_notes', function (Blueprint $table) {
             $table->renameColumn('Updated', 'updated_at');
         });
     }
