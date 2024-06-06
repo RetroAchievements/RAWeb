0be9716ef2374a4153e9dba3182385e10c18b9d8
Wes Copeland <wlcopeland1@gmail.com>
2024-06-06 19:58:22 -0400

    fix: address pr feedback

--- app/Platform/Commands/DeleteStalePlayerPointsStatsEntries.php
+++ app/Platform/Commands/DeleteStalePlayerPointsStatsEntries.php
@@ -7,14 +7,19 @@ namespace App\Platform\Commands;
 use App\Models\PlayerStat;
 use App\Platform\Enums\PlayerStatType;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 
 class DeleteStalePlayerPointsStatsEntries extends Command
 {
-    protected $signature = 'ra:platform:player:delete-stale-points-stats';
-    protected $description = 'Delete any player points stats with values of 0';
+    protected $signature = 'ra:platform:player:delete-stale-points-stats
+                            {days=7 : The number of days to keep}';
+    protected $description = 'Delete any player points stats with values of 0 older than the specified number of days';
 
     public function handle(): void
     {
+        $days = (int) $this->argument('days');
+        $dateThreshold = Carbon::now()->subDays($days);
+
         $relevantPlayerStatTypes = [
             PlayerStatType::PointsHardcoreDay,
             PlayerStatType::PointsHardcoreWeek,
@@ -25,6 +30,7 @@ class DeleteStalePlayerPointsStatsEntries extends Command
         ];
         PlayerStat::whereIn('type', $relevantPlayerStatTypes)
             ->where('value', 0)
+            ->where('created_at', '<', $dateThreshold)
             ->delete();
     }
 }
