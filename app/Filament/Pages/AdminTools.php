4aba084e2c307d49b71a80c83ff3579a09e76244
Wes Copeland <wlcopeland1@gmail.com>
2024-07-08 20:00:03 -0400

    feat(manage): migrate Most Reported Games to filament

--- app/Filament/Pages/AdminTools.php
+++ app/Filament/Pages/AdminTools.php
@@ -4,26 +4,27 @@ declare(strict_types=1);
 
 namespace App\Filament\Pages;
 
-use App\Enums\Permissions;
 use App\Models\Role;
 use App\Models\User;
 use Filament\Pages\Page;
 
-class Tools extends Page
+class AdminTools extends Page
 {
-    protected static ?int $navigationSort = 99;
+    protected static ?int $navigationSort = 1;
 
     protected static ?string $navigationIcon = 'heroicon-s-wrench';
 
-    protected static string $view = 'filament.pages.tools';
+    protected static ?string $navigationGroup = 'Tools';
+
+    protected static ?string $title = 'Admin';
+
+    protected static string $view = 'filament.pages.admin-tools';
 
     public static function canAccess(): bool
     {
         /** @var User $user */
         $user = auth()->user();
 
-        return
-            $user->hasAnyRole([Role::MODERATOR, Role::ADMINISTRATOR])
-            || $user->getAttribute('Permissions') >= Permissions::Moderator;
+        return $user->hasAnyRole([Role::MODERATOR, Role::ADMINISTRATOR]);
     }
 }
--- app/Models/Game.php
+++ app/Models/Game.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 use Laravel\Scout\Searchable;
@@ -387,6 +388,14 @@ class Game extends BaseModel implements HasComments, HasMedia
     public function visibleLeaderboards(): HasMany
     {
         return $this->leaderboards()->visible();
+    }
+
+    /**
+     * @return HasManyThrough<Ticket>
+     */
+    public function tickets(): HasManyThrough
+    {
+        return $this->hasManyThrough(Ticket::class, Achievement::class, 'GameID', 'AchievementID');
     }
 
     // == scopes
