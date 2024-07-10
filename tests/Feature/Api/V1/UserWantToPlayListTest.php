d0216aec82fe62df34c8f0e5334c26facbaf0751
iOSLife <developer@ioslife.dev>
2024-07-10 11:29:24 -0400

    adding offset test

--- tests/Feature/Api/V1/UserWantToPlayListTest.php
+++ tests/Feature/Api/V1/UserWantToPlayListTest.php
@@ -214,4 +214,147 @@ class UserWantToPlayListTest extends TestCase
                 ],
             ]);
     }
+    
+
+    public function testGetUserWantToPlayListOffset(): void
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        /** Set up a user with 10 games on Want to Play List: */
+
+        /** @var User $user */
+        $user = User::factory()->create(['User' => 'myUser']);
+
+        /** @var System $system */
+        $system = System::factory()->create();
+
+        /** @var Game $gameOne */
+        $gameOne = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameOne->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameTwo */
+        $gameTwo = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameTwo->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameThree */
+        $gameThree = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameThree->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameFour */
+        $gameFour = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameFour->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameFive */
+        $gameFive = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameFive->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameSix */
+        $gameSix = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameSix->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameSeven */
+        $gameSeven = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameSeven->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameEight */
+        $gameEight = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameEight->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameNine */
+        $gameNine = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameNine->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        /** @var Game $gameTen */
+        $gameTen = Game::factory()->create(['ConsoleID' => $system->ID]);
+        UserGameListEntry::create([
+            'user_id' => $user->id,
+            'GameID' => $gameTen->ID,
+            'type' => UserGameListType::Play,
+        ]);
+
+        $this->get($this->apiUrl('GetUserWantToPlayList', ['u' => $user->User, 'o' => 5]))
+            ->assertSuccessful()
+            ->assertJson([
+                'Count' => 5,
+                'Total' => 5,
+                'Results' => [
+                    [
+                        "ID" => $gameSix->ID,
+                        "Title" => $gameSix->Title,
+                        "ImageIcon" => $gameSix->ImageIcon,
+                        "ConsoleID" => $system->ID,
+                        "PointsTotal" => $gameSix->points_total,
+                        'AchievementsPublished' => $gameSix->achievements_published,
+                    ],
+                    [
+                        "ID" => $gameSeven->ID,
+                        "Title" => $gameSeven->Title,
+                        "ImageIcon" => $gameSeven->ImageIcon,
+                        "ConsoleID" => $system->ID,
+                        "PointsTotal" => $gameSeven->points_total,
+                        'AchievementsPublished' => $gameSeven->achievements_published,
+                    ],
+                    [
+                        "ID" => $gameEight->ID,
+                        "Title" => $gameEight->Title,
+                        "ImageIcon" => $gameEight->ImageIcon,
+                        "ConsoleID" => $system->ID,
+                        "PointsTotal" => $gameEight->points_total,
+                        'AchievementsPublished' => $gameEight->achievements_published,
+                    ],
+                    [
+                        "ID" => $gameNine->ID,
+                        "Title" => $gameNine->Title,
+                        "ImageIcon" => $gameNine->ImageIcon,
+                        "ConsoleID" => $system->ID,
+                        "PointsTotal" => $gameNine->points_total,
+                        'AchievementsPublished' => $gameNine->achievements_published,
+                    ],
+                    [
+                        "ID" => $gameTen->ID,
+                        "Title" => $gameTen->Title,
+                        "ImageIcon" => $gameTen->ImageIcon,
+                        "ConsoleID" => $system->ID,
+                        "PointsTotal" => $gameTen->points_total,
+                        'AchievementsPublished' => $gameTen->achievements_published,
+                    ],
+                ],
+            ]);
+    }
 }
