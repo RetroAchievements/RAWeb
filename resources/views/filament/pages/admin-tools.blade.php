10b477cbc059024a3141c6aae1012d6e2eb796ba
Wes Copeland <wlcopeland1@gmail.com>
2024-07-21 12:11:57 -0400

    feat(manage): migrate Get Achievement Unlocks tool to filament (#2544)

--- resources/views/filament/pages/admin-tools.blade.php
+++ resources/views/filament/pages/admin-tools.blade.php
@@ -1,12 +1,22 @@
 <x-filament-panels::page>
-    <x-filament::grid :xl="2">
+    <x-filament::grid :xl="2" class="gap-6">
         <x-filament::section>
             <div class="flex flex-col gap-y-4">
                 <x-filament::section.heading>
                     Get Game Achievement IDs
                 </x-filament::section.heading>
 
-                <livewire:filament-tools.get-game-achievement-ids />
+                <livewire:administrative-tools.get-game-achievement-ids />
+            </div>
+        </x-filament::section>
+
+        <x-filament::section>
+            <div class="flex flex-col gap-y-4">
+                <x-filament::section.heading>
+                    Get Users Who Have Unlocked Achievement ID
+                </x-filament::section.heading>
+
+                <livewire:administrative-tools.get-users-who-have-unlocked-achievement-id />
             </div>
         </x-filament::section>
     </x-filament::grid>
