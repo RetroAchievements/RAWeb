0d8a2b0dfaff3b66eb3718a526e26d50a902fab4
Wes Copeland <wlcopeland1@gmail.com>
2024-07-21 16:33:42 -0400

    feat(manage): add memory notes management to game resources

--- app/Models/MemoryNote.php
+++ app/Models/MemoryNote.php
@@ -7,10 +7,16 @@ namespace App\Models;
 use App\Support\Database\Eloquent\BaseModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class MemoryNote extends BaseModel
 {
     use SoftDeletes;
+
+    use LogsActivity {
+        LogsActivity::activities as auditLog;
+    }
 
     // TODO drop game_id, migrate to game_hash_set_id
     protected $table = 'memory_notes';
@@ -28,7 +34,24 @@ class MemoryNote extends BaseModel
         'body',
     ];
 
+    // == logging
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly([
+                'user_id',
+            ])
+            ->dontSubmitEmptyLogs();
+    }
+
     // == accessors
+
+    public function getAddressHexAttribute(): string
+    {
+        // 16399 -> "0x00400f"
+        return '0x' . str_pad(dechex($this->address), 6, '0', STR_PAD_LEFT);
+    }
 
     // == mutators
 
@@ -49,7 +72,7 @@ class MemoryNote extends BaseModel
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id', 'ID');
     }
 
     // == scopes
