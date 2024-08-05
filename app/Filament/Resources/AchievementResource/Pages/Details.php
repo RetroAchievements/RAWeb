1c8cc026423d03e064ceb98e3a731cce460f84ff
Wes Copeland <wlcopeland1@gmail.com>
2024-08-05 19:04:51 -0400

    feat(manage): improve achievement breadcrumbs usability

--- app/Filament/Resources/AchievementResource/Pages/Details.php
+++ app/Filament/Resources/AchievementResource/Pages/Details.php
@@ -5,12 +5,27 @@ declare(strict_types=1);
 namespace App\Filament\Resources\AchievementResource\Pages;
 
 use App\Filament\Resources\AchievementResource;
+use App\Models\Achievement;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class Details extends ViewRecord
 {
     protected static string $resource = AchievementResource::class;
+
+    public function getBreadcrumbs(): array
+    {
+        /** @var Achievement $achievement */
+        $achievement = $this->record;
+        $game = $achievement->game;
+
+        return [
+            route('filament.admin.resources.games.index') => 'Games',
+            route('filament.admin.resources.games.view', $game) => $game->title,
+            route('filament.admin.resources.achievements.view', $achievement) => $achievement->title,
+            'View',
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
--- app/Filament/Resources/AchievementResource/Pages/Edit.php
+++ app/Filament/Resources/AchievementResource/Pages/Edit.php
@@ -6,6 +6,7 @@ namespace App\Filament\Resources\AchievementResource\Pages;
 
 use App\Filament\Concerns\HasFieldLevelAuthorization;
 use App\Filament\Resources\AchievementResource;
+use App\Models\Achievement;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -14,6 +15,20 @@ class Edit extends EditRecord
     use HasFieldLevelAuthorization;
 
     protected static string $resource = AchievementResource::class;
+
+    public function getBreadcrumbs(): array
+    {
+        /** @var Achievement $achievement */
+        $achievement = $this->record;
+        $game = $achievement->game;
+
+        return [
+            route('filament.admin.resources.games.index') => 'Games',
+            route('filament.admin.resources.games.view', $game) => $game->title,
+            route('filament.admin.resources.achievements.view', $achievement) => $achievement->title,
+            'Edit',
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
