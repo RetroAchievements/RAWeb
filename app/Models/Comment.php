a5c528095b09225a34493bff24e53128a47fe914
iOSLife <developer@ioslife.dev>
2024-08-07 10:16:24 -0400

    pint

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Support\Database\Eloquent\BaseModel;
+use Database\Factories\CommentFactory;
 use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Database\Factories\CommentFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
