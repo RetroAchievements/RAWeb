475616412ef19d217e307569f1b548169bf26d57
Wes Copeland <wlcopeland1@gmail.com>
2024-08-13 18:16:08 -0400

    perf: calculate stuff on the server

--- resources/views/components/head-analytics.blade.php
+++ resources/views/components/head-analytics.blade.php
@@ -3,46 +3,57 @@
     For example, /game/1 and /game/5 should not be considered as separate pages for
     the purpose of page view tracking.
     
-    The script below handles this aggregation. Patterns are matched in various URLs,
+    The code below handles this aggregation. Patterns are matched in various URLs,
     and path params are replaced with "_PARAM_".
 
     Additionally, just so we can drill down into the data if we'd like, the param
     is passed along to Plausible as a custom prop.
 --}}
 
-{{-- 
+@php
+    // Get the current URL path.
+    $url = request()->path();
 
-The JS snippet below is a minified version of the following code:
+    // Check if the URL should be redacted
+    if (preg_match('/\/\d+$/', $url) || preg_match('/\/\d+\//', $url) || preg_match('/^user\/[^\/]+/', $url)) {
+        // Redact dynamic segments in the URL.
+        $redactedUrl = preg_replace('/\d+/', '_PARAM_', $url);
 
-(function() {
-    var url = window.location.pathname;
-    var redactedUrl = url.replace(/\/(\d+|[^\/]+)(?=\/|$)/g, function(match, p1) {
-        if (/^\d+$/.test(p1) || !/^(system|games|user|progress|hashes)$/.test(p1)) {
-            return "/_PARAM_";
-        }
-        return match;
-    });
-    var props = {
-        isAuthenticated: false, // pull auth()->user() from Laravel here
-    };
-    var patterns = [
-        { regex: /^\/system\/(\d+)\//, prop: 'system' },
-        { regex: /^\/game\/(\d+)$/, prop: 'game' },
-        { regex: /^\/achievement\/(\d+)$/, prop: 'achievement' },
-        { regex: /^\/user\/([^\/]+)(\/progress)?$/, prop: 'user' },
-        { regex: /^\/game\/(\d+)\/hashes$/, prop: 'game' }
+        // Additionally redact the user routes.
+        $redactedUrl = preg_replace('/^user\/[^\/]+/', 'user/_PARAM_', $redactedUrl);
+    } else {
+        $redactedUrl = "/$url";
+    }
+
+    if ($redactedUrl === '//') {
+        $redactedUrl = '/';
+    }
+    if (!str_starts_with($redactedUrl, '/')) {
+        $redactedUrl = "/{$redactedUrl}";
+    }
+
+    $props = [
+        'isAuthenticated' => auth()->check(),
     ];
-    for (var i = 0; i < patterns.length; i++) {
-        var match = url.match(patterns[i].regex);
-        if (match) {
-            props[patterns[i].prop] = match[1];
+
+    // Define regex patterns to extract props from the URL.
+    $patterns = [
+        '/^system\/(\d+)\//' => 'system',
+        '/^game\/(\d+)$/' => 'game',
+        '/^achievement\/(\d+)$/' => 'achievement',
+        '/^user\/([^\/]+)(\/progress)?$/' => 'user',
+        '/^game\/(\d+)\/hashes$/' => 'game',
+        '/^ticket\/(\d+)$/' => 'ticket',
+    ];
+
+    // Loop through each pattern to extract props.
+    foreach ($patterns as $regex => $prop) {
+        if (preg_match($regex, $url, $matches)) {
+            $props[$prop] = $matches[1];
             break;
         }
     }
-    plausible('pageview', { u: redactedUrl + window.location.search, props });
-})();
-
---}}
+@endphp
 
 @if (!app()->environment('local'))
     @if (app()->environment('stage'))
@@ -50,10 +61,22 @@ The JS snippet below is a minified version of the following code:
     @elseif (app()->environment('production'))
         <script defer data-domain="retroachievements.org" src="https://plausible.retroachievements.org/psa2.js"></script>
     @endif
+
     <script>
         window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }
     </script>
+
     <script>
-        !function(){for(var e=window.location.pathname,r=e.replace(/\/(\d+|[^\/]+)(?=\/|$)/g,function(e,r){return/^\d+$/.test(r)||!/^(system|games|user|progress|hashes)$/.test(r)?"/_PARAM_":e}),s={isAuthenticated:{{ !!auth()->user() ? 'true' : 'false' }}},a=[{regex:/^\/system\/(\d+)\//,prop:"system"},{regex:/^\/game\/(\d+)$/,prop:"game"},{regex:/^\/achievement\/(\d+)$/,prop:"achievement"},{regex:/^\/user\/([^\/]+)(\/progress)?$/,prop:"user"},{regex:/^\/game\/(\d+)\/hashes$/,prop:"game"}],t=0;t<a.length;t++){var p=e.match(a[t].regex);if(p){s[a[t].prop]=p[1];break}}plausible("pageview",{u:r+window.location.search,props:s})}();
+        (function() {
+            var redactedUrl = "{{ $redactedUrl }}";
+            var props = @json($props);
+
+            let searchParams = '';
+            if (window.location.search.includes('t=play') || window.location.search.includes('t=develop')) {
+                searchParams = window.location.search;
+            }
+
+            plausible('pageview', { u: redactedUrl + searchParams, props });
+        })();
     </script>
 @endif
