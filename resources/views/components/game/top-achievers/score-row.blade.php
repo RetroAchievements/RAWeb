71b5af104685975dd041bd62f72c55656c80c9ea
Jamiras <jamirasmt@gmail.com>
2024-08-23 11:33:45 -0600

    show icon for users who have mastered a set in the most points earned list

--- resources/views/components/game/top-achievers/score-row.blade.php
+++ resources/views/components/game/top-achievers/score-row.blade.php
@@ -2,6 +2,7 @@
     'rank' => 1,
     'user' => null, // User
     'score' => 0,
+    'maxScore' => -1,
 ])
 
 @if (request()->user() && $user->id === request()->user()->id)
@@ -15,5 +16,12 @@
             <x-user.avatar display="icon" icon-size="xs" :user="$user" />
             <x-user.avatar hasHref="true" :user="$user" />
         </td>
-        <td class="text-right">{{ localized_number($score) }}</td>
+        <td>
+            <div class="flex items-center gap-1 float-right">
+                @if ($score === $maxScore)
+                    <div class="rounded-full bg-[gold] light:bg-yellow-600 w-2 h-2"></div>
+                @endif
+                <p>{{ localized_number($score) }}</p>
+            </div>
+        </td>
     </tr>
