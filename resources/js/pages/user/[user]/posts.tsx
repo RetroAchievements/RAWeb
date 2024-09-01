7c289694472cf9095e769e36309e1204832ef20c
Wes Copeland <wlcopeland1@gmail.com>
2024-09-01 19:36:57 -0400

    fix: add page meta

--- resources/js/pages/user/[user]/posts.tsx
+++ resources/js/pages/user/[user]/posts.tsx
@@ -1,14 +1,20 @@
 import { Head } from '@inertiajs/react';
 
+import { usePageProps } from '@/common/hooks/usePageProps';
 import { AppLayout } from '@/common/layouts/AppLayout';
 import type { AppPage } from '@/common/models';
 import { UserPostsMainRoot } from '@/features/users/components/UserPostsMainRoot';
 
 const UserPosts: AppPage = () => {
+  const { targetUser } = usePageProps<App.Community.Data.UserRecentPostsPageProps>();
+
   return (
     <>
-      <Head title="TODO">
-        <meta name="description" content="TODO" />
+      <Head title={`Forum Posts - ${targetUser.displayName}`}>
+        <meta
+          name="description"
+          content={`A list of ${targetUser.displayName}'s forum posts that have been made on the RetroAchievements forum.`}
+        />
       </Head>
 
       <AppLayout.Main>
