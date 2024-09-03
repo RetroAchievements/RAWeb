60ed120aa37dc5dd9e5ff351654b887c61477424
Wes Copeland <wlcopeland1@gmail.com>
2024-09-03 19:20:32 -0400

    feat(api): add API_GetGameHashes endpoint

---
+++ public/API/API_GetGameHashes.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ *  API_GetGameHashes - returns information about supported game files for a specific game
+ *    i : game id
+ *
+ *  array
+ *   object     [value]
+ *    string     Name        name given to the hash, usually a filename
+ *    string     MD5         unique hash
+ *    array      Labels
+ *     string     [value]    labels associated to the hash such as "nointro" or "redump"
+ *    string     PatchUrl    link to RAPatches URL for the hash
+ */
+
+use App\Models\Game;
+use App\Platform\Data\GameHashData;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+
+$input = Validator::validate(Arr::wrap(request()->query()), [
+    'i' => ['required', 'min:1'],
+]);
+
+$game = Game::find($input['i']);
+
+if (!$game) {
+    return response()->json([], 404);
+}
+
+$hashes = GameHashData::fromCollection($game->hashes);
+
+$mappedHashes = $game->hashes->map(function ($hash) {
+    return [
+        'Name' => $hash->name,
+        'MD5' => $hash->md5,
+        'Labels' => array_map(fn ($label) => $label, array_filter(explode(',', $hash->labels))),
+        'PatchUrl' => $hash->patch_url,
+    ];
+})->toArray();
+
+return [
+    'Results' => $mappedHashes,
+];
