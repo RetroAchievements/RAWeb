daa68932b0fd778e0d3400e7a371a0f63512bec7
Wes Copeland <wlcopeland1@gmail.com>
2024-09-03 17:16:26 -0400

    feat: migrate Supported Game Files to React (#2659)

--- resources/js/test/factories/index.ts
+++ resources/js/test/factories/index.ts
@@ -1,1 +1,6 @@
+export * from './createForumTopicComment';
+export * from './createGame';
+export * from './createGameHash';
+export * from './createGameHashLabel';
+export * from './createSystem';
 export * from './createUser';
