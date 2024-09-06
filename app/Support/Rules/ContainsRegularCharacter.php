cb8ae827d1837885527a3d9c3749ce9cfdb8ce65
Wes Copeland <wlcopeland1@gmail.com>
2024-09-06 19:13:34 -0400

    fix: require one regular character to submit posts, comments, and messages

---
+++ app/Support/Rules/ContainsRegularCharacter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Support\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ContainsRegularCharacter implements ValidationRule
+{
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        // Check for at least one letter, number, punctuation mark, or symbol.
+        $requireOneRegularCharacter = '/[\p{L}\p{N}\p{P}\p{S}]/u';
+
+        if (!preg_match($requireOneRegularCharacter, $value)) {
+            $fail('validation.contains_regular_characters')->translate();
+        }
+    }
+}
