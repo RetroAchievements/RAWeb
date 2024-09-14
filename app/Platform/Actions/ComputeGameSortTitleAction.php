def3cbc1560399bb7b3755aafad2adf752e86e67
Wes Copeland <wlcopeland1@gmail.com>
2024-09-13 22:45:44 -0400

    fix: address pr feedback

--- app/Platform/Actions/ComputeGameSortTitleAction.php
+++ app/Platform/Actions/ComputeGameSortTitleAction.php
@@ -6,37 +6,80 @@ namespace App\Platform\Actions;
 
 class ComputeGameSortTitleAction
 {
+    public function execute(string $gameTitle): string
+    {
+        $sortTitle = $this->replaceRomanNumerals($gameTitle);
+        $sortTitle = $this->removeArticles($sortTitle);
+        $sortTitle = mb_strtolower($sortTitle);
+        $sortTitle = $this->fixTagTildes($sortTitle);
+
+        return $sortTitle;
+    }
+
     /**
-     * Sort titles ensure games in lists are sorted properly.
+     * Replace Roman numerals with their corresponding padded numeric
+     * equivalents (eg: "IV" -> "04").
+     */
+    private function replaceRomanNumerals(string $title): string
+    {
+        $romanNumerals = [
+            'I' => 1, 'II' => 2, 'III' => 3, 'IV' => 4, 'V' => 5,
+            'VI' => 6, 'VII' => 7, 'VIII' => 8, 'IX' => 9, 'X' => 10,
+            'XI' => 11, 'XII' => 12, 'XIII' => 13, 'XIV' => 14, 'XV' => 15,
+            'XVI' => 16, 'XVII' => 17, 'XVIII' => 18, 'XIX' => 19, 'XX' => 20,
+        ];
+
+        $title = preg_replace_callback(
+            '/\b([IVX]+)\b/',
+            function ($matches) use ($romanNumerals) {
+                $roman = $matches[1];
+                if (isset($romanNumerals[$roman])) {
+                    return sprintf('%02d', $romanNumerals[$roman]);
+                }
+
+                return $roman;
+            },
+            $title
+        );
+
+        return $title;
+    }
+
+    /**
+     * "Legend of Zelda, The: A Link to the Past" -> "Legend of Zelda: A Link to the Past"
+     *
+     * ", The" has been removed.
+     */
+    private function removeArticles(string $title): string
+    {
+        if (preg_match('/^(.+),\s*(a|an|the)\b(.*)$/i', $title, $matches)) {
+            // Combine the main title and any trailing text.
+            $mainTitle = trim($matches[1] . $matches[3]);
+
+            return $mainTitle;
+        }
+
+        return $title;
+    }
+
+    /**
      * For titles starting with "~", the sort order is determined by the content
      * within the "~" markers followed by the content after the "~". This ensures
      * that titles with "~" are grouped together and sorted alphabetically based
      * on their designated categories and then by their actual game title.
-     *
-     * The "~" prefix is retained in the SortTitle of games with "~" to ensure these
-     * games are sorted at the end of the list, maintaining a clear separation from
-     * non-prefixed titles. This approach allows game titles to be grouped and sorted
-     * in a specific order:
-     *
-     * 1. Non-prefixed titles are sorted alphabetically at the beginning of the list.
-     * 2. Titles prefixed with "~" are grouped at the end, sorted first by the category
-     *    specified within the "~" markers, and then alphabetically by the title following
-     *    the "~".
      */
-    public function execute(string $gameTitle): string
+    private function fixTagTildes(string $title): string
     {
-        $sortTitle = mb_strtolower($gameTitle);
-
-        if ($sortTitle[0] === '~') {
-            $endOfFirstTilde = strpos($sortTitle, '~', 1);
+        if ($title[0] === '~') {
+            $endOfFirstTilde = strpos($title, '~', 1);
             if ($endOfFirstTilde !== false) {
-                $withinTildes = substr($sortTitle, 1, $endOfFirstTilde - 1);
-                $afterTildes = trim(substr($sortTitle, $endOfFirstTilde + 1));
+                $withinTildes = substr($title, 1, $endOfFirstTilde - 1);
+                $afterTildes = trim(substr($title, $endOfFirstTilde + 1));
 
-                $sortTitle = '~' . $withinTildes . ' ' . $afterTildes;
+                $title = '~' . $withinTildes . ' ' . $afterTildes;
             }
         }
 
-        return $sortTitle;
+        return $title;
     }
 }
--- tests/Feature/Platform/Action/WriteGameSortTitleFromGameTitleActionTest.php
+++ tests/Feature/Platform/Action/WriteGameSortTitleFromGameTitleActionTest.php
@@ -42,7 +42,7 @@ class WriteGameSortTitleFromGameTitleActionTest extends TestCase
     public function testItCanBeConfiguredToOverrideCustomSortTitles(): void
     {
         // Arrange
-        $game = Game::factory()->create(['Title' => 'Final Fantasy IV', 'sort_title' => 'final fantasy 04']);
+        $game = Game::factory()->create(['Title' => 'Final Fantasy IV', 'sort_title' => 'final fantasy 0004']);
 
         // Act
         (new WriteGameSortTitleFromGameTitleAction())->execute(
@@ -53,19 +53,95 @@ class WriteGameSortTitleFromGameTitleActionTest extends TestCase
         $game = $game->fresh();
 
         // Assert
-        $this->assertEquals('final fantasy iv', $game->sort_title);
+        $this->assertEquals('final fantasy 04', $game->sort_title);
     }
 
     public function testItCorrectlyHandlesGameTitlesWithTildes(): void
     {
         // Arrange
-        $game = Game::factory()->create(['Title' => '~Homebrew~ Classic Kong', 'sort_title' => null]);
+        $gameOne = Game::factory()->create(['Title' => '~Homebrew~ Classic Kong', 'sort_title' => null]);
+        $gameTwo = Game::factory()->create(['Title' => 'Puyo Puyo~n', 'sort_title' => null]);
+
+        // Act
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameOne, $gameOne->title);
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameTwo, $gameTwo->title);
+        $gameOne = $gameOne->fresh();
+        $gameTwo = $gameTwo->fresh();
+
+        // Assert
+        $this->assertEquals('~homebrew classic kong', $gameOne->sort_title);
+        $this->assertEquals('puyo puyo~n', $gameTwo->sort_title);
+    }
+
+    public function testItCorrectlyConvertsRomanNumerals(): void
+    {
+        // Arrange
+        $gameOne = Game::factory()->create(['Title' => 'Final Fantasy IV', 'sort_title' => null]);
+        $gameTwo = Game::factory()->create(['Title' => 'Final Fantasy X', 'sort_title' => null]);
+
+        // Act
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameOne, $gameOne->title);
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameTwo, $gameTwo->title);
+        $gameOne = $gameOne->fresh();
+        $gameTwo = $gameTwo->fresh();
+
+        // Assert
+        $this->assertEquals('final fantasy 04', $gameOne->sort_title);
+        $this->assertEquals('final fantasy 10', $gameTwo->sort_title);
+    }
+
+    public function testItAvoidsNonRomanStrings(): void
+    {
+        // Arrange
+        $game = Game::factory()->create(['Title' => 'GIVX', 'sort_title' => null]);
 
         // Act
         (new WriteGameSortTitleFromGameTitleAction())->execute($game, $game->title);
         $game = $game->fresh();
 
         // Assert
-        $this->assertEquals('~homebrew classic kong', $game->sort_title);
+        $this->assertEquals('givx', $game->sort_title);
+    }
+
+    public function testItNormalizesArticleFragments(): void
+    {
+        // Arrange
+        $gameOne = Game::factory()->create(['Title' => 'Legend of Zelda, The: A Link to the Past', 'sort_title' => null]);
+        $gameTwo = Game::factory()->create(['Title' => 'American Tale, An', 'sort_title' => null]);
+        $gameThree = Game::factory()->create(['Title' => 'Grand Day Out, A', 'sort_title' => null]);
+
+        // Act
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameOne, $gameOne->title);
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameTwo, $gameTwo->title);
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameThree, $gameThree->title);
+        $gameOne = $gameOne->fresh();
+        $gameTwo = $gameTwo->fresh();
+        $gameThree = $gameThree->fresh();
+
+        // Assert
+        $this->assertEquals('legend of zelda: a link to the past', $gameOne->sort_title);
+        $this->assertEquals('american tale', $gameTwo->sort_title);
+        $this->assertEquals('grand day out', $gameThree->sort_title);
+    }
+
+    public function testItAvoidsNonArticleFragments(): void
+    {
+        // Arrange
+        $gameOne = Game::factory()->create(['Title' => 'The Great Escape', 'sort_title' => null]);
+        $gameTwo = Game::factory()->create(['Title' => 'An Unexpected Journey', 'sort_title' => null]);
+        $gameThree = Game::factory()->create(['Title' => 'A Series of Unfortunate Events', 'sort_title' => null]);
+
+        // Act
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameOne, $gameOne->title);
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameTwo, $gameTwo->title);
+        (new WriteGameSortTitleFromGameTitleAction())->execute($gameThree, $gameThree->title);
+        $gameOne = $gameOne->fresh();
+        $gameTwo = $gameTwo->fresh();
+        $gameThree = $gameThree->fresh();
+
+        // Assert
+        $this->assertEquals('the great escape', $gameOne->sort_title);
+        $this->assertEquals('an unexpected journey', $gameTwo->sort_title);
+        $this->assertEquals('a series of unfortunate events', $gameThree->sort_title);
     }
 }
