399ea39a21578cb5b294b896f76a69ba2b209c88
Wes Copeland <wlcopeland1@gmail.com>
2024-09-16 16:27:25 -0400

    fix: backfill task credit on filament insert

--- app/Filament/Resources/AchievementResource/RelationManagers/AuthorshipCreditsRelationManager.php
+++ app/Filament/Resources/AchievementResource/RelationManagers/AuthorshipCreditsRelationManager.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\Filament\Resources\AchievementResource\RelationManagers;
 
 use App\Filament\Resources\AchievementAuthorshipCreditFormSchema;
+use App\Models\Achievement;
 use App\Models\AchievementAuthor;
 use App\Platform\Enums\AchievementAuthorTask;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 
 class AuthorshipCreditsRelationManager extends RelationManager
 {
@@ -45,7 +47,51 @@ class AuthorshipCreditsRelationManager extends RelationManager
             ->headerActions([
                 Tables\Actions\CreateAction::make()
                     ->label('Add achievement credit')
-                    ->modalHeading('Add achievement credit'),
+                    ->modalHeading('Add achievement credit')
+                    ->using(function (array $data, string $model): Model {
+                        /** @var Achievement $achievement */
+                        $achievement = $this->ownerRecord;
+
+                        /**
+                         * An achievement developer doing most tasks is implied. If someone
+                         * new contributes, we'll backfill a credit record for the achievement
+                         * developer.
+                         *
+                         * For example, if someone creates a new badge for the achievement,
+                         * we'll credit the achievement developer for art, then credit the
+                         * new person for art.
+                         */
+                        $developer = $achievement->developer;
+                        if ($developer) {
+                            $doesDeveloperCreditExist = AchievementAuthor::where('achievement_id', $achievement->id)
+                                ->where('user_id', $developer->id)
+                                ->where('task', $data['task'])
+                                ->exists();
+
+                            if (!$doesDeveloperCreditExist) {
+                                $firstBadgeComment = $achievement->legacyComments()
+                                    ->automated()
+                                    ->where('Payload', 'LIKE', "{$developer->display_name}%")
+                                    ->where('Payload', 'LIKE', "%badge%")
+                                    ->first();
+
+                                $backdate = $firstBadgeComment?->Submitted ?? $achievement->DateCreated;
+
+                                AchievementAuthor::create([
+                                    'user_id' => $developer->id,
+                                    'achievement_id' => $achievement->id,
+                                    'task' => $data['task'],
+                                    'created_at' => $backdate,
+                                ]);
+                            }
+                        }
+
+                        return AchievementAuthor::create([
+                            'user_id' => (int) $data['user_id'],
+                            'achievement_id' => $achievement->id,
+                            'task' => $data['task'],
+                        ]);
+                    }),
             ])
             ->actions([
                 Tables\Actions\EditAction::make()
