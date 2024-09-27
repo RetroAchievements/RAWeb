d6a7e75fface5053f46e391b247d4f96b4f762d2
Wes Copeland <wlcopeland1@gmail.com>
2024-09-26 20:38:56 -0400

    feat: add support for Design credit

--- app/Platform/Enums/AchievementAuthorTask.php
+++ app/Platform/Enums/AchievementAuthorTask.php
@@ -7,6 +7,7 @@ namespace App\Platform\Enums;
 enum AchievementAuthorTask: string
 {
     case Artwork = "artwork";
+    case Design = "design";
     case Logic = "logic";
     case Testing = "testing";
     case Writing = "writing";
@@ -15,6 +16,7 @@ enum AchievementAuthorTask: string
     {
         return match ($this) {
             self::Artwork => 'Artwork',
+            self::Design => 'Design',
             self::Logic => 'Logic',
             self::Testing => 'Testing',
             self::Writing => 'Writing',
