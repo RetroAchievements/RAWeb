85dd52ff2ed91bb7df6667a7afea0671ac0b275f
Wes Copeland <wlcopeland1@gmail.com>
2024-10-23 08:23:59 -0400

    fix(game): sort Supported Game Files items (#2763)

--- resources/js/features/games/components/HashesMainRoot/HashesList/HashesList.tsx
+++ resources/js/features/games/components/HashesMainRoot/HashesList/HashesList.tsx
@@ -17,21 +17,24 @@ export const HashesList: FC = () => {
   const namedHashes = hashes.filter((hash) => !!hash.name?.trim());
   const unnamedHashes = hashes.filter((hash) => !hash.name?.trim());
 
+  const sortedNamedHashes = [...namedHashes].sort((a, b) => a.name!.localeCompare(b.name!));
+  const sortedUnnamedHashes = [...unnamedHashes].sort((a, b) => a.md5.localeCompare(b.md5));
+
   return (
     <Embed data-testid={hashesListContainerTestId} className="flex flex-col gap-4">
-      {namedHashes.length ? (
+      {sortedNamedHashes.length ? (
         <ul className="flex flex-col gap-3" data-testid="named-hashes">
-          {namedHashes.map((labeledHash) => (
+          {sortedNamedHashes.map((labeledHash) => (
             <HashesListItem key={labeledHash.md5} hash={labeledHash} />
           ))}
         </ul>
       ) : null}
 
-      {namedHashes.length && unnamedHashes.length ? <div className="my-6" /> : null}
+      {sortedNamedHashes.length && sortedUnnamedHashes.length ? <div className="my-6" /> : null}
 
-      {unnamedHashes.length ? (
+      {sortedUnnamedHashes.length ? (
         <ul className="flex flex-col" data-testid="unnamed-hashes">
-          {unnamedHashes.map((unlabeledHash) => (
+          {sortedUnnamedHashes.map((unlabeledHash) => (
             <HashesListItem key={unlabeledHash.md5} hash={unlabeledHash} />
           ))}
         </ul>
