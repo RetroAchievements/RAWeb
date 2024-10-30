81eb7e1e6789f3c784b89ec1adcb0fda08cf0f91
Wes Copeland <wlcopeland1@gmail.com>
2024-10-29 20:41:47 -0400

    feat(DataTablePagination): add a manual paginator control

--- resources/js/features/game-list/components/DataTablePagination/DataTablePagination.tsx
+++ resources/js/features/game-list/components/DataTablePagination/DataTablePagination.tsx
@@ -6,9 +6,9 @@ import type { RouteName } from 'ziggy-js';
 
 import { BaseButton } from '@/common/components/+vendor/BaseButton';
 import { BasePagination, BasePaginationContent } from '@/common/components/+vendor/BasePagination';
-import { useFormatNumber } from '@/common/hooks/useFormatNumber';
 
 import { useDataTablePrefetchPagination } from '../../hooks/useDataTablePrefetchPagination';
+import { ManualPaginatorField } from './ManualPaginatorField';
 
 interface DataTablePaginationProps<TData> {
   table: Table<TData>;
@@ -24,28 +24,57 @@ export default function DataTablePagination<TData>({
 
   const { pagination } = table.getState();
 
-  const { formatNumber } = useFormatNumber();
-
   // Given the user hovers over a pagination button, it is very likely they will
   // wind up clicking the button. Queries are cheap, so prefetch the destination page.
   const { prefetchPagination } = useDataTablePrefetchPagination(table, tableApiRouteName);
 
-  const handlePageChange = (newPageIndex: number, isNext: boolean) => {
-    table.setPageIndex(newPageIndex);
-    const nextPageIndex = isNext ? newPageIndex + 1 : newPageIndex - 1;
+  const scrollToTopOfPage = () => {
+    const scrollTarget = document.getElementById('pagination-scroll-target');
 
-    // Prefetch the next or previous page after setting the new page.
-    if (nextPageIndex >= 0 && nextPageIndex < table.getPageCount()) {
-      prefetchPagination(nextPageIndex);
+    if (!scrollTarget) {
+      return;
     }
 
-    // Scroll the user to the top of the page.
-    setTimeout(() => {
-      window.scrollTo({
-        top: document.getElementById('pagination-scroll-target')?.offsetTop,
-        behavior: 'smooth',
-      });
+    window.scrollTo({
+      top: scrollTarget.offsetTop,
+      behavior: 'instant',
     });
+  };
+
+  /**
+   * Handles page change and optional prefetching of adjacent pages for a smoother user experience.
+   *
+   * @param {Array<'next' | 'previous'>} [prefetchDirections] - Optional. Specifies whether to prefetch
+   * adjacent pages. 'next' prefetches the following page, and 'previous' prefetches the previous page.
+   * This is helpful for loading pages before the user actually clicks the pagination button.
+   *
+   * @example
+   * // Navigate to page 3 and prefetch the next page:
+   * handlePageChange(3, ['next']);
+   *
+   * // Navigate to page 2 and prefetch both next and previous pages:
+   * handlePageChange(2, ['next', 'previous']);
+   */
+  const handlePageChange = (
+    newPageIndex: number,
+    prefetchDirections?: Array<'next' | 'previous'>,
+  ) => {
+    // Update the current page.
+    table.setPageIndex(newPageIndex);
+
+    // Handle prefetching of adjacent pages.
+    const lastPageIndex = table.getPageCount() - 1;
+    const canPrefetchNext = prefetchDirections?.includes('next') && newPageIndex < lastPageIndex;
+    const canPrefetchPrevious = prefetchDirections?.includes('previous') && newPageIndex > 0;
+
+    if (canPrefetchNext) {
+      prefetchPagination(Math.min(newPageIndex + 1, lastPageIndex));
+    }
+    if (canPrefetchPrevious) {
+      prefetchPagination(Math.max(newPageIndex - 1, 0));
+    }
+
+    scrollToTopOfPage();
   };
 
   return (
@@ -53,53 +82,48 @@ export default function DataTablePagination<TData>({
       <div />
 
       <div className="flex items-center gap-6 lg:gap-8">
-        <p className="text-neutral-200 light:text-neutral-900">
-          {t('Page :count of :total', {
-            count: formatNumber(pagination.pageIndex + 1),
-            total: formatNumber(table.getPageCount()),
-          })}
-        </p>
+        <BasePagination className="flex items-center gap-6 lg:gap-8">
+          <ManualPaginatorField table={table} onPageChange={handlePageChange} />
 
-        <BasePagination>
-          <BasePaginationContent className="gap-2">
+          <BasePaginationContent className="gap-2" role="group">
             <BaseButton
-              className="h-8 w-8 p-0"
-              onClick={() => handlePageChange(0, false)}
+              className="size-8 p-0"
+              onClick={() => handlePageChange(0, ['next'])}
               onMouseEnter={() => prefetchPagination(0)}
               disabled={!table.getCanPreviousPage()}
+              aria-label={t('Go to first page')}
             >
-              <span className="sr-only">{t('Go to first page')}</span>
-              <LuArrowLeftToLine className="h-4 w-4" />
+              <LuArrowLeftToLine className="size-4" aria-hidden={true} />
             </BaseButton>
 
             <BaseButton
-              className="h-8 w-8 p-0"
-              onClick={() => handlePageChange(pagination.pageIndex - 1, false)}
+              className="size-8 p-0"
+              onClick={() => handlePageChange(pagination.pageIndex - 1, ['previous'])}
               onMouseEnter={() => prefetchPagination(pagination.pageIndex - 1)}
               disabled={!table.getCanPreviousPage()}
+              aria-label={t('Go to previous page')}
             >
-              <span className="sr-only">{t('Go to previous page')}</span>
-              <LuArrowLeft className="h-4 w-4" />
+              <LuArrowLeft className="size-4" aria-hidden={true} />
             </BaseButton>
 
             <BaseButton
-              className="h-8 w-8 p-0"
-              onClick={() => handlePageChange(pagination.pageIndex + 1, true)}
+              className="size-8 p-0"
+              onClick={() => handlePageChange(pagination.pageIndex + 1, ['next'])}
               onMouseEnter={() => prefetchPagination(pagination.pageIndex + 1)}
               disabled={!table.getCanNextPage()}
+              aria-label={t('Go to next page')}
             >
-              <span className="sr-only">{t('Go to next page')}</span>
-              <LuArrowRight className="h-4 w-4" />
+              <LuArrowRight className="size-4" aria-hidden={true} />
             </BaseButton>
 
             <BaseButton
-              className="h-8 w-8 p-0"
-              onClick={() => handlePageChange(table.getPageCount() - 1, true)}
+              className="size-8 p-0"
+              onClick={() => handlePageChange(table.getPageCount() - 1, ['previous'])}
               onMouseEnter={() => prefetchPagination(table.getPageCount() - 1)}
               disabled={!table.getCanNextPage()}
+              aria-label={t('Go to last page')}
             >
-              <span className="sr-only">{t('Go to last page')}</span>
-              <LuArrowRightToLine className="h-4 w-4" />
+              <LuArrowRightToLine className="size-4" aria-hidden={true} />
             </BaseButton>
           </BasePaginationContent>
         </BasePagination>
