efeaa8fca58fa9b9140c6209c452902ea6b91892
Wes Copeland <wlcopeland1@gmail.com>
2024-11-19 17:40:42 -0500

    chore: add menu option

--- resources/js/features/settings/components/LocaleSectionCard/LocaleSectionCard.tsx
+++ resources/js/features/settings/components/LocaleSectionCard/LocaleSectionCard.tsx
@@ -57,6 +57,7 @@ export const LocaleSectionCard: FC = () => {
                       {/* These labels should not be localized. */}
                       <BaseSelectItem value="en_US">{'English (US)'}</BaseSelectItem>
                       <BaseSelectItem value="en_GB">{'English (UK)'}</BaseSelectItem>
+                      <BaseSelectItem value="de_DE">{'Deutsch (Deutschland)'}</BaseSelectItem>
                       <BaseSelectItem value="es_ES">{'Español (España)'}</BaseSelectItem>
                       <BaseSelectItem value="fr_FR">{'Français (France)'}</BaseSelectItem>
                       <BaseSelectItem value="pl_PL">{'Polski (Polska)'}</BaseSelectItem>
