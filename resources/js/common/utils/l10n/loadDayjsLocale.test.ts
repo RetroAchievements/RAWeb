b429d6b0eb1fbfc8d1138dbd5c924a06bde5370b
Wes Copeland <wlcopeland1@gmail.com>
2024-11-19 17:33:57 -0500

    fix(create-ticket): remediate some localization issues

--- resources/js/common/utils/l10n/loadDayjsLocale.test.ts
+++ resources/js/common/utils/l10n/loadDayjsLocale.test.ts
@@ -23,6 +23,7 @@ describe('Util: loadDayjsLocale', () => {
 
   it.each`
     userLocale | dayjsLocale
+    ${'de_DE'} | ${'de'}
     ${'en_GB'} | ${'en-gb'}
     ${'pt_BR'} | ${'pt-br'}
     ${'pl_PL'} | ${'pl'}
