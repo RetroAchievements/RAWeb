dc8902db1c74959878960fd7269e0617cd9ba934
Jamiras <jamirasmt@gmail.com>
2024-11-23 16:01:08 -0700

    restore un-regenerated generatedAppConstants

--- resources/js/common/utils/generatedAppConstants.ts
+++ resources/js/common/utils/generatedAppConstants.ts
@@ -1,151 +1,5 @@
 /* eslint-disable */
 /* generated with `composer types` */
-export const AwardType = {
-    Mastery: 1,
-    AchievementUnlocksYield: 2,
-    AchievementPointsYield: 3,
-    PatreonSupporter: 6,
-    CertifiedLegend: 7,
-    GameBeaten: 8,
-} as const;
-
-
-export const StringifiedAwardType = {
-    Mastery: '1',
-    AchievementUnlocksYield: '2',
-    AchievementPointsYield: '3',
-    PatreonSupporter: '6',
-    CertifiedLegend: '7',
-    GameBeaten: '8',
-} as const;
-
-
-export const ClaimType = {
-    Primary: 0,
-    Collaboration: 1,
-} as const;
-
-
-export const StringifiedClaimType = {
-    Primary: '0',
-    Collaboration: '1',
-} as const;
-
-
-export const UserGameListType = {
-    AchievementSetRequest: 'achievement_set_request',
-    Play: 'play',
-    Develop: 'develop',
-} as const;
-
-
-export const ArticleType = {
-    Game: 1,
-    Achievement: 2,
-    User: 3,
-    News: 4,
-    Leaderboard: 6,
-    AchievementTicket: 7,
-    Forum: 8,
-    UserModeration: 9,
-    GameHash: 10,
-    SetClaim: 11,
-    GameModification: 12,
-} as const;
-
-
-export const StringifiedArticleType = {
-    Game: '1',
-    Achievement: '2',
-    User: '3',
-    News: '4',
-    Leaderboard: '6',
-    AchievementTicket: '7',
-    Forum: '8',
-    UserModeration: '9',
-    GameHash: '10',
-    SetClaim: '11',
-    GameModification: '12',
-} as const;
-
-
-export const ClaimStatus = {
-    Active: 0,
-    Complete: 1,
-    Dropped: 2,
-    InReview: 3,
-} as const;
-
-
-export const StringifiedClaimStatus = {
-    Active: '0',
-    Complete: '1',
-    Dropped: '2',
-    InReview: '3',
-} as const;
-
-
-export const TicketType = {
-    TriggeredAtWrongTime: 1,
-    DidNotTrigger: 2,
-} as const;
-
-
-export const StringifiedTicketType = {
-    TriggeredAtWrongTime: '1',
-    DidNotTrigger: '2',
-} as const;
-
-
-export const ClaimSetType = {
-    NewSet: 0,
-    Revision: 1,
-} as const;
-
-
-export const StringifiedClaimSetType = {
-    NewSet: '0',
-    Revision: '1',
-} as const;
-
-
-export const SubscriptionSubjectType = {
-    ForumTopic: 'ForumTopic',
-    UserWall: 'UserWall',
-    GameWall: 'GameWall',
-    Achievement: 'Achievement',
-    GameTickets: 'GameTickets',
-    GameAchievements: 'GameAchievements',
-} as const;
-
-
-export const UserRole = {
-    ROOT: 'root',
-    ADMINISTRATOR: 'administrator',
-    RELEASE_MANAGER: 'release-manager',
-    GAME_HASH_MANAGER: 'game-hash-manager',
-    DEVELOPER_STAFF: 'developer-staff',
-    DEVELOPER: 'developer',
-    DEVELOPER_JUNIOR: 'developer-junior',
-    ARTIST: 'artist',
-    WRITER: 'writer',
-    GAME_EDITOR: 'game-editor',
-    PLAY_TESTER: 'play-tester',
-    MODERATOR: 'moderator',
-    FORUM_MANAGER: 'forum-manager',
-    TICKET_MANAGER: 'ticket-manager',
-    NEWS_MANAGER: 'news-manager',
-    EVENT_MANAGER: 'event-manager',
-    CHEAT_INVESTIGATOR: 'cheat-investigator',
-    FOUNDER: 'founder',
-    ARCHITECT: 'architect',
-    ENGINEER: 'engineer',
-    TEAM_ACCOUNT: 'team-account',
-    BETA: 'beta',
-    DEVELOPER_VETERAN: 'developer-veteran',
-} as const;
-
-
 export const UnlockMode = {
     Softcore: 0,
     Hardcore: 1,
@@ -201,5 +55,151 @@ export const StringifiedUserPreference = {
     Game_HideMissableIndicators: '16',
     User_OnlyContactFromFollowing: '17',
     Game_OptOutOfAllSets: '18',
+} as const;
+
+
+export const UserRole = {
+    ROOT: 'root',
+    ADMINISTRATOR: 'administrator',
+    RELEASE_MANAGER: 'release-manager',
+    GAME_HASH_MANAGER: 'game-hash-manager',
+    DEVELOPER_STAFF: 'developer-staff',
+    DEVELOPER: 'developer',
+    DEVELOPER_JUNIOR: 'developer-junior',
+    ARTIST: 'artist',
+    WRITER: 'writer',
+    GAME_EDITOR: 'game-editor',
+    PLAY_TESTER: 'play-tester',
+    MODERATOR: 'moderator',
+    FORUM_MANAGER: 'forum-manager',
+    TICKET_MANAGER: 'ticket-manager',
+    NEWS_MANAGER: 'news-manager',
+    EVENT_MANAGER: 'event-manager',
+    CHEAT_INVESTIGATOR: 'cheat-investigator',
+    FOUNDER: 'founder',
+    ARCHITECT: 'architect',
+    ENGINEER: 'engineer',
+    TEAM_ACCOUNT: 'team-account',
+    BETA: 'beta',
+    DEVELOPER_VETERAN: 'developer-veteran',
+} as const;
+
+
+export const ClaimStatus = {
+    Active: 0,
+    Complete: 1,
+    Dropped: 2,
+    InReview: 3,
+} as const;
+
+
+export const StringifiedClaimStatus = {
+    Active: '0',
+    Complete: '1',
+    Dropped: '2',
+    InReview: '3',
+} as const;
+
+
+export const ClaimType = {
+    Primary: 0,
+    Collaboration: 1,
+} as const;
+
+
+export const StringifiedClaimType = {
+    Primary: '0',
+    Collaboration: '1',
+} as const;
+
+
+export const AwardType = {
+    Mastery: 1,
+    AchievementUnlocksYield: 2,
+    AchievementPointsYield: 3,
+    PatreonSupporter: 6,
+    CertifiedLegend: 7,
+    GameBeaten: 8,
+} as const;
+
+
+export const StringifiedAwardType = {
+    Mastery: '1',
+    AchievementUnlocksYield: '2',
+    AchievementPointsYield: '3',
+    PatreonSupporter: '6',
+    CertifiedLegend: '7',
+    GameBeaten: '8',
+} as const;
+
+
+export const ArticleType = {
+    Game: 1,
+    Achievement: 2,
+    User: 3,
+    News: 4,
+    Leaderboard: 6,
+    AchievementTicket: 7,
+    Forum: 8,
+    UserModeration: 9,
+    GameHash: 10,
+    SetClaim: 11,
+    GameModification: 12,
+} as const;
+
+
+export const StringifiedArticleType = {
+    Game: '1',
+    Achievement: '2',
+    User: '3',
+    News: '4',
+    Leaderboard: '6',
+    AchievementTicket: '7',
+    Forum: '8',
+    UserModeration: '9',
+    GameHash: '10',
+    SetClaim: '11',
+    GameModification: '12',
+} as const;
+
+
+export const TicketType = {
+    TriggeredAtWrongTime: 1,
+    DidNotTrigger: 2,
+} as const;
+
+
+export const StringifiedTicketType = {
+    TriggeredAtWrongTime: '1',
+    DidNotTrigger: '2',
+} as const;
+
+
+export const SubscriptionSubjectType = {
+    ForumTopic: 'ForumTopic',
+    UserWall: 'UserWall',
+    GameWall: 'GameWall',
+    Achievement: 'Achievement',
+    GameTickets: 'GameTickets',
+    GameAchievements: 'GameAchievements',
+} as const;
+
+
+export const UserGameListType = {
+    AchievementSetRequest: 'achievement_set_request',
+    Play: 'play',
+    Develop: 'develop',
+} as const;
+
+
+export const ClaimSetType = {
+    NewSet: 0,
+    Revision: 1,
+} as const;
+
+
+export const StringifiedClaimSetType = {
+    NewSet: '0',
+    Revision: '1',
 } as const;
 
