53c710e2f2893874b02e3b5a3baf230b414de8f6
Wes Copeland <wlcopeland1@gmail.com>
2024-12-06 17:14:35 -0500

    fix: dont open a modal dialog on inertia ticket redirect

--- app/Platform/Controllers/TriggerTicketController.php
+++ app/Platform/Controllers/TriggerTicketController.php
@@ -12,8 +12,8 @@ use App\Models\TriggerTicket;
 use App\Platform\Actions\BuildTicketCreationDataAction;
 use App\Support\Concerns\HandlesResources;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Inertia\Inertia;
 use Inertia\Response as InertiaResponse;
 
@@ -41,7 +41,7 @@ class TriggerTicketController extends Controller
         Request $request,
         Achievement $achievement,
         BuildTicketCreationDataAction $buildTicketCreationData,
-    ): InertiaResponse|RedirectResponse {
+    ): InertiaResponse|Response {
         $this->authorize('create', [TriggerTicket::class, $achievement]);
 
         // A user can only have one ticket open at a time for a triggerable.
@@ -51,7 +51,7 @@ class TriggerTicketController extends Controller
             ->whereNotIn('ReportState', [TicketState::Closed, TicketState::Resolved])
             ->first();
         if ($existingTicket) {
-            return redirect()->route('ticket.show', ['ticket' => $existingTicket->id]);
+            return Inertia::location(route('ticket.show', ['ticket' => $existingTicket->id]));
         }
 
         $props = $buildTicketCreationData->execute($achievement, $request->user());
@@ -59,7 +59,7 @@ class TriggerTicketController extends Controller
         // If for some reason there are no hashes or emulators associated with a
         // game, then it isn't possible to create tickets for its triggerables.
         if (!count($props->gameHashes) || !count($props->emulators)) {
-            return redirect()->route('achievement.show', $achievement->id);
+            return Inertia::location(route('achievement.show', $achievement->id));
         }
 
         return Inertia::render('achievement/[achievement]/tickets/create', $props);
