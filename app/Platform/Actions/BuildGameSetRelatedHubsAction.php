f85c0a1d8f0b61232108db68c04fd54292719278
Wes Copeland <wlcopeland1@gmail.com>
2024-12-08 16:02:08 -0500

    refactor: make code more eloquent-like

--- app/Platform/Actions/BuildGameSetRelatedHubsAction.php
+++ app/Platform/Actions/BuildGameSetRelatedHubsAction.php
@@ -8,7 +8,6 @@ use App\Models\GameSet;
 use App\Models\System;
 use App\Platform\Data\GameSetData;
 use App\Platform\Enums\GameSetType;
-use Illuminate\Support\Facades\DB;
 
 class BuildGameSetRelatedHubsAction
 {
@@ -17,23 +16,16 @@ class BuildGameSetRelatedHubsAction
      */
     public function execute(GameSet $gameSet): array
     {
-        // Get the parent hub IDs where this game set is used.
-        $parentHubIds = DB::table('game_set_links as gsl')
-            ->join('game_sets as gs', 'gs.id', '=', 'gsl.parent_game_set_id')
-            ->where('gsl.child_game_set_id', $gameSet->id)
-            ->where('gs.type', GameSetType::Hub)
-            ->where('gs.deleted_at', null)
-            ->select('gs.id')
-            ->pluck('id');
-
-        // Fetch all parent hubs with counts in a single query.
-        $relatedHubs = GameSet::whereIn('id', $parentHubIds)
+        return GameSet::whereHas('children', function ($query) use ($gameSet) {
+            $query->where('child_game_set_id', $gameSet->id);
+        })
+            ->whereType(GameSetType::Hub)
             ->select([
-                'game_sets.id',
-                'game_sets.title',
-                'game_sets.image_asset_path',
-                'game_sets.type',
-                'game_sets.updated_at',
+                'id',
+                'title',
+                'image_asset_path',
+                'type',
+                'updated_at',
             ])
             ->withCount([
                 'games' => function ($query) {
@@ -44,12 +36,10 @@ class BuildGameSetRelatedHubsAction
                     $query->whereNull('game_sets.deleted_at');
                 },
             ])
-            ->orderBy('game_sets.title')
+            ->orderBy('title')
             ->get()
             ->map(fn (GameSet $hub) => GameSetData::fromGameSetWithCounts($hub))
             ->values()
             ->all();
-
-        return $relatedHubs;
     }
 }
