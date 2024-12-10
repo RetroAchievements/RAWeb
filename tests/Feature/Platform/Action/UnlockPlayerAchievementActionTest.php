08270d4c9c11196e9a73d7e16ced320a5f1f5211
Jamiras <jamirasmt@gmail.com>
2024-12-10 08:01:27 -0700

    rename file

--- tests/Feature/Platform/Action/UnlockPlayerAchievementActionTest.php
+++ tests/Feature/Platform/Action/UnlockPlayerAchievementActionTest.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
-class UnlockPlayerAchievementActionTests extends TestCase
+class UnlockPlayerAchievementActionTest extends TestCase
 {
     use RefreshDatabase;
 
