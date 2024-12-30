d1d22a28f75a4e7d15b9dcc1b9e6888fcc08a88c
Wes Copeland <wlcopeland1@gmail.com>
2024-12-30 11:20:56 -0500

    fix(TrendingRightNow): resolve css issue when game title is long (#2971)

--- resources/js/features/home/components/+root/TrendingRightNow/TrendingRightNow.tsx
+++ resources/js/features/home/components/+root/TrendingRightNow/TrendingRightNow.tsx
@@ -20,9 +20,15 @@ export const TrendingRightNow: FC = () => {
       <div className="grid gap-1 sm:grid-cols-2">
         {trendingGames.map((trendingGame) => (
           <div key={`trending-${trendingGame.game.id}`} className="rounded-lg bg-embed p-2">
-            <div className="flex w-full items-end justify-between">
-              <GameAvatar {...trendingGame.game} size={40} showSystemChip={true} />
-              <p className="text-2xs">
+            <div className="relative flex w-full items-end justify-between">
+              <GameAvatar
+                {...trendingGame.game}
+                size={40}
+                showSystemChip={true}
+                gameTitleClassName="line-clamp-1"
+              />
+
+              <p className="absolute bottom-0 right-0 text-2xs">
                 {t('playerCount', {
                   count: trendingGame.playerCount,
                   val: trendingGame.playerCount,
