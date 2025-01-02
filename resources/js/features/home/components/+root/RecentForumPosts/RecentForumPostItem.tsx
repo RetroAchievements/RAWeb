f322df9789b17442d5647946ca1d2cc35e2bf44f
Wes Copeland <wlcopeland1@gmail.com>
2025-01-01 19:47:41 -0500

    fix: more precision

--- resources/js/features/home/components/+root/RecentForumPosts/RecentForumPostItem.tsx
+++ resources/js/features/home/components/+root/RecentForumPosts/RecentForumPostItem.tsx
@@ -22,7 +22,7 @@ export const RecentForumPostItem: FC<RecentForumPostItemProps> = ({ post }) => {
   }
 
   return (
-    <div className="max-w-[840px] rounded bg-embed px-2.5 py-1.5">
+    <div className="rounded bg-embed px-2.5 py-1.5">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-1.5">
           <UserAvatar {...post.latestComment.user} size={16} />
@@ -48,7 +48,7 @@ export const RecentForumPostItem: FC<RecentForumPostItemProps> = ({ post }) => {
         />
       </p>
 
-      <p className="line-clamp-1">{post.latestComment.body}</p>
+      <p className="line-clamp-1 lg:max-w-[580px] xl:max-w-[816px]">{post.latestComment.body}</p>
     </div>
   );
 };
