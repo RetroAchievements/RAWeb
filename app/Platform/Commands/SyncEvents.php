8333a6e03610bd1093e6a9d0806de02f06578048
Jamiras <jamirasmt@gmail.com>
2025-01-17 13:08:50 -0700

    add ConvertToTiered conversion

--- app/Platform/Commands/SyncEvents.php
+++ app/Platform/Commands/SyncEvents.php
@@ -9,11 +9,14 @@ use App\Models\Achievement;
 use App\Models\Game;
 use App\Models\Event;
 use App\Models\EventAchievement;
+use App\Models\EventAward;
 use App\Models\PlayerAchievement;
 use App\Models\PlayerBadge;
 use App\Models\System;
+use App\Models\User;
 use App\Platform\Enums\AchievementFlag;
 use App\Platform\Jobs\UpdateGameMetricsJob;
+use App\Platform\Jobs\UpdatePlayerGameMetricsJob;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -40,6 +43,20 @@ class SyncEvents extends Command
                 15025 => ['10/20/2018', '10/26/2018'],
                 3230 => ['10/27/2018', '11/2/2018'],
             ]),
+            13447 => new ConvertToTracked('aotw-2018-christmas', [
+                601 => ['11/30/2018', '12/6/2018'],
+                2530 => ['12/7/2018', '12/13/2018'],
+                31510 => ['12/14/2018', '12/20/2018'],
+                31278 => ['12/21/2018', '12/27/2018'],
+            ]),
+            13448 => new ConvertToTiered('devember-2018', [1 => '1 set fixed', 2 => '2 sets fixed'], [
+                68551 => ['Hotscrock'],
+                68552 => ['BenGhazi'],
+                68553 => ['Keltron3030', 'televandalist'],
+                68554 => ['SporyTike', 'kdecks', 'Salsa', 'JAM'],
+                68555 => ['Thoreau', 'Blazekickn', 'theztret00', 'Grenade44', 'DrPixel', 'ColonD', 'Tutumos'],
+                68556 => ['Zaphnath', 'Rimsala', 'SirVG', 'Jamiras', 'devidokop', 'MGNS8M', 'ikki5'],
+            ])
         ];
 
         $gameCount = count($gameConversions);
@@ -94,7 +111,6 @@ class ConvertGame
 
     protected function process(Command $command, Event $event): void
     {
-
     }
 
     protected function convertSiteAwards(Event $event): void
@@ -116,6 +132,100 @@ class ConvertGame
             ->where('AwardData', $event->legacyGame->id)
             ->where('AwardDataExtra', 0)
             ->delete();
+    }
+
+    protected function setAchievementCount(Event $event, int $count): void
+    {
+        // find the specified number of published achievements, and demote the rest
+        $publishedCount = 0;
+        $unofficialCount = 0;
+        for ($index = 0; $index < $event->legacyGame->achievements->count(); $index++)
+        {
+            $achievement = $event->legacyGame->achievements->skip($index)->first();
+            if ($achievement->Flags === AchievementFlag::OfficialCore->value)
+            {
+                if ($publishedCount === $count)
+                {
+                    $achievement->Flags = AchievementFlag::Unofficial->value;
+                    $achievement->save();
+                }
+                else
+                {
+                    $publishedCount++;
+                }
+            }
+            else
+            {
+                $unofficialCount++;
+            }
+        }
+
+        // didn't find enough published achievements, look for unpublished achievements to promote
+        if ($publishedCount < $count && $unofficialCount > 0)
+        {
+            for ($index = 0; $index < $event->legacyGame->achievements->count(); $index++)
+            {
+                $achievement = $event->legacyGame->achievements->skip($index)->first();
+                if ($achievement->Flags === AchievementFlag::Unofficial->value)
+                {
+                    $achievement->Flags = AchievementFlag::OfficialCore->value;
+                    $achievement->save();
+
+                    $publishedCount++;
+                    if ($publishedCount === $count)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        $event->legacyGame->achievements_published = $publishedCount;
+        $event->legacyGame->save();
+    }
+
+    protected function createEventAchievement(Command $command, Achievement $achievement, int $sourceAchievementId,
+        ?Carbon $activeFrom = null, ?Carbon $activeThrough = null): EventAchievement
+    {
+        if (!Achievement::exists($sourceAchievementId)) {
+            $command->error("Could not find source achievement: $sourceAchievementId");
+            return null;
+        }
+
+        $eventAchievement = EventAchievement::where('achievement_id', $achievement->id)->first();
+        if (!$eventAchievement) {
+            $eventAchievement = EventAchievement::create([
+                'achievement_id' => $achievement->id,
+                'source_achievement_id' => $sourceAchievementId,
+                'active_from' => $activeFrom,
+                'active_through' => $activeThrough,
+            ]);
+        } else {
+            $eventAchievement->source_achievement_id = $sourceAchievementId;
+            $eventAchievement->active_from = $activeFrom;
+            $eventAchievement->active_through = $activeThrough;
+            $eventAchievement->save();
+        }
+
+        // update unlock timestamps on the event achievements to match the source unlock
+        $winners = PlayerAchievement::where('achievement_id', $sourceAchievementId)
+            ->where('unlocked_hardcore_at', '>=', $eventAchievement->active_from)
+            ->where('unlocked_hardcore_at', '<', $eventAchievement->active_until);
+        foreach ($winners->get() as $winner) {
+            $unlock = PlayerAchievement::where('achievement_id', $achievement->id)
+                ->where('user_id', $winner->user_id)
+                ->first();
+            if ($unlock) {
+                $unlock->unlocked_hardcore_at = $unlock->unlocked_at = $winner->unlocked_hardcore_at;
+                $unlock->save();
+            }
+        }
+
+        // EventAchievementObserver will copy data and additional unlocks for anyone that
+        // wasn't awarded the badge from the source achievement
+        $eventAchievement->save();
+
+        return $eventAchievement;
     }
 }
 
@@ -175,71 +285,24 @@ class ConvertToTracked extends ConvertGame
 
     protected function process(Command $command, Event $event): void
     {
-        // demote extra achievements first so player metrics calculation will correctly
-        // identify completion
-        $index = $event->legacyGame->achievements->count();
-        while ($index > count($this->achievements))
-        {
-            $index--;
-            $achievement = $event->legacyGame->achievements->skip($index)->first();
-            $achievement->Flags = AchievementFlag::Unofficial->value;
-            $achievement->save();
-        }
-        $event->legacyGame->achievements_published = $index;
-        $event->legacyGame->save();
+        $this->setAchievementCount($event, count($this->achievements));
 
         // convert achievements to event achievements
         $index = 0;
         foreach ($this->achievements as $sourceAchievementId => $dates)
         {
-            $achievement = $event->legacyGame->achievements->skip($index)->first();
+            $achievement = $event->legacyGame->achievements->where('Flags', AchievementFlag::OfficialCore->value)->skip($index)->first();
             if (!$achievement) {
                 $command->error("Could not find achievement[$index]");
                 return;
             }
 
-            if (!Achievement::exists($sourceAchievementId)) {
-                $command->error("Could not find source achievement: $sourceAchievementId");
-                return;
-            }
-
-            $eventAchievement = EventAchievement::where('achievement_id', $achievement->id)->first();
-            if (!$eventAchievement) {
-                $eventAchievement = EventAchievement::create([
-                    'achievement_id' => $achievement->id,
-                    'source_achievement_id' => $sourceAchievementId,
-                    'active_from' => Carbon::parse($dates[0]),
-                    'active_through' => Carbon::parse($dates[1]),
-                ]);
-            } else {
-                $eventAchievement->source_achievement_id = $sourceAchievementId;
-                $eventAchievement->active_from = Carbon::parse($dates[0]);
-                $eventAchievement->active_through = Carbon::parse($dates[1]);
-                $eventAchievement->save();
-            }
+            $eventAchievement = $this->createEventAchievement($command, $achievement, $sourceAchievementId, Carbon::parse($dates[0]), Carbon::parse($dates[1]));
 
             if ($index === 0) {
                 $event->active_from = $eventAchievement->active_from;
             }
-
-            // update unlock timestamps on the event achievements to match the source unlock
-            $winners = PlayerAchievement::where('achievement_id', $sourceAchievementId)
-                ->where('unlocked_hardcore_at', '>=', $eventAchievement->active_from)
-                ->where('unlocked_hardcore_at', '<', $eventAchievement->active_until);
-            foreach ($winners->get() as $winner) {
-                $unlock = PlayerAchievement::where('achievement_id', $achievement->id)
-                    ->where('user_id', $winner->user_id)
-                    ->first();
-                if ($unlock) {
-                    $unlock->unlocked_hardcore_at = $unlock->unlocked_at = $winner->unlocked_hardcore_at;
-                    $unlock->save();
-                }
-            }
-
-            // EventAchievementObserver will copy data and additional unlocks for anyone that
-            // wasn't awarded the badge from the source achievement
-            $eventAchievement->save();
-
+    
             $index++;
         }
 
@@ -250,3 +313,123 @@ class ConvertToTracked extends ConvertGame
         dispatch(new UpdateGameMetricsJob($event->legacyGame->id))->onQueue('game-metrics');
     }
 }
+
+class ConvertToTiered extends ConvertGame
+{
+    protected array $tiers;
+    protected array $achievements;
+
+    public function __construct(string $slug, array $tiers, array $achievements) {
+        $this->slug = $slug;
+        $this->tiers = $tiers;
+        $this->achievements = $achievements;
+    }
+
+    protected function convertSiteAwards(Event $event): void
+    {
+        // do not process site awards here, we'll do it later so we can assign tiers
+    }
+
+    protected function process(Command $command, Event $event): void
+    {
+        $this->setAchievementCount($event, count($this->achievements));
+
+        $tier_counts = [];
+        $tier_index = 1;
+        foreach ($this->tiers as $count => $label)
+        {
+            $eventAward = EventAward::where('event_id', $event->id)
+                ->where('tier_index', $tier_index)
+                ->first();
+            
+            if (!$eventAward)
+            {
+                $eventAward = EventAward::create([
+                    'event_id' => $event->id,
+                    'tier_index' => $tier_index,
+                    'label' => $label,
+                    'achievements_required' => $count,
+                    'image_asset_path' => $event->image_asset_path,
+                ]);
+            }
+
+            $tier_counts[] = $count;
+            $tier_index++;
+        }
+
+        // convert achievements to event achievements
+        $tier_index = count($tier_counts);
+        $count = count($this->achievements);
+        $index = 0;
+        $allUserIds = [];
+        foreach ($this->achievements as $achievementId => $users)
+        {
+            $achievement = Achievement::find($achievementId);
+            if (!$achievement) {
+                $command->error("Could not find achievement: $achievementId");
+                return;
+            }
+
+            $userIds = User::whereIn('User', $users)->withTrashed()->pluck('ID')->toArray();
+            foreach ($userIds as $userId) {
+                // find hardcore badge
+                $badge = PlayerBadge::where('AwardType', AwardType::Mastery)
+                    ->where('AwardData', $event->legacyGame->id)
+                    ->where('AwardDataExtra', 1)
+                    ->where('user_id', $userId)
+                    ->first();
+                if (!$badge) {
+                    // hardcore badge not found, look for softcore badge
+                    $badge = PlayerBadge::where('AwardType', AwardType::Mastery)
+                        ->where('AwardData', $event->legacyGame->id)
+                        ->where('AwardDataExtra', 0)
+                        ->where('user_id', $userId)
+                        ->first();
+                }
+
+                if ($badge) {
+                    // convert found badge
+                    $badge->AwardType = AwardType::Event;
+                    $badge->AwardData = $event->id;
+                    $badge->AwardDataExtra = $tier_index;
+                    $badge->save();
+                }
+            }
+
+            $allUserIds = array_merge($allUserIds, $userIds);
+
+            // delete all unlocks for users not at this tier
+            PlayerAchievement::where('achievement_id', $achievementId)
+                ->whereNotIn('user_id', $allUserIds)
+                ->delete();
+
+            // update any softcore unlocks at this tier to hardcore
+            PlayerAchievement::where('achievement_id', $achievementId)
+                ->whereNull('unlocked_hardcore_at')
+                ->update(['unlocked_hardcore_at' => DB::raw('unlocked_at')]);
+
+            // update tier_index if crossing a threshold
+            if ($tier_index > 0 && $tier_counts[$tier_index - 1] === $count) {
+                $tier_index--;
+            }
+            $count--;
+
+            $index++;
+        }
+
+        // update metrics and sync to game_achievement_set
+        dispatch(new UpdateGameMetricsJob($event->legacyGame->id))->onQueue('game-metrics');
+
+        // delete any remaining badges
+        PlayerBadge::where('AwardType', AwardType::Mastery)
+            ->where('AwardData', $event->legacyGame->id)
+            ->where('AwardDataExtra', 0)
+            ->delete();
+
+        foreach ($allUserIds as $userId)
+        {
+            dispatch(new UpdatePlayerGameMetricsJob($userId, $event->legacyGame->id));
+        }
+    }
+}
+
