6264c3469386178ef59bcefb318c483f01457a19
Wes Copeland <wlcopeland1@gmail.com>
2025-01-18 15:03:32 -0500

    feat(forum): migrate forum topic create page to React

---
+++ resources/js/features/forums/components/CreateForumTopicMainRoot/CreateForumTopicMainRoot.tsx
@@ -0,0 +1,35 @@
+import { type FC, memo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { ForumBreadcrumbs } from '@/common/components/ForumBreadcrumbs';
+import { usePageProps } from '@/common/hooks/usePageProps';
+
+import { useForumPostPreview } from '../../hooks/useForumPostPreview';
+import { ForumPostCard } from '../ForumPostCard';
+import { CreateTopicForm } from './CreateTopicForm/CreateTopicForm';
+
+export const CreateForumTopicMainRoot: FC = memo(() => {
+  const { forum } = usePageProps<App.Data.CreateForumTopicPageProps>();
+
+  const { t } = useTranslation();
+
+  const { initiatePreview, previewContent } = useForumPostPreview();
+
+  return (
+    <div>
+      <ForumBreadcrumbs
+        forum={forum}
+        forumCategory={forum.category}
+        t_currentPageLabel={t('Start new topic')}
+      />
+
+      <CreateTopicForm onPreview={initiatePreview} />
+
+      {previewContent ? (
+        <div data-testid="preview-content" className="mt-4">
+          <ForumPostCard body={previewContent} />
+        </div>
+      ) : null}
+    </div>
+  );
+});
