a581f1ad67f7c7327d24fd72f85d35e6c4534087
Wes Copeland <wlcopeland1@gmail.com>
2025-01-23 17:59:28 -0500

    fix: address pr feedback

--- app/Platform/Services/GameSuggestions/Strategies/RevisedGameStrategy.php
+++ app/Platform/Services/GameSuggestions/Strategies/RevisedGameStrategy.php
@@ -6,6 +6,7 @@ namespace App\Platform\Services\GameSuggestions\Strategies;
 
 use App\Community\Enums\AwardType;
 use App\Models\Game;
+use App\Models\System;
 use App\Models\User;
 use App\Platform\Data\GameSuggestionContextData;
 use App\Platform\Enums\GameSuggestionReason;
@@ -27,6 +28,7 @@ class RevisedGameStrategy implements GameSuggestionStrategy
          * - The user has a prior mastery award for.
          */
         return Game::query()
+            ->whereNotIn('ConsoleID', System::getNonGameSystems())
             ->whereHas('playerGames', function (Builder $query) {
                 $query->whereUserId($this->user->id)->whereNotAllAchievementsUnlocked();
             })
