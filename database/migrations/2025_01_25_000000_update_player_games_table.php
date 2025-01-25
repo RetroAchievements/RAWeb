27cdb093cd2fa0e2aeed4a3a99e41587e4ea494e
Wes Copeland <wlcopeland1@gmail.com>
2025-01-25 17:05:17 -0500

    perf(CommonPlayersStrategy): add index to reduce suggestions bottleneck

---
+++ database/migrations/2025_01_25_000000_update_player_games_table.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    public function up(): void
+    {
+        Schema::table('player_games', function (Blueprint $table) {
+            $table->index([
+                'user_id',
+                'achievements_unlocked',
+                'achievements_total',
+                'deleted_at',
+                'game_id',
+            ], 'idx_player_games_suggestions'); // custom name needed because the auto-generated one is too long
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('player_games', function (Blueprint $table) {
+            $table->dropIndex('idx_player_games_suggestions');
+        });
+    }
+};
