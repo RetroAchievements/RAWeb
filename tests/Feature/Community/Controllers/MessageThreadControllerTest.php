5b5889fbcecfac9e1c130c5237c20fe29d685fa6
Wes Copeland <wlcopeland1@gmail.com>
2025-01-26 18:51:06 -0500

    perf: don't load every single message per topic

--- tests/Feature/Community/Controllers/MessageThreadControllerTest.php
+++ tests/Feature/Community/Controllers/MessageThreadControllerTest.php
@@ -65,7 +65,7 @@ class MessageThreadControllerTest extends TestCase
             ->has('paginatedMessageThreads.items', 1)
             ->where('paginatedMessageThreads.items.0.id', $thread->id)
             ->where('paginatedMessageThreads.items.0.title', $thread->title)
-            ->where('paginatedMessageThreads.items.0.lastMessageId', $thread->last_message_id)
+            ->has('paginatedMessageThreads.items.0.lastMessage.createdAt')
             ->where('paginatedMessageThreads.items.0.isUnread', false)
             ->has('paginatedMessageThreads.items.0.messages')
             ->has('paginatedMessageThreads.items.0.participants')
