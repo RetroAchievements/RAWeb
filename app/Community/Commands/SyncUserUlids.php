a22d5e296ba28b3554ce1568523a27b3956aef66
Wes Copeland <wlcopeland1@gmail.com>
2025-01-26 17:52:32 -0500

    feat(api): allow querying for user profile by ULID (proof of concept)

---
+++ app/Community/Commands/SyncUserUlids.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Community\Commands;
+
+use App\Models\User;
+use Carbon\Carbon;
+use DB;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class SyncUserUlids extends Command
+{
+    protected $signature = 'ra:sync:user-ulids';
+
+    protected $description = 'Sync user ulid field values';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        $total = User::whereNull('ulid')->count();
+
+        if ($total === 0) {
+            $this->info('No records need ULIDs.');
+
+            return;
+        }
+
+        $progressBar = $this->output->createProgressBar($total);
+        $progressBar->start();
+
+        User::withTrashed()
+            ->whereNull('ulid')
+            ->chunkById(4000, function ($users) use ($progressBar, &$lastTimestamp) {
+                $updates = [];
+
+                /** @var User $user */
+                foreach ($users as $user) {
+                    // Increment timestamp for each ULID to ensure some variance.
+                    $lastTimestamp++;
+
+                    $ulid = (string) Str::ulid(Carbon::createFromTimestamp($lastTimestamp));
+
+                    $updates[] = [
+                        'ID' => $user->id,
+                        'ulid' => $ulid,
+                    ];
+                }
+
+                // Perform a batch update for speed.
+                DB::table('UserAccounts')
+                    ->upsert(
+                        $updates,
+                        ['ID'],
+                        ['ulid']
+                    );
+
+                $progressBar->advance(count($updates));
+            });
+
+        $progressBar->finish();
+
+        $this->newLine();
+        $this->info('Done.');
+    }
+}
