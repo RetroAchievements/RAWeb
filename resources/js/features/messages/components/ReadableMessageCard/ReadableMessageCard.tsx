897d1b8f48b77efbdd0ff728d5bba82e0ab4cf70
Wes Copeland <wlcopeland1@gmail.com>
2025-02-01 13:56:02 -0500

    fix(ReadableMessageCard): mirror php timestamp logic

--- resources/js/features/messages/components/ReadableMessageCard/ReadableMessageCard.tsx
+++ resources/js/features/messages/components/ReadableMessageCard/ReadableMessageCard.tsx
@@ -2,27 +2,19 @@ import type { FC } from 'react';
 
 import { ShortcodeRenderer } from '@/common/components/ShortcodeRenderer';
 import { UserAvatar } from '@/common/components/UserAvatar';
-import { usePageProps } from '@/common/hooks/usePageProps';
-import { formatDate } from '@/common/utils/l10n/formatDate';
+
+import { MessageTimestamp } from './MessageTimestamp';
 
 interface ReadableMessageCardProps {
   message: App.Community.Data.Message;
 }
 
 export const ReadableMessageCard: FC<ReadableMessageCardProps> = ({ message }) => {
-  const { auth } = usePageProps();
-
   return (
     <div className="rounded bg-embed px-2.5 py-1.5">
       <div className="flex items-center gap-2">
         <UserAvatar {...message.author!} size={24} />
-
-        <p className="smalldate">
-          {formatDate(
-            message.createdAt,
-            auth?.user.preferences.prefersAbsoluteDates ? 'MMM DD, YYYY, HH:mm' : 'LLL',
-          )}
-        </p>
+        <MessageTimestamp message={message} />
       </div>
 
       <hr className="my-2 w-full border-embed-highlight" />
