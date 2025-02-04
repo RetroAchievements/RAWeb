ec3a0e7d1d3471877e8524184457c34e8c988a1d
Wes Copeland <wlcopeland1@gmail.com>
2025-02-04 18:52:01 -0500

    chore: pint

--- database/migrations/2025_02_02_000000_update_useraccounts_table.php
+++ database/migrations/2025_02_02_000000_update_useraccounts_table.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
     public function up(): void
