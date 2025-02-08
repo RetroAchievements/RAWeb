7b1f6ba27d4be7fd3b469cd7764af988defe7a5b
Wes Copeland <wlcopeland1@gmail.com>
2025-02-08 15:49:45 -0500

    chore: fix test

--- resources/js/features/messages/components/+show/MessagesShowRoot.test.tsx
+++ resources/js/features/messages/components/+show/MessagesShowRoot.test.tsx
@@ -137,8 +137,7 @@ describe('Component: MessagesShowRoot', () => {
 
     // ACT
     const comboboxEl = screen.getAllByRole('combobox')[0];
-    await userEvent.click(comboboxEl);
-    await userEvent.click(screen.getByRole('option', { name: '2' }));
+    await userEvent.selectOptions(comboboxEl, ['2']);
 
     // ASSERT
     expect(visitSpy).toHaveBeenCalledWith(
