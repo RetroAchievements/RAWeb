3d31d1e41b930ea1e1e39d42d551fbb80b58b5e5
Wes Copeland <wlcopeland1@gmail.com>
2025-02-23 12:00:01 -0500

    fix(BaseSelect): remediate Android overflow issue (#3237)

--- resources/js/common/components/+vendor/BaseSelect.tsx
+++ resources/js/common/components/+vendor/BaseSelect.tsx
@@ -46,12 +46,15 @@ const BaseSelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        'relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border light:border-neutral-200 light:bg-white',
+        'relative z-50 min-w-[8rem] overflow-hidden rounded-md border light:border-neutral-200 light:bg-white',
         'shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out light:text-neutral-950',
         'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95',
         'data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2',
         'data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         'border-neutral-800 bg-neutral-950 text-neutral-50',
+
+        // Don't overflow on mobile Android Chrome.
+        'max-h-[var(--radix-select-content-available-height)]',
 
         position === 'popper' &&
           'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
