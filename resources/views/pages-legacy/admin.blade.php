75dd4e88ce5671f0e54f4027fb031f700206dcc8
Wes Copeland <wlcopeland1@gmail.com>
2025-03-04 20:29:12 -0500

    fix: remediate another display_name issue

--- resources/views/pages-legacy/admin.blade.php
+++ resources/views/pages-legacy/admin.blade.php
@@ -19,7 +19,7 @@ if ($action === 'alt_identifier') {
     if ($forUser == null) {
         $message = "Unknown user: $altsForUser";
     } else {
-        $altsForUser = $forUser->User;
+        $altsForUser = "{$forUser->display_name} ({$forUser->User})";
 
         $emailAddresses = [];
         if (!empty($forUser->EmailAddress)) {
