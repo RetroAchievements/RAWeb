5b3ae988ee89add9b6aab421fd74a864ca2430ef
Wes Copeland <wlcopeland1@gmail.com>
2025-03-23 16:04:57 -0400

    fix: address feedback

--- app/Filament/Resources/PlatformResource.php
+++ app/Filament/Resources/PlatformResource.php
@@ -70,7 +70,7 @@ class PlatformResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+
                 ]),
             ]);
     }
