513812c89e5f80cb23b2aa9f9e23f9c9a0e55569
Wes Copeland <wlcopeland1@gmail.com>
2025-03-27 19:01:53 -0400

    fix(getUserProgress): return all game achievements (#3364)

--- app/Helpers/database/player-game.php
+++ app/Helpers/database/player-game.php
@@ -102,7 +102,7 @@ function getUserProgress(User $user, array $gameIDs, int $numRecentAchievements 
     if ($numRecentAchievements >= 0) {
         $gameAchievementsMap = [];
         foreach ($gameIDs as $gameID) {
-            $unlockedAchievementsQuery = Achievement::query()
+            $achievementsQuery = Achievement::query()
                 ->published()
                 ->where('GameID', $gameID)
                 ->with(['game'])
@@ -110,7 +110,6 @@ function getUserProgress(User $user, array $gameIDs, int $numRecentAchievements 
                     $join->on('player_achievements.achievement_id', '=', 'Achievements.ID');
                     $join->where('player_achievements.user_id', $user->id);
                 })
-                ->whereNotNull('player_achievements.unlocked_at')
                 ->select(
                     'Achievements.*',
                     'player_achievements.unlocked_at',
@@ -120,10 +119,12 @@ function getUserProgress(User $user, array $gameIDs, int $numRecentAchievements 
 
             // Only limit if we're not requesting all achievements.
             if ($numRecentAchievements > 0) {
-                $unlockedAchievementsQuery->limit($numRecentAchievements);
+                $achievementsQuery->whereNotNull('player_achievements.unlocked_at')
+                    ->orderBy('player_achievements.unlocked_at', 'desc')
+                    ->limit($numRecentAchievements);
             }
 
-            $gameAchievementsMap[$gameID] = $unlockedAchievementsQuery->get();
+            $gameAchievementsMap[$gameID] = $achievementsQuery->get();
         }
 
         foreach ($gameAchievementsMap as $gameID => $achievements) {
