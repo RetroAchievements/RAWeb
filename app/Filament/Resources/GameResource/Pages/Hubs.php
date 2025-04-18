8168e6fdf15db66dd5b8a8e85fb7e79175452851
Wes Copeland <wlcopeland1@gmail.com>
2025-04-18 19:55:37 -0400

    fix(manage): remediate server error on similar games dupe attach

--- app/Filament/Resources/GameResource/Pages/Hubs.php
+++ app/Filament/Resources/GameResource/Pages/Hubs.php
@@ -20,7 +20,7 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
-use Livewire;
+use Livewire\Livewire;
 
 class Hubs extends ManageRelatedRecords
 {
