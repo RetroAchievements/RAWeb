e8cf0fed32f683f6fd826d87edd7c36c20881f6c
Wes Copeland <wlcopeland1@gmail.com>
2025-04-24 17:33:18 -0400

    fix(useHydrateShortcodeDynamicEntities): sync atoms with page props

--- resources/js/common/hooks/useHydrateShortcodeDynamicEntities.ts
+++ resources/js/common/hooks/useHydrateShortcodeDynamicEntities.ts
@@ -1,4 +1,6 @@
+import { useSetAtom } from 'jotai';
 import { useHydrateAtoms } from 'jotai/utils';
+import { useEffect } from 'react';
 
 import {
   persistedAchievementsAtom,
@@ -7,16 +9,43 @@ import {
   persistedTicketsAtom,
   persistedUsersAtom,
 } from '../state/shortcode.atoms';
+import { usePageProps } from './usePageProps';
 
 export function useHydrateShortcodeDynamicEntities(
-  dynamicEntities: App.Community.Data.ShortcodeDynamicEntities,
+  initialDynamicEntities: App.Community.Data.ShortcodeDynamicEntities,
 ) {
+  const { dynamicEntities: propsDynamicEntities } = usePageProps<{
+    dynamicEntities: App.Community.Data.ShortcodeDynamicEntities;
+  }>();
+
+  // Always call useHydrateAtoms during SSR.
   useHydrateAtoms([
-    [persistedAchievementsAtom, dynamicEntities.achievements],
-    [persistedGamesAtom, dynamicEntities.games],
-    [persistedHubsAtom, dynamicEntities.hubs],
-    [persistedTicketsAtom, dynamicEntities.tickets],
-    [persistedUsersAtom, dynamicEntities.users],
-    //
+    [persistedAchievementsAtom, initialDynamicEntities.achievements],
+    [persistedGamesAtom, initialDynamicEntities.games],
+    [persistedHubsAtom, initialDynamicEntities.hubs],
+    [persistedTicketsAtom, initialDynamicEntities.tickets],
+    [persistedUsersAtom, initialDynamicEntities.users],
+  ]);
+
+  // These setters are only used for client-side updates.
+  const setPersistedAchievements = useSetAtom(persistedAchievementsAtom);
+  const setPersistedGames = useSetAtom(persistedGamesAtom);
+  const setPersistedHubs = useSetAtom(persistedHubsAtom);
+  const setPersistedTickets = useSetAtom(persistedTicketsAtom);
+  const setPersistedUsers = useSetAtom(persistedUsersAtom);
+
+  useEffect(() => {
+    setPersistedAchievements(propsDynamicEntities.achievements);
+    setPersistedGames(propsDynamicEntities.games);
+    setPersistedHubs(propsDynamicEntities.hubs);
+    setPersistedTickets(propsDynamicEntities.tickets);
+    setPersistedUsers(propsDynamicEntities.users);
+  }, [
+    propsDynamicEntities,
+    setPersistedAchievements,
+    setPersistedGames,
+    setPersistedHubs,
+    setPersistedTickets,
+    setPersistedUsers,
   ]);
 }
