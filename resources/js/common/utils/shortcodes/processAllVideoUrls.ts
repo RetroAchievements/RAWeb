c4aa0a700aa78eda39c57c334f950772f6b85c8e
Wes Copeland <wlcopeland1@gmail.com>
2025-04-24 08:20:29 -0400

    fix(processAllVideoUrls): update regex pattern to accomodate spoiler tags

--- resources/js/common/utils/shortcodes/processAllVideoUrls.ts
+++ resources/js/common/utils/shortcodes/processAllVideoUrls.ts
@@ -6,17 +6,20 @@ import { processVideoUrl } from './processVideoUrl';
  * Handles both YouTube and Twitch URLs, preserving any parameters.
  */
 export function processAllVideoUrls(text: string): string {
-  return text.replace(/https?:\/\/[^\s<>]+\b/g, (url, offset) => {
-    const videoInfo = processVideoUrl(url);
+  return text.replace(
+    /https?:\/\/[^\s\[\]<>,.]+(?:\.[^\s\[\]<>,.]+)*[^\s\[\]<>,.]/g,
+    (url, offset) => {
+      const videoInfo = processVideoUrl(url);
 
-    if (
-      videoInfo &&
-      !getIsInsideBbcodeTag(offset, text, 'url') &&
-      !getIsInsideBbcodeTag(offset, text, 'video')
-    ) {
-      return `[video]${url}[/video]`;
-    }
+      if (
+        videoInfo &&
+        !getIsInsideBbcodeTag(offset, text, 'url') &&
+        !getIsInsideBbcodeTag(offset, text, 'video')
+      ) {
+        return `[video]${url}[/video]`;
+      }
 
-    return url;
-  });
+      return url;
+    },
+  );
 }
