6218958e72921fb781bb137fa6894428bbbb0e84
Jamiras <jamirasmt@gmail.com>
2025-05-23 07:02:44 -0600

    show alternate release titles on game page

--- resources/views/components/game/heading.blade.php
+++ resources/views/components/game/heading.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Community\Enums\UserGameListType;
+use App\Models\GameRelease;
 use App\Models\System;
 use App\Enums\Permissions;
 ?>
@@ -17,12 +18,24 @@ use App\Enums\Permissions;
 <?php
 $addToListType = UserGameListType::Play;
 $iconUrl = getSystemIconUrl($consoleId);
+
+$alternateReleases = GameRelease::query()
+    ->where('game_id', $gameId)
+    ->where('is_canonical_game_title', false)
+    ->select('title')
+    ->pluck('title')
+    ->toArray();
+$alternateTitles = implode(', ', $alternateReleases);
 ?>
 
 <h1 class="text-h3">
     <span class="block mb-1">
         <x-game-title :rawTitle="$gameTitle" />
     </span>
+
+    @if ($alternateTitles)
+    <span class="block mb-1 smalltext">Also known as {{ $alternateTitles }}</span>
+    @endif
 
     <div class="flex justify-between">
         <div class="flex items-center gap-x-1">
