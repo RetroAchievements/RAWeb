cbb4be3a74b31383e267d471819502ead26d46e5
Wes Copeland <wlcopeland1@gmail.com>
2025-05-24 09:24:20 -0400

    fix: remount Alpine.js tooltip props correctly, key GameAvatar images

--- resources/js/common/components/GameAvatar/GameAvatar.tsx
+++ resources/js/common/components/GameAvatar/GameAvatar.tsx
@@ -80,6 +80,7 @@ export const GameAvatar: FC<GameAvatarProps> = ({
             <GlowingImage width={size} height={size} src={badgeUrl} alt={title ?? 'Game'} />
           ) : (
             <img
+              key={badgeUrl}
               loading={loading}
               decoding={decoding}
               width={size}
@@ -87,7 +88,10 @@ export const GameAvatar: FC<GameAvatarProps> = ({
               src={badgeUrl}
               alt={title ?? 'Game'}
               style={{ aspectRatio: '1/1' }}
-              className={cn('rounded-sm object-cover', variant === 'inline' ? 'mr-1.5' : null)}
+              className={cn(
+                'rounded-sm bg-neutral-800 object-cover light:bg-neutral-100',
+                variant === 'inline' ? 'mr-1.5' : null,
+              )}
             />
           )}
         </>
