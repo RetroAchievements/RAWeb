48690228b630b1c6488adae270eb2c2018a851cb
Wes Copeland <wlcopeland1@gmail.com>
2025-06-20 09:53:16 -0400

    chore: fix typo

--- resources/js/features/games/utils/buildGameMetaDescription.ts
+++ resources/js/features/games/utils/buildGameMetaDescription.ts
@@ -3,5 +3,5 @@ export function buildGameMetaDescription(game: App.Platform.Data.Game): string {
     return `No achievements have been created yet for ${game.title}. Join RetroAchievements to request achievements for this game and earn achievements on many other classic games.`;
   }
 
-  return `There are ${game.achievementsPublished} achievements worth ${game.pointsTotal!.toLocaleString()}. ${game.title} for ${game.system!.name} - explore and compete on this classic game at RetroAchievements.`;
+  return `There are ${game.achievementsPublished} achievements worth ${game.pointsTotal!.toLocaleString()} points. ${game.title} for ${game.system!.name} - explore and compete on this classic game at RetroAchievements.`;
 }
