df476f80209dddefabc9425fafc7d78f62362f31
Wes Copeland <wlcopeland1@gmail.com>
2025-06-20 13:34:16 -0400

    test: add coverage

--- resources/js/features/games/utils/buildGameMetaDescription.test.ts
+++ resources/js/features/games/utils/buildGameMetaDescription.test.ts
@@ -21,13 +21,39 @@ describe('Util: buildGameMetaDescription', () => {
 
   it('given the game has achievements published, outputs the correct message', () => {
     // ARRANGE
-    const system = createSystem();
-    const game = createGame({ system, achievementsPublished: 100, pointsTotal: 800 });
+    const system = createSystem({ name: 'NES' });
+    const game = createGame({
+      title: 'Super Mario Bros.',
+      system,
+      achievementsPublished: 100,
+      pointsTotal: 800,
+    });
 
     // ACT
     const result = buildGameMetaDescription(game);
 
     // ASSERT
-    expect(result).toContain('There are 100 achievements');
+    expect(result).toBe(
+      'There are 100 achievements worth 800 points. Super Mario Bros. for NES - explore and compete on this classic game at RetroAchievements.',
+    );
+  });
+
+  it('given the game has 1,200 points, formats the number with a comma', () => {
+    // ARRANGE
+    const system = createSystem({ name: 'SNES' });
+    const game = createGame({
+      title: 'The Legend of Zelda',
+      system,
+      achievementsPublished: 50,
+      pointsTotal: 1200, // !! should have comma formatting
+    });
+
+    // ACT
+    const result = buildGameMetaDescription(game);
+
+    // ASSERT
+    expect(result).toBe(
+      'There are 50 achievements worth 1,200 points. The Legend of Zelda for SNES - explore and compete on this classic game at RetroAchievements.',
+    );
   });
 });
