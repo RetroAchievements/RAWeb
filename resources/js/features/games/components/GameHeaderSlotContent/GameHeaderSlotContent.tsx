c61860e25e4987fb9294ef1755a4a2003bf73a26
Wes Copeland <wlcopeland1@gmail.com>
2025-06-22 14:32:03 -0400

    feat(game): add Want to Play/Develop toggle buttons to React game pages (#3648)
    * feat(game): add Want to Play/Develop toggle buttons to React game pages
    
    * fix: update toast message
    
    * chore: fix annoying flaky test
    
    * fix: address feedback
    
    * feat: want to revise label
    
    * fix: re-add missing string

---
+++ resources/js/features/games/components/GameHeaderSlotContent/GameHeaderSlotContent.tsx
@@ -0,0 +1,35 @@
+import type { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { LuCheck, LuPlus } from 'react-icons/lu';
+
+import { BaseButton } from '@/common/components/+vendor/BaseButton';
+import { usePageProps } from '@/common/hooks/usePageProps';
+import { useGameBacklogState } from '@/features/game-list/components/GameListItems/useGameBacklogState';
+
+export const GameHeaderSlotContent: FC = () => {
+  const { game, isOnWantToPlayList: isInitiallyOnWantToPlayList } =
+    usePageProps<App.Platform.Data.GameShowPageProps>();
+
+  const { t } = useTranslation();
+
+  const { toggleBacklog: toggleWantToPlay, isInBacklogMaybeOptimistic: isOnWantToPlayList } =
+    useGameBacklogState({
+      game,
+      isInitiallyInBacklog: isInitiallyOnWantToPlayList,
+      userGameListType: 'play',
+    });
+
+  return (
+    <div className="flex items-center gap-2">
+      <BaseButton
+        onClick={() => toggleWantToPlay()}
+        className="flex items-center gap-1 rounded-full !py-0 !text-xs"
+        size="sm"
+        aria-pressed={isOnWantToPlayList}
+      >
+        {isOnWantToPlayList ? <LuCheck className="size-4" /> : <LuPlus className="size-4" />}
+        {t('Want to Play')}
+      </BaseButton>
+    </div>
+  );
+};
