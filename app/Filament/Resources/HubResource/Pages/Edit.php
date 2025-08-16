fd4726cec7ef9193380183b9e04e55bd0028dc52
Wes Copeland <wlcopeland1@gmail.com>
2025-08-16 12:51:58 -0400

    fix(manage): handle Livewire temporary image errors (#3766)
    * fix(manage): handle Livewire temporary image errors
    
    * fix: address feedback
    
    * chore: pint

--- app/Filament/Resources/HubResource/Pages/Edit.php
+++ app/Filament/Resources/HubResource/Pages/Edit.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\HubResource\Pages;
 
-use App\Filament\Actions\ProcessUploadedImageAction;
+use App\Filament\Actions\ApplyUploadedImageToDataAction;
 use App\Filament\Enums\ImageUploadType;
 use App\Filament\Resources\HubResource;
-use App\Models\GameSet;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -25,20 +24,7 @@ class Edit extends EditRecord
 
     protected function mutateFormDataBeforeSave(array $data): array
     {
-        /** @var GameSet $record */
-        $record = $this->record;
-
-        $existingImage = $record->image_asset_path;
-
-        if (isset($data['image_asset_path'])) {
-            $data['image_asset_path'] = (new ProcessUploadedImageAction())->execute(
-                $data['image_asset_path'],
-                ImageUploadType::HubBadge
-            );
-        } else {
-            // If no new image was uploaded, retain the existing image.
-            $data['image_asset_path'] = $existingImage;
-        }
+        (new ApplyUploadedImageToDataAction())->execute($data, 'image_asset_path', ImageUploadType::HubBadge);
 
         return $data;
     }
