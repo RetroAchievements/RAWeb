6d5890d8180375815ca77034d04c7097a64d9618
ludvigrosenqvist <ludvigrosenqvist@gmail.com>
2025-08-16 12:00:37 +0200

    Added sort by award date function

--- resources/js/tall-stack/dynamic/reorderSiteAwards.ts
+++ resources/js/tall-stack/dynamic/reorderSiteAwards.ts
@@ -1,5 +1,6 @@
 export * from './reorderSiteAwards/handleRowDragOver';
 export * from './reorderSiteAwards/handleShowSavedHiddenRowsChange';
+export * from './reorderSiteAwards/handleSortByAwardDate';
 export * from './reorderSiteAwards/index';
 export * from './reorderSiteAwards/refreshVisibilityAfterSave';
 export { reorderSiteAwardsStore as store } from './reorderSiteAwards/reorderSiteAwardsStore';
