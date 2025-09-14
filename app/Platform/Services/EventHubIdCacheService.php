e8b4abf460772f03d6c1700f4f96d56074c24037
Wes Copeland <wlcopeland1@gmail.com>
2025-09-14 10:14:50 -0400

    perf(game): optimize hub loading

---
+++ app/Platform/Services/EventHubIdCacheService.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Platform\Services;
+
+use App\Models\GameSet;
+use App\Models\GameSetLink;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+
+class EventHubIdCacheService
+{
+    /**
+     * @return Collection<int, int>
+     */
+    public static function getEventHubIds(): Collection
+    {
+        return Cache::flexible('event_hub_ids', [
+            86_400,  // Fresh for 24 hours.
+            604_800, // We can serve it stale indefinitely, it doesn't really matter.
+        ], function () {
+            $childEventHubIds = GameSetLink::query()
+                ->whereIn('parent_game_set_id', [
+                    GameSet::CommunityEventsHubId,
+                    GameSet::DeveloperEventsHubId,
+                ])
+                ->pluck('child_game_set_id');
+
+            $titleEventHubIds = GameSet::where('title', 'like', '%Events -%')
+                ->pluck('id');
+
+            return $childEventHubIds->merge($titleEventHubIds)->unique();
+        });
+    }
+
+    /**
+     * Clear the cache when event hubs change.
+     */
+    public static function clearCache(): void
+    {
+        Cache::forget('event_hub_ids');
+    }
+}
