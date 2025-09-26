294a43313f79d4886c5ee6c4235ebff162947a8d
Wes Copeland <wlcopeland1@gmail.com>
2025-09-25 20:52:49 -0400

    perf(game): extract repetitive CSS to dedicated classnames

--- resources/js/common/components/AchievementsListItem/AchievementsListItem.tsx
+++ resources/js/common/components/AchievementsListItem/AchievementsListItem.tsx
@@ -58,7 +58,7 @@ export const AchievementsListItem: FC<AchievementsListItemProps> = ({
 
   return (
     <motion.li
-      className="flex w-full gap-x-3.5 px-2 py-3 odd:bg-[rgba(50,50,50,0.4)] light:odd:bg-neutral-100 md:gap-x-3 md:py-1"
+      className="game-set-item"
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 10 }}
