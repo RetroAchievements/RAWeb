5c6f242626b0ff7c18e8e08a349602e50f03486d
Wes Copeland <wlcopeland1@gmail.com>
2025-09-26 15:36:00 -0400

    perf(game): extract repetitive CSS to dedicated classnames (#3962)

--- resources/js/common/components/+vendor/BaseButton.tsx
+++ resources/js/common/components/+vendor/BaseButton.tsx
@@ -6,54 +6,29 @@ import { type ButtonHTMLAttributes, forwardRef, useMemo } from 'react';
 
 import { cn } from '@/common/utils/cn';
 
-const baseButtonVariants = cva(
-  [
-    'inline-flex items-center justify-center whitespace-nowrap rounded text-sm font-medium light:ring-offset-white',
-    'focus-visible:outline-none focus-visible:ring-1 light:focus-visible:ring-neutral-950 focus-visible:ring-offset-2',
-    'disabled:pointer-events-none disabled:opacity-50',
-    'ring-offset-neutral-950 focus-visible:ring-neutral-300 select-none',
-    'lg:active:translate-y-[1px] lg:active:scale-[0.98] lg:disabled:active:scale-100 lg:disabled:active:translate-y-0 lg:transition-transform lg:duration-100',
-  ],
-  {
-    variants: {
-      variant: {
-        default: cn(
-          'bg-embed text-link border border-neutral-700 hover:bg-embed-highlight hover:text-link-hover hover:border-menu-link',
-          'light:bg-white light:border-link light:text-link light:hover:bg-neutral-100',
-          'disabled:hover:bg-embed disabled:hover:text-link disabled:hover:border-neutral-700',
-          'disabled:light:hover:bg-white disabled:light:hover:text-link disabled:light:hover:border-link',
-        ),
-        defaultDisabled: cn(
-          'opacity-50 lg:active:!scale-100 lg:active:!translate-y-0 lg:!transition-none',
-          'bg-embed text-link border border-neutral-700',
-          'light:bg-white light:border-link light:text-link',
-          'hover:bg-embed hover:text-link hover:border-neutral-700',
-          'light:hover:bg-white light:hover:text-link light:hover:border-link',
-        ),
-        destructive: 'bg-embed border btn-danger hover:text-link-hover hover:border-menu-link',
-        outline: cn(
-          'border light:border-neutral-200 light:bg-white light:hover:bg-neutral-100 light:hover:text-neutral-900',
-          'border-neutral-800 bg-neutral-950 hover:bg-neutral-800 hover:text-neutral-50',
-        ),
-        secondary:
-          'light:bg-neutral-200 light:text-neutral-900 bg-neutral-800 text-neutral-50 hover:bg-neutral-800/80',
-        ghost:
-          'light:hover:bg-neutral-100 light:hover:text-neutral-900 hover:bg-neutral-800 hover:text-neutral-50',
-        link: 'light:text-neutral-900 underline-offset-4 hover:underline text-link',
-      },
-      size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-[30px] rounded-md px-3 !text-[13px]',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
-      },
+const baseButtonVariants = cva(['btn-base'], {
+  variants: {
+    variant: {
+      default: 'btn-base--default',
+      defaultDisabled: 'btn-base--default-disabled',
+      destructive: 'btn-base--destructive',
+      outline: 'btn-base--outline',
+      secondary: 'btn-base--secondary',
+      ghost: 'btn-base--ghost',
+      link: 'btn-base--link',
     },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
+    size: {
+      default: 'btn-base--size-default',
+      sm: 'btn-base--size-sm',
+      lg: 'btn-base--size-lg',
+      icon: 'btn-base--size-icon',
     },
   },
-);
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+});
 
 export interface BaseButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
