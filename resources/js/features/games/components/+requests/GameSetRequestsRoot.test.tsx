a2ee9af7b27e14594688b38b9b61ca882884c401
Chew <chew@chew.pw>
2025-10-01 13:32:03 -0500

    feat(tests): add some more tests

--- resources/js/features/games/components/+requests/GameSetRequestsRoot.test.tsx
+++ resources/js/features/games/components/+requests/GameSetRequestsRoot.test.tsx
@@ -1,28 +1,60 @@
+import { t } from 'i18next';
+import { expect } from 'vitest';
+
 import { render } from '@/test';
-import { createGame, createSystem } from '@/test/factories';
+import { createGame, createUser } from '@/test/factories';
 
 import { GameSetRequestsRoot } from './GameSetRequestsRoot';
 
 describe('Component: GameSetRequestsRoot', () => {
   it('renders without crashing', () => {
-    const game = createGame({
-      badgeUrl: 'badge.jpg',
-      system: createSystem({
-        iconUrl: 'icon.jpg',
-      }),
-    });
+    const fakeGame = createGame();
 
-    // ARRANGE
+    // Build Container
     const { container } = render(<GameSetRequestsRoot />, {
       pageProps: {
         deferredRequestors: null,
         initialRequestors: [],
-        game,
+        game: fakeGame,
         totalCount: 0,
       } as App.Community.Data.GameSetRequestsPageProps,
     });
 
     // ASSERT
     expect(container).toBeTruthy();
+  });
+
+  it('shows no one if there are no requestors', () => {
+    const fakeGame = createGame();
+
+    // Build Container
+    const { container } = render(<GameSetRequestsRoot />, {
+      pageProps: {
+        deferredRequestors: null,
+        initialRequestors: [],
+        game: fakeGame,
+        totalCount: 0,
+      } as App.Community.Data.GameSetRequestsPageProps,
+    });
+
+    expect(container).toHaveTextContent(t('There are currently no active requests.'));
+  });
+
+  it('shows if there is one requestor', () => {
+    const fakeGame = createGame();
+    const fakeUser = createUser();
+
+    const { container } = render(<GameSetRequestsRoot />, {
+      pageProps: {
+        deferredRequestors: null,
+        initialRequestors: [fakeUser],
+        game: fakeGame,
+        totalCount: 1,
+      } as App.Community.Data.GameSetRequestsPageProps,
+    });
+
+    expect(container).toHaveTextContent(
+      'This achievement set has been requested by the following 1 users:',
+    );
   });
 });
