303c1bd23b0bd9b29d8e081bf84ad7250d591602
Wes Copeland <wlcopeland1@gmail.com>
2025-10-29 19:40:31 -0400

    chore: phpstan

--- app/Connect/Controllers/ConnectApiController.php
+++ app/Connect/Controllers/ConnectApiController.php
@@ -5,14 +5,10 @@ declare(strict_types=1);
 namespace App\Connect\Controllers;
 
 use App\Connect\Concerns\AchievementRequests;
-use App\Connect\Concerns\AuthRequests;
 use App\Connect\Concerns\BootstrapRequests;
-use App\Connect\Concerns\DevelopmentRequests;
-use App\Connect\Concerns\GameRequests;
 use App\Connect\Concerns\HeartbeatRequests;
 use App\Connect\Concerns\LeaderboardRequests;
 use App\Connect\Concerns\LegacyCompatProxyRequests;
-use App\Connect\Concerns\TicketRequests;
 use App\Http\Controller;
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
