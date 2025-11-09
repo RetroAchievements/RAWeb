59be8c0da38c7a651ce92ab703375a7e7cd0ff52
Wes Copeland <wlcopeland1@gmail.com>
2025-11-09 11:20:42 -0500

    refactor(game): convert the achievement set tooltip cards to use BaseHoverCard

--- resources/js/features/games/components/GameAchievementSetsContainer/SetSelectionTabs/SetSelectionTabs.tsx
+++ resources/js/features/games/components/GameAchievementSetsContainer/SetSelectionTabs/SetSelectionTabs.tsx
@@ -2,14 +2,14 @@ import { useAtomValue } from 'jotai';
 import { type FC } from 'react';
 import { route } from 'ziggy-js';
 
-import { BaseTooltip, BaseTooltipTrigger } from '@/common/components/+vendor/BaseTooltip';
+import { BaseHoverCard, BaseHoverCardTrigger } from '@/common/components/+vendor/BaseHoverCard';
 import { InertiaLink } from '@/common/components/InertiaLink';
 import { usePageProps } from '@/common/hooks/usePageProps';
 import { cn } from '@/common/utils/cn';
 import { currentListViewAtom } from '@/features/games/state/games.atoms';
 import { BASE_SET_LABEL } from '@/features/games/utils/baseSetLabel';
 
-import { GameAchievementSetTooltipContent } from '../../GameAchievementSetTooltipContent';
+import { GameAchievementSetHoverCardContent } from '../../GameAchievementSetHoverCardContent';
 import { useTabIndicator } from './useTabIndicator';
 
 interface SetSelectionTabsProps {
@@ -17,7 +17,7 @@ interface SetSelectionTabsProps {
 }
 
 export const SetSelectionTabs: FC<SetSelectionTabsProps> = ({ activeTab }) => {
-  const { game, selectableGameAchievementSets, ziggy } =
+  const { game, selectableGameAchievementSets } =
     usePageProps<App.Platform.Data.GameShowPageProps>();
 
   const currentListView = useAtomValue(currentListViewAtom);
@@ -47,8 +47,8 @@ export const SetSelectionTabs: FC<SetSelectionTabsProps> = ({ activeTab }) => {
       {/* Tabs */}
       <div className="relative flex items-center space-x-[6px]">
         {selectableGameAchievementSets.map((gas, index) => (
-          <BaseTooltip key={gas.id} open={ziggy.device === 'mobile' ? false : undefined}>
-            <BaseTooltipTrigger>
+          <BaseHoverCard key={gas.id} openDelay={300} closeDelay={0}>
+            <BaseHoverCardTrigger asChild>
               <InertiaLink
                 href={route('game2.show', {
                   game: game.id,
@@ -80,10 +80,10 @@ export const SetSelectionTabs: FC<SetSelectionTabsProps> = ({ activeTab }) => {
                   />
                 </div>
               </InertiaLink>
-            </BaseTooltipTrigger>
+            </BaseHoverCardTrigger>
 
-            <GameAchievementSetTooltipContent gameAchievementSet={gas} />
-          </BaseTooltip>
+            <GameAchievementSetHoverCardContent gameAchievementSet={gas} />
+          </BaseHoverCard>
         ))}
       </div>
     </div>
