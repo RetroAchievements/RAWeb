026218263a6aeeccdacfaff06bc241948569d844
Wes Copeland <wlcopeland1@gmail.com>
2025-12-05 21:25:22 -0500

    fix: update JsonResponse

--- app/Http/Middleware/JsonResponse.php
+++ app/Http/Middleware/JsonResponse.php
@@ -11,7 +11,9 @@ class JsonResponse
 {
     public function handle(Request $request, Closure $next): mixed
     {
-        if (!$request->headers->has('Accept')) {
+        // Don't override the Accept header for JSON:API routes.
+        // JSON:API requires Accept: application/vnd.api+json
+        if (!str_starts_with($request->path(), 'api/v2/')) {
             $request->headers->set('Accept', 'application/json');
         }
 
