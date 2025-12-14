38d36bd2875e7804e9c974d2e2998f3e54049696
drisc <cory@drisc.io>
2025-12-14 20:33:10 +0000

    feat(migration): add index

--- database/migrations/2025_12_08_145911_update_leaderboarddef_status.php
+++ database/migrations/2025_12_08_145911_update_leaderboarddef_status.php
@@ -10,13 +10,14 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('LeaderboardDef', function (Blueprint $table) {
-            $table->string('state')->default('active')->after('DisplayOrder');
+            $table->string('state')->default('active')->after('DisplayOrder')->index();
         });
     }
 
     public function down(): void
     {
         Schema::table('LeaderboardDef', function (Blueprint $table) {
+            $table->dropIndex('state');
             $table->dropColumn('state');
         });
     }
