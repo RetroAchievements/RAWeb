87fbfe72b10d229331134625d156793bb840653c
drisc <cory@drisc.io>
2025-12-26 01:53:31 +0000

    feat(passport): create ensure command and update composer.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,7 @@ use App\Console\Commands\CleanupAvatars;
 use App\Console\Commands\DeleteExpiredEmailVerificationTokens;
 use App\Console\Commands\DeleteExpiredPasswordResetTokens;
 use App\Console\Commands\DeleteOverdueUserAccounts;
+use App\Console\Commands\EnsurePassportKeys;
 use App\Console\Commands\GenerateTypeScript;
 use App\Console\Commands\LogUsersOnlineCount;
 use App\Console\Commands\PruneApiLogs;
@@ -67,6 +68,7 @@ class AppServiceProvider extends ServiceProvider
                 LogUsersOnlineCount::class,
                 PruneApiLogs::class,
                 SquashMigrations::class,
+                EnsurePassportKeys::class,
 
                 // User Accounts
                 CleanupAvatars::class,
