b98b8f524c1b35486831d1c354acc298dd4223e6
Wes Copeland <wlcopeland1@gmail.com>
2026-01-09 08:20:59 -0500

    fix: also filter out alerts for unranked users

--- app/Support/Alerts/Listeners/TriggerSuspiciousBeatTimeAlert.php
+++ app/Support/Alerts/Listeners/TriggerSuspiciousBeatTimeAlert.php
@@ -34,6 +34,10 @@ class TriggerSuspiciousBeatTimeAlert implements ShouldQueue
 
         $game = $event->game;
 
+        if ($event->user->is_unranked) {
+            return;
+        }
+
         // Final Fantasy XI supports retroactive unlocks.
         // We use a regex so we don't match against XII or XIII.
         if (preg_match('/^Final Fantasy XI($|[:\s\-])/', $game->title)) {
