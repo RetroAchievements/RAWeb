a4cc5e54420ee86d6fc77d192f568c55e69cc1b0
Chew <chew@chew.pw>
2026-02-17 22:34:26 -0600

    bring over changes from event badge choosing

--- app/Community/Controllers/ReorderSiteAwardsController.php
+++ app/Community/Controllers/ReorderSiteAwardsController.php
@@ -38,7 +38,7 @@ class ReorderSiteAwardsController extends Controller
 
         $query = "
         -- game awards (mastery, beaten)
-        SELECT " . unixTimestampStatement('saw.awarded_at', 'AwardedAt') . ", saw.award_type, saw.user_id, saw.award_key, saw.award_tier, saw.order_column, gd.title AS Title, s.id AS ConsoleID, s.name AS ConsoleName, NULL AS Flags, gd.image_icon_asset_path AS ImageIcon
+        SELECT " . unixTimestampStatement('saw.awarded_at', 'AwardedAt') . ", saw.award_type, saw.user_id, saw.award_key, saw.award_tier, saw.order_column, gd.title AS Title, s.id AS ConsoleID, s.name AS ConsoleName, NULL AS Flags, gd.image_icon_asset_path AS ImageIcon, NULL AS display_award_tier
             FROM user_awards AS saw
             LEFT JOIN games AS gd ON ( gd.id = saw.award_key AND saw.award_type IN ('{$gameAwardValues}') )
             LEFT JOIN systems AS s ON s.id = gd.system_id
@@ -59,7 +59,7 @@ class ReorderSiteAwardsController extends Controller
                 ))
         UNION
         -- event awards
-        SELECT " . unixTimestampStatement('saw.awarded_at', 'AwardedAt') . ", saw.award_type, saw.user_id, saw.award_key, saw.award_tier, saw.order_column, gd.title AS Title, " . System::Events . ", 'Events', NULL, e.image_asset_path AS ImageIcon
+        SELECT " . unixTimestampStatement('saw.awarded_at', 'AwardedAt') . ", saw.award_type, saw.user_id, saw.award_key, saw.award_tier, saw.order_column, gd.title AS Title, " . System::Events . ", 'Events', NULL, e.image_asset_path AS ImageIcon, saw.display_award_tier
             FROM user_awards AS saw
             LEFT JOIN events e ON e.id = saw.award_key
             LEFT JOIN games gd ON gd.id = e.legacy_game_id
@@ -68,7 +68,7 @@ class ReorderSiteAwardsController extends Controller
                 AND saw.user_id = :userId3
         UNION
         -- non-game awards (developer contribution, ...)
-        SELECT " . unixTimestampStatement('MAX(saw.awarded_at)', 'AwardedAt') . ", saw.award_type, saw.user_id, MAX( saw.award_key ), saw.award_tier, saw.order_column, NULL, NULL, NULL, NULL, NULL
+        SELECT " . unixTimestampStatement('MAX(saw.awarded_at)', 'AwardedAt') . ", saw.award_type, saw.user_id, MAX( saw.award_key ), saw.award_tier, saw.order_column, NULL, NULL, NULL, NULL, NULL, NULL
             FROM user_awards AS saw
             WHERE
                 saw.award_type NOT IN('{$gameAwardValues}','" . AwardType::Event->value . "')
@@ -126,8 +126,10 @@ class ReorderSiteAwardsController extends Controller
         }
 
         $eventData = new Collection;
+        $eventAwardData = new Collection;
         if (! empty($awardEventIds)) {
             $eventData = Event::whereIn('id', $awardEventIds)->with('legacyGame')->get()->keyBy('id');
+            $eventAwardData = EventAward::whereIn('event_id', $awardEventIds)->get()->groupBy('event_id');
         }
 
         $gameAwards = []; // Mastery awards that aren't Events.
@@ -164,28 +166,50 @@ class ReorderSiteAwardsController extends Controller
 
                 $event = $eventData->find($id);
                 if ($event) {
-                    $tooltip = "Awarded for completing the $event->title event";
+                    $tooltipTitle = $event->title;
+                    $tooltipDescription = "Awarded for completing this event";
                     $image = $event->image_asset_path;
 
-                    if ($extra !== 0) {
-                        $eventAward = EventAward::where('event_id', $id)
-                            ->where('tier_index', $extra)
-                            ->first();
+                    // Use the display preference for the badge image, but always
+                    // use the actual earned tier for the tooltip text. Otherwise,
+                    // it's very ambiguous what tier the player is actually on if
+                    // they have a saved tier preference.
+                    $displayTier = (int) ($award['display_award_tier'] ?? $extra);
+                    $actualTier = $extra;
 
-                        if ($eventAward) {
-                            $image = $eventAward->image_asset_path;
+                    $tierIndicesToFetch = array_unique([$displayTier, $actualTier]);
+                    $eventAwardsByTier = ($eventAwardData->get($id) ?? collect())
+                        ->whereIn('tier_index', $tierIndicesToFetch)
+                        ->keyBy('tier_index');
 
-                            if ($eventAward->points_required < $event->legacyGame->points_total) {
-                                $tooltip = "Awarded for earning at least $eventAward->points_required points in the $event->title event";
-                            }
-                        }
+                    $displayEventAward = $eventAwardsByTier->get($displayTier);
+                    if ($displayEventAward) {
+                        $image = $displayEventAward->image_asset_path;
                     }
 
-                    $award["Tooltip"] = $tooltip;
+                    $actualEventAward = $eventAwardsByTier->get($actualTier);
+                    if ($actualEventAward && $actualEventAward->points_required < $event->legacyGame->points_total) {
+                        // Strip the event/game title prefix from the tier label to avoid duplication.
+                        $tierLabel = $actualEventAward->label;
+                        $gameTitle = $event->legacyGame->title ?? '';
+                        if ($tierLabel !== $gameTitle && str_starts_with($tierLabel, $gameTitle)) {
+                            $tierLabel = ltrim(substr($tierLabel, strlen($gameTitle)), ' -:');
+                        }
+
+                        // Only append the tier label if the event title doesn't already contain it.
+                        if (! str_ends_with($event->title, $tierLabel)) {
+                            $tooltipTitle = "$event->title - $tierLabel";
+                        }
+
+                        $tooltipDescription = "Awarded for earning at least $actualEventAward->points_required points";
+                    }
+
+                    $award["Tooltip"] = $tooltipTitle . "<br>" . $tooltipDescription;
                     $award["ImageIcon"] = media_asset($image);
                     $award["IsGold"] = true;
                     $award["Link"] = route('event.show', $event->id);
                     /* <div class='p-2 max-w-[320px] text-pretty'><span>$tooltip</span><p class='italic'>{$awardDate}</p></div> */
+                    // tooltip: "<div class='p-2 max-w-[320px] text-pretty text-menu-link flex flex-col gap-1'><p class='font-bold'>{$tooltipTitle}</p><span>{$tooltipDescription}</span><p class='italic'>{$awardDate}</p></div>",
                 }
 
             } elseif (AwardType::isActive($type)) {
