5f547c31b55fa66f9338e9b72a89c2f9928aa822
Wes Copeland <wlcopeland1@gmail.com>
2026-03-08 16:03:18 -0400

    feat(game): add expandable RP details

--- resources/js/features/games/components/GameRecentPlayers/GameRecentPlayers.tsx
+++ resources/js/features/games/components/GameRecentPlayers/GameRecentPlayers.tsx
@@ -1,5 +1,6 @@
-import type { FC } from 'react';
+import { type FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useCookie } from 'react-use';
 
 import { usePageProps } from '@/common/hooks/usePageProps';
 
@@ -7,13 +8,25 @@ import { GameRecentPlayersList } from './GameRecentPlayersList';
 import { GameRecentPlayersTable } from './GameRecentPlayersTable';
 
 export const GameRecentPlayers: FC = () => {
-  const { recentPlayers } = usePageProps<App.Platform.Data.GameShowPageProps>();
-
+  const { isRichPresenceExpanded, recentPlayers } =
+    usePageProps<App.Platform.Data.GameShowPageProps>();
   const { t } = useTranslation();
+
+  const [, setCookieValue] = useCookie('prefers_expanded_rich_presence');
+  const [isExpanded, setIsExpanded] = useState(isRichPresenceExpanded);
+
+  // Only show a pointer cursor if at least one RP message is long enough to actually truncate.
+  const hasAnyTruncatedRichPresence = recentPlayers.some((p) => p.richPresence.length >= 40);
 
   if (!recentPlayers?.length) {
     return null;
   }
+
+  const handleToggleExpanded = () => {
+    const next = !isExpanded;
+    setIsExpanded(next);
+    setCookieValue(String(next), { expires: 180, path: '/' });
+  };
 
   return (
     <div>
@@ -21,10 +34,18 @@ export const GameRecentPlayers: FC = () => {
 
       <div className="rounded-lg bg-embed p-1">
         <div className="flex flex-col gap-2 p-1 sm:hidden">
-          <GameRecentPlayersList />
+          <GameRecentPlayersList
+            canToggleExpanded={hasAnyTruncatedRichPresence}
+            isExpanded={isExpanded}
+            onToggleExpanded={handleToggleExpanded}
+          />
         </div>
 
-        <GameRecentPlayersTable />
+        <GameRecentPlayersTable
+          canToggleExpanded={hasAnyTruncatedRichPresence}
+          isExpanded={isExpanded}
+          onToggleExpanded={handleToggleExpanded}
+        />
       </div>
     </div>
   );
