4441115dbfb8b3e2682f5b780b33ae60d820bd2a
Wes Copeland <wlcopeland1@gmail.com>
2026-03-27 17:37:24 -0400

    perf(vitest): make full test runs faster

--- resources/js/common/components/AppProviders/AppProviders.tsx
+++ resources/js/common/components/AppProviders/AppProviders.tsx
@@ -1,14 +1,17 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider as JotaiProvider } from 'jotai';
 import { domAnimation, LazyMotion } from 'motion/react';
-import { type FC, type ReactNode } from 'react';
+import { lazy, type FC, type ReactNode, Suspense } from 'react';
 import { I18nextProvider, type I18nextProviderProps } from 'react-i18next';
 
 import { BaseToaster } from '../+vendor/BaseToaster';
 import { BaseTooltipProvider } from '../+vendor/BaseTooltip';
 import { GlobalSearchProvider } from '../GlobalSearchProvider';
 import { useAppQueryClient } from './useAppQueryClient';
+
+const ReactQueryDevtools = lazy(() =>
+  import('@tanstack/react-query-devtools').then((m) => ({ default: m.ReactQueryDevtools })),
+);
 
 interface AppProvidersProps {
   children: ReactNode;
@@ -31,9 +34,11 @@ export const AppProviders: FC<AppProvidersProps> = ({ children, i18n }) => {
 
                 {/* Everything below this line is excluded from prod builds. */}
                 {import.meta.env.VITE_REACT_QUERY_DEVTOOLS_ENABLED === 'true' ? (
-                  <div className="hidden text-lg sm:block" data-testid="query-devtools">
-                    <ReactQueryDevtools />
-                  </div>
+                  <Suspense>
+                    <div className="hidden text-lg sm:block" data-testid="query-devtools">
+                      <ReactQueryDevtools />
+                    </div>
+                  </Suspense>
                 ) : null}
               </GlobalSearchProvider>
             </BaseTooltipProvider>
