658538086c2fb1511f2d75843687082f313997c9
Jamiras <jamirasmt@gmail.com>
2026-04-05 17:14:37 +0000

    add info tooltip to setting

--- resources/js/features/settings/components/PreferencesSectionCard/PreferencesSwitchField.tsx
+++ resources/js/features/settings/components/PreferencesSectionCard/PreferencesSwitchField.tsx
@@ -1,5 +1,7 @@
 import { type FC } from 'react';
 import type { Control } from 'react-hook-form';
+import { LuInfo } from 'react-icons/lu';
+import { cn } from '@/common/utils/cn';
 
 import {
   BaseFormControl,
@@ -8,6 +10,7 @@ import {
   BaseFormLabel,
 } from '@/common/components/+vendor/BaseForm';
 import { BaseSwitch } from '@/common/components/+vendor/BaseSwitch';
+import { BaseTooltip, BaseTooltipContent, BaseTooltipTrigger } from '@/common/components/+vendor/BaseTooltip';
 import type { StringifiedUserPreference } from '@/common/utils/generatedAppConstants';
 import type { TranslatedString } from '@/types/i18next';
 
@@ -25,6 +28,8 @@ interface PreferencesTableRowProps {
    * If true, the switch will show as enabled when the setting is turned off.
    */
   isSwitchInverted?: boolean;
+
+  t_infoText?: TranslatedString;
 }
 
 export const PreferencesSwitchField: FC<PreferencesTableRowProps> = ({
@@ -32,6 +37,7 @@ export const PreferencesSwitchField: FC<PreferencesTableRowProps> = ({
   fieldName,
   control,
   isSwitchInverted = false,
+  t_infoText = null,
 }) => {
   return (
     <BaseFormField
@@ -40,6 +46,23 @@ export const PreferencesSwitchField: FC<PreferencesTableRowProps> = ({
       render={({ field }) => (
         <BaseFormItem className="flex w-full items-center justify-between gap-1">
           <BaseFormLabel>{t_label}</BaseFormLabel>
+
+          {t_infoText !== null ? (
+            <BaseTooltip>
+              <BaseTooltipTrigger>
+                <LuInfo
+                  className={cn(
+                    'hidden size-4 text-neutral-500 transition hover:text-neutral-300 sm:inline',
+                    'light:text-neutral-700',
+                  )}
+                />
+              </BaseTooltipTrigger>
+
+              <BaseTooltipContent className="max-w-72 font-normal leading-normal">
+                <span className="text-xs font-normal">{t_infoText}</span>
+              </BaseTooltipContent>
+            </BaseTooltip>
+          ) : null}
 
           <BaseFormControl>
             <BaseSwitch
