0f9ae707f05a049321affe9747087962439539eb
Wes Copeland <wlcopeland1@gmail.com>
2026-04-19 13:36:10 -0400

    chore: oxfmt

--- resources/js/features/game-list/utils/getIsDefaultSorting.ts
+++ resources/js/features/game-list/utils/getIsDefaultSorting.ts
@@ -1,9 +1,6 @@
 import type { ColumnSort, SortingState } from '@tanstack/react-table';
 
-export function getIsDefaultSorting(
-  sorting: SortingState,
-  defaultColumnSort: ColumnSort,
-): boolean {
+export function getIsDefaultSorting(sorting: SortingState, defaultColumnSort: ColumnSort): boolean {
   return (
     sorting.length === 1 &&
     sorting[0].id === defaultColumnSort.id &&
