a546ef6cc973f270dd692a95d919008467778943
Jamiras <jamirasmt@gmail.com>
2026-05-02 16:32:07 +0000

    extract BuildConnectSniffsAction

--- resources/views/pages-legacy/sentry.blade.php
+++ resources/views/pages-legacy/sentry.blade.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Enums\ClientSupportLevel;
+use App\Connect\Actions\BuildConnectSniffsAction;
 use App\Enums\Permissions;
-use App\Models\ConnectWarning;
-use App\Models\Leaderboard;
-use App\Models\User;
-use App\Platform\Services\UserAgentService;
 use Carbon\Carbon;
 
 if (!authenticateFromCookie($user, $permissions, $userDetails, Permissions::Moderator)) {
@@ -50,139 +46,27 @@ for ($i = 0; $i < 90; $i++) {
 $method_counts = [];
 $smell_counts = [];
 $user_counts = [];
-
 $sniffs = [];
+
 $selected = requestInputSanitized('file');
 if ($selected) {
-    $usernames = [];
-    $leaderboardIds = [];
-    $invalidUserHashes = [];
-    $userAgentService = new UserAgentService();
-
     $date = Carbon::parse(substr($selected, 7));
-    $entries = ConnectWarning::query()
-        ->where('created_at', '>=', $date->clone()->startOfDay())
-        ->where('created_at', '<=', $date->clone()->endOfDay())
-        ->with('playerSession', 'playerSession.gameHash')
-        ->orderBy('created_at')
-        ->get();
-    foreach ($entries as $entry) {
-        $method_counts[$entry->method] = ($method_counts[$entry->method] ?? 0) + 1;
-        $user_counts[$entry->username] = ($user_counts[$entry->username] ?? 0) + 1;
-        if (!in_array($entry->username, $usernames)) {
-            $usernames[] = $entry->username;
-        }
+    $sniffs = (new BuildConnectSniffsAction())->execute($date, $clients);
 
-        $sniff = [
-            'date' => $entry->created_at->format('Y-m-d H:i:s'),
-            'user' => $entry->username,
-            'method' => $entry->method,
-            'smells' => [],
-            'validationHash' => $entry->validation_hash,
-            'serverValidationHashes' => [],
-        ];
-
-        foreach (explode(',', $entry->smells) as $smell) {
-            $sniff['smells'][] = $smell;
-
-            if ($smell === 'bad_validation' && $entry->related_id) {
-                $validationHash = strtolower($entry->validation_hash);
-
-                $existingId = (int) ($invalidUserHashes[$entry->username][$validationHash] ?? 0);
-                if ($existingId === 0) {
-                    $invalidUserHashes[$entry->username][$validationHash] = $entry->related_id;
-                } elseif ($existingId !== $entry->related_id) {
-                    $sniff['smells'][] = 'repeated_validation';
-                    $sniff['repeatedHash' . ucfirst($entry->related_type) . 'Id'] = $existingId;
-                }
-            }
-        }
-
-        if ($entry->offset !== null) {
-            $sniff['offset'] = $entry->offset;
-        }
-
-        if ($entry->related_type === 'leaderboard') {
-            if (!in_array($entry->related_id, $leaderboardIds)) {
-                $leaderboardIds[] = $entry->related_id;
-            }
-
-            $sniff['leaderboardId'] = $entry->related_id;
-            $sniff['score'] = $entry->extra;
-
-            $sniff['serverValidationHashes']['id_user_score'] = md5($entry->related_id . $entry->username . $entry->extra);
-            
-            if ($entry->offset !== null) {
-                $sniff['serverValidationHashes']['id_user_score_offset'] = md5($entry->related_id . $entry->username . $entry->extra . $entry->offset);
-            }
-        }
-
-        if ($entry->playerSession) {
-            $sniff['gameHash'] = $entry->playerSession->gameHash?->md5;
-        }
-
-        $sniff['userAgent'] = $userAgent = $entry->user_agent;
-        if (empty($userAgent)) {
-            $sniff['smells'][] = 'no_user_agent';
-        } elseif (str_contains($userAgent, 'Mozilla')) {
-            $sniff['smells'][] = 'browser';
-        } elseif (in_array('invalid_client', $sniff['smells']) || in_array('blocked_client', $sniff['smells'])) {
-            $data = $userAgentService->decode($userAgent);
-            $client = $data['client'];
-            $sniff['smells'][] = $client;
-            if (!in_array($client, $clients)) {
-                $clients[] = $client;
-            }
-        }
-        
-        $sniffs[] = $sniff;
-    }
-
-    $userInfos = [];
-    $users = User::query()
-        ->whereIn('display_name', $usernames)
-        ->withTrashed()
-        ->select('id', 'username', 'display_name', 'Permissions', 'deleted_at', 'unranked_at')
-        ->get()
-        ->toArray();
-    foreach ($users as $user) {
-        $userInfos[strtolower($user['username'])] = $user;
-        $userInfos[strtolower($user['display_name'])] = $user;
-    }
-
-    $leaderboards = Leaderboard::query()
-        ->whereIn('id', $leaderboardIds)
-        ->select('id', 'title')
-        ->get()
-        ->keyBy('id')
-        ->toArray();
-
-    foreach ($sniffs as &$sniff) {
-        $lowerUsername = strtolower($sniff['user']);
-        if (array_key_exists($lowerUsername, $userInfos)) {
-            $sniff['userinfo'] = $userInfos[$lowerUsername];
-            $sniff['link'] = route('user.show', $sniff['userinfo']['display_name']);
-        } else {
-            $sniff['link'] = route('user.show', $sniff['user']);
-            $sniff['smells'][] = empty($lowerUsername) ? 'no_user' : 'unknown_user';
-        }
-
-        if (array_key_exists('leaderboardId', $sniff) && array_key_exists($sniff['leaderboardId'], $leaderboards)) {
-            $sniff['leaderboard'] = $leaderboards[$sniff['leaderboardId']];
-        }
+    foreach ($sniffs as $sniff) {
+        $user_counts[$sniff['user']] = ($user_counts[$sniff['user']] ?? 0) + 1;
+        $method_counts[$sniff['method']] = ($method_counts[$sniff['method']] ?? 0) + 1;
 
         foreach ($sniff['smells'] as $smell) {
             $smell_counts[$smell] = ($smell_counts[$smell] ?? 0) + 1;
         }
     }
 
-    $sniffs = array_reverse($sniffs); // newest first
+    ksort($user_counts);
+    ksort($method_counts);
+    ksort($smell_counts);
+    sort($clients);
 }
-
-ksort($user_counts);
-ksort($method_counts);
-ksort($smell_counts);
-sort($clients);
 
 $colors = [
     'bad_validation' => [ // validation hash did not match expected values
