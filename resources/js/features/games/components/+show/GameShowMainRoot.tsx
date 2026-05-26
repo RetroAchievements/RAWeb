f268c66a24825041f426e6b466cc307bd364bf03
Wes Copeland <wlcopeland1@gmail.com>
2026-05-26 17:07:47 -0400

    feat(game): add more polish to screenshots on the game page (#4915)
    This PR makes a few subtle polish-related changes to how game
    screenshots are displayed on game pages:
    * Images in the dialog now preload correctly again (this broke with the
    change to dedicated thumbnail images).
    * Blur-ins are now fully wired in on the dialog.
    * Thumbnail dimensions are now passed to the game page as props so the
    thumbnails themselves don't layout shift if they're not already cached
    by the browser.
    * Additional flicker reduction has been added to the dialog (including
    giving it a solid black backdrop).
    
    **How to test this PR**
    Go to a few game pages, make sure nothing in the thumbnails looks weird,
    and make sure clicking them still opens the dialog properly (and it
    behaves as you might expect).

--- resources/js/features/games/components/+show/GameShowMainRoot.tsx
+++ resources/js/features/games/components/+show/GameShowMainRoot.tsx
@@ -44,11 +44,11 @@ export const GameShowMainRoot: FC = () => {
       {allPageAchievements.length ? <ResetAllProgressDialog /> : null}
 
       <PlayableMainMedia
-        expectedHeight={game.system?.screenshotResolutions?.[0]?.height}
-        expectedWidth={game.system?.screenshotResolutions?.[0]?.width}
         hasAnalogTvOutput={game.system?.hasAnalogTvOutput}
         hasBeatenGame={hasBeatenGame}
+        imageIngameDimensions={game.imageIngameDimensions}
         imageIngameUrl={game.imageIngameUrl!}
+        imageTitleDimensions={game.imageTitleDimensions}
         imageTitleUrl={game.imageTitleUrl!}
         isPixelated={!game.system!.supportsUpscaledScreenshots}
         numScreenshots={numScreenshots}
