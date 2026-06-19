4734147cc6a105d19895af2a1a585e25bad21304
Wes Copeland <wlcopeland1@gmail.com>
2026-06-19 11:59:42 -0400

    perf: address N+1 issue

--- app/Platform/Data/TicketData.php
+++ app/Platform/Data/TicketData.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Platform\Data;
 
 use App\Community\Enums\TicketState;
+use App\Models\Achievement;
+use App\Models\Leaderboard;
 use App\Models\Ticket;
 use App\Platform\Enums\TicketableType;
 use LogicException;
@@ -31,10 +33,14 @@ class TicketData extends Data
             id: $ticket->id,
             ticketableType: $ticketableType,
             state: Lazy::create(fn () => $ticket->state),
-            ticketable: Lazy::create(fn () => match ($ticketableType) {
-                TicketableType::Achievement => AchievementData::fromAchievement($ticket->achievement),
-                TicketableType::Leaderboard => LeaderboardData::fromLeaderboard($ticket->leaderboard),
-                TicketableType::RichPresence => throw new LogicException('Rich presence tickets are not currently supported.'),
+            ticketable: Lazy::create(function () use ($ticket) {
+                $ticketable = $ticket->ticketable;
+
+                return match (true) {
+                    $ticketable instanceof Achievement => AchievementData::fromAchievement($ticketable),
+                    $ticketable instanceof Leaderboard => LeaderboardData::fromLeaderboard($ticketable),
+                    default => throw new LogicException("Unsupported ticketable for ticket {$ticket->id}."),
+                };
             }),
         );
     }
