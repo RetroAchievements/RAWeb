a3ebc66d083964fd7623b2413653aeef081c19a8
angelol-git <ang.ladores.dev@gmail.com>
2026-07-13 18:53:24 -0400

    address feedback

--- resources/js/features/messages/components/MessagesCardList/MessagesCard.tsx
+++ resources/js/features/messages/components/MessagesCardList/MessagesCard.tsx
@@ -7,6 +7,7 @@ import { InertiaLink } from '@/common/components/InertiaLink';
 import { UserAvatar } from '@/common/components/UserAvatar';
 import { usePageProps } from '@/common/hooks/usePageProps';
 import { cn } from '@/common/utils/cn';
+import { getOtherParticipant } from '@/features/messages/utils/getOtherParticipant';
 
 interface MessagesCardProps {
   messageThread: App.Community.Data.MessageThread;
@@ -18,16 +19,13 @@ export const MessagesCard: FC<MessagesCardProps> = ({ messageThread }) => {
 
   const { t } = useTranslation();
 
-  const otherParticipant =
-    (messageThread.participants?.find(
-      (p) => p.displayName !== senderUserDisplayName,
-    ) as App.Data.User) ?? messageThread.participants?.[0];
+  const otherParticipant = getOtherParticipant(messageThread, senderUserDisplayName);
 
   return (
     <div className="embedded">
       <div className="flex flex-col gap-2">
         <div className="flex items-center justify-between">
-          <UserAvatar {...otherParticipant} size={16} />
+          {otherParticipant ? <UserAvatar {...otherParticipant} size={16} /> : <span />}
 
           <span className="text-2xs text-neutral-400 light:text-neutral-700">
             <DiffTimestamp
